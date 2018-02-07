f3f32e3f01596db8ecaab2cd3216a6ec2222657a
Dmitry Zhukov <dzhuk@yandex-team.ru>
2018-02-07 19:25:38 +0300

    feat third_party: import spdlog (#3)
    Relates: TAXIBACKEND-12191

---
+++ third_party/spdlog/fmt/fmt.h
@@ -0,0 +1,34 @@
+//
+// Copyright(c) 2016 Gabi Melman.
+// Distributed under the MIT License (http://opensource.org/licenses/MIT)
+//
+
+#pragma once
+
+//
+// Include a bundled header-only copy of fmtlib or an external one.
+// By default spdlog include its own copy.
+//
+
+#if !defined(SPDLOG_FMT_EXTERNAL)
+
+#ifndef FMT_HEADER_ONLY
+#define FMT_HEADER_ONLY
+#endif
+#ifndef FMT_USE_WINDOWS_H
+#define FMT_USE_WINDOWS_H 0
+#endif
+#include "bundled/format.h"
+#if defined(SPDLOG_FMT_PRINTF)
+#include "bundled/printf.h"
+#endif
+
+#else //external fmtlib
+
+#include <fmt/format.h>
+#if defined(SPDLOG_FMT_PRINTF)
+#include <fmt/printf.h>
+#endif
+
+#endif
+
