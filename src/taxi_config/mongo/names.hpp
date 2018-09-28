732eb242f90e0f15b42245b207d10e5bf163c5aa
Andrey Bugaevskiy <bugaevskiy@yandex-team.ru>
2018-09-28 12:01:46 +0000

    bug mongo: remove global names.hpp again (#135)
    Relates: TAXICOMMON-83

--- src/taxi_config/mongo/names.hpp
+++ src/taxi_config/mongo/names.hpp
@@ -10,7 +10,7 @@ static const std::string kDb = "dbtaxi";
 static const std::string kDbProcessing = "dbprocessing";
 
 namespace config {
-static const std::string kCollection = kDb + ".config";
+static const std::string kCollection = "config";
 
 static const std::string kId = "_id";
 static const std::string kUpdated = "updated";
