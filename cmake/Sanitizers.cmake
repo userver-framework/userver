7bfd56b6e2feaa5bc3d41021d602b285a2d64119
Antony Polukhin <antoshkka@yandex-team.ru>
2018-12-20 19:24:52 +0300

    feat all: add blacklists for sanitizers (#364)
    Relates: TAXICOMMON-491

--- cmake/Sanitizers.cmake
+++ cmake/Sanitizers.cmake
@@ -8,23 +8,27 @@ endif()
 
 if(SANITIZE STREQUAL "")
   # no sanitizer
-elseif(SANITIZE STREQUAL "ub")
-  # https://clang.llvm.org/docs/UndefinedBehaviorSanitizer.html
-  set(SANITIZE_BUILD_FLAGS -fsanitize=undefined -fno-sanitize-recover=undefined)
-elseif(SANITIZE STREQUAL "addr")
-  # http://releases.llvm.org/5.0.0/tools/clang/docs/AddressSanitizer.html
-  set(SANITIZE_DEFS BOOST_USE_ASAN)
-  set(SANITIZE_BUILD_FLAGS -fsanitize=address -g)
-  set(SANITIZE_CXX_FLAGS -fno-omit-frame-pointer)
-elseif(SANITIZE STREQUAL "thread")
-  # http://releases.llvm.org/5.0.0/tools/clang/docs/ThreadSanitizer.html
-  set(SANITIZE_BUILD_FLAGS -fsanitize=thread -g)
-elseif(SANITIZE STREQUAL "mem")
-  # https://clang.llvm.org/docs/MemorySanitizer.html
-  set(SANITIZE_BUILD_FLAGS -fsanitize=memory -g)
-  set(SANITIZE_CXX_FLAGS -fno-omit-frame-pointer)
 else()
-  message(FATAL_ERROR "-DSANITIZE has invalid value (${SANITIZE}), possible values: ${SANITIZE_ENUM}")
+  get_filename_component(SANITIZE_BLACKLIST san_blacklist.txt ABSOLUTE CACHE)
+
+  if(SANITIZE STREQUAL "ub")
+    # https://clang.llvm.org/docs/UndefinedBehaviorSanitizer.html
+    set(SANITIZE_BUILD_FLAGS -fsanitize=undefined -fno-sanitize-recover=undefined)
+  elseif(SANITIZE STREQUAL "addr")
+    # http://releases.llvm.org/5.0.0/tools/clang/docs/AddressSanitizer.html
+    set(SANITIZE_DEFS BOOST_USE_ASAN)
+    set(SANITIZE_BUILD_FLAGS -fsanitize=address -g)
+    set(SANITIZE_CXX_FLAGS -fno-omit-frame-pointer)
+  elseif(SANITIZE STREQUAL "thread")
+    # http://releases.llvm.org/5.0.0/tools/clang/docs/ThreadSanitizer.html
+    set(SANITIZE_BUILD_FLAGS -fsanitize=thread -g)
+  elseif(SANITIZE STREQUAL "mem")
+    # https://clang.llvm.org/docs/MemorySanitizer.html
+    set(SANITIZE_BUILD_FLAGS -fsanitize=memory -g)
+    set(SANITIZE_CXX_FLAGS -fno-omit-frame-pointer)
+  else()
+    message(FATAL_ERROR "-DSANITIZE has invalid value (${SANITIZE}), possible values: ${SANITIZE_ENUM}")
+  endif()
 endif()
 
 if(DEFINED SANITIZE_BLACKLIST AND (NOT SANITIZE_BLACKLIST STREQUAL ""))
