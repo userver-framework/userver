d79118e90401aeb412f0ae57619a0866d2c3c05c
Vasily Kulikov <segoon@yandex-team.ru>
2018-12-20 19:25:58 +0300

    feat: add build-dependencies (#355)
    Relates: TAXICOMMON-497

--- cmake/FindPackageRequired.cmake
+++ cmake/FindPackageRequired.cmake
@@ -3,7 +3,10 @@ macro(init_debian_depends)
 endmacro()
 
 macro(append_debian_depends PACKAGE)
-    set(DEBIAN_DEPENDS "${DEBIAN_DEPENDS}, ${PACKAGE}" CACHE INTERNAL DEBIAN_DEPENDS)
+    string(FIND "${DEBIAN_DEPENDS}" "${PACKAGE}" INDEX)
+    if(INDEX EQUAL -1)
+        set(DEBIAN_DEPENDS "${DEBIAN_DEPENDS}, ${PACKAGE}" CACHE INTERNAL DEBIAN_DEPENDS)
+    endif()
 endmacro()
 
 macro(find_package_required PACKAGE debpackage)
@@ -13,8 +16,20 @@ macro(find_package_required PACKAGE debpackage)
 
     if(NOT ${PACKAGE_UPPERCASE}_FOUND AND NOT ${PACKAGE}_FOUND)
         message(FATAL_ERROR
-		"Cmake module for ${PACKAGE} not found.\n"
-            "Please install '${debpackage}' package.")
+                "Cmake module for ${PACKAGE} not found.\n"
+                "Please install '${debpackage}' package.")
+    endif()
+endmacro()
+
+macro(find_package_components_required PACKAGE debpackage)
+    find_package(${PACKAGE} COMPONENTS ${ARGN})
+    append_debian_depends(${debpackage})
+    string(TOUPPER "${PACKAGE}" PACKAGE_UPPERCASE)
+
+    if(NOT ${PACKAGE_UPPERCASE}_FOUND AND NOT ${PACKAGE}_FOUND)
+        message(FATAL_ERROR
+                "Cmake module for ${PACKAGE} not found.\n"
+                "Please install '${debpackage}' package.")
     endif()
 endmacro()
 
@@ -25,7 +40,18 @@ macro(find_package_required_version PACKAGE debpackage version)
     string(TOUPPER "${PACKAGE}" PACKAGE_UPPERCASE)
     if(NOT ${PACKAGE_UPPERCASE}_FOUND AND NOT ${PACKAGE}_FOUND)
         message(FATAL_ERROR
-		"Cmake module for ${PACKAGE} version ${version} not found.\n"
-            "Please install '${debpackage}' package.")
+                "Cmake module for ${PACKAGE} version ${version} not found.\n"
+                "Please install '${debpackage}' package.")
+    endif()
+endmacro()
+
+macro(find_program_required PROGRAM debpackage)
+    find_program(BINARY ${PROGRAM})
+    append_debian_depends(${debpackage})
+
+    if(${BINARY} STREQUAL BINARY-NOTFOUND)
+        message(FATAL_ERROR
+                "Program ${PROGRAM} not found in $PATH.\n"
+                "Please install '${debpackage}' package.")
     endif()
 endmacro()
