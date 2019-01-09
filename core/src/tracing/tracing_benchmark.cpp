674d0300a1e193e4875907ab552fe1e1eb3f4fad
Vasily Kulikov <segoon@yandex-team.ru>
2019-01-09 13:03:41 +0300

    bug benchmark: fix abort() (#390)
    Relates: TAXICOMMON-556

--- core/src/tracing/tracing_benchmark.cpp
+++ core/src/tracing/tracing_benchmark.cpp
@@ -1,15 +1,20 @@
 #include <benchmark/benchmark.h>
 
+#include <engine/run_in_coro.hpp>
 #include <tracing/noop.hpp>
 #include <utils/gbench_auxilary.hpp>
 
 namespace {
 
 void tracing_noop_ctr(benchmark::State& state) {
-  auto tracer = tracing::MakeNoopTracer();
+  RunInCoro(
+      [&] {
+        auto tracer = tracing::MakeNoopTracer();
 
-  for (auto _ : state)
-    benchmark::DoNotOptimize(tracer->CreateSpanWithoutParent("name"));
+        for (auto _ : state)
+          benchmark::DoNotOptimize(tracer->CreateSpanWithoutParent("name"));
+      },
+      1);
 }
 BENCHMARK(tracing_noop_ctr);
 
