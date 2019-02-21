02537524523089cea1ead66b832935734f4aebc0
Nikita Borisov <nikslim@yandex-team.ru>
2019-02-21 14:45:07 +0300

    feat core: add headers to meta crypto header (#501)
    Relates: TAXICOMMON-717

--- core/include/crypto/crypto.hpp
+++ core/include/crypto/crypto.hpp
@@ -4,6 +4,8 @@
 /// @brief Include-all header for crypto routines
 
 #include <crypto/algorithm.hpp>
+#include <crypto/base64.hpp>
+#include <crypto/exception.hpp>
 #include <crypto/hash.hpp>
 
 /// Cryptography support
