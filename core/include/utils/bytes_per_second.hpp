fa8e20c7695e57dec5206157ecb7e7a268100728
Antony Polukhin <antoshkka@yandex-team.ru>
2019-04-02 12:40:12 +0300

    feat all: add rate limit types and conversions (#578)
    Relates: TAXICOMMON-681

---
+++ core/include/utils/bytes_per_second.hpp
@@ -0,0 +1,32 @@
+#pragma once
+
+#include <string>
+
+#include <formats/parse/to.hpp>
+
+namespace utils {
+
+enum class BytesPerSecond : long long {};
+
+constexpr long long ToLongLong(BytesPerSecond x) {
+  return static_cast<long long>(x);
+}
+
+constexpr bool operator==(BytesPerSecond lhs, BytesPerSecond rhs) {
+  return ToLongLong(lhs) == ToLongLong(rhs);
+}
+
+constexpr bool operator!=(BytesPerSecond lhs, BytesPerSecond rhs) {
+  return !(lhs == rhs);
+}
+
+/// Understands all the date-rate unit suffixes from
+/// https://en.wikipedia.org/wiki/Data-rate_units#Conversion_table
+BytesPerSecond StringToBytesPerSecond(const std::string& data);
+
+template <class Value>
+BytesPerSecond Parse(const Value& v, formats::parse::To<BytesPerSecond>) {
+  return StringToBytesPerSecond(v.template As<std::string>());
+}
+
+}  // namespace utils
