072655821db9dfcc0607da88bf7d3e0b7cabc382
Nikita Borisov <nikslim@yandex-team.ru>
2019-05-15 13:21:02 +0300

    feat core: enable stacktraces on mac (#665)
    Relates: TAXICOMMON-1007

--- core/src/logging/stacktrace_cache_test.cpp
+++ core/src/logging/stacktrace_cache_test.cpp
@@ -9,10 +9,8 @@ TEST(stacktrace_cache, frame) {
   EXPECT_EQ(orig_name, cache_name);
 }
 
-#ifndef __MACH__
 TEST(stacktrace_cache, full) {
   auto st = boost::stacktrace::stacktrace();
   EXPECT_EQ(boost::stacktrace::to_string(st),
             logging::stacktrace_cache::to_string(st));
 }
-#endif  // __MACH__
