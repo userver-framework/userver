0ca10ca1bc6061575a2617a8a020f0cefa201be0
Sergei Fedorov <ser-fedorov@yandex-team.ru>
2019-06-11 19:08:43 +0300

    feat all: allow earlier parsing in user handlers (#702)
    Relates: TAXICOMMON-1081

--- core/include/server/handlers/http_handler_json_base.hpp
+++ core/include/server/handlers/http_handler_json_base.hpp
@@ -32,12 +32,13 @@ class HttpHandlerJsonBase : public HttpHandlerBase {
   const formats::json::Value* GetResponseJson(
       const request::RequestContext& context) const;
 
+ protected:
+  void ParseRequestData(const http::HttpRequest& request,
+                        request::RequestContext& context) const;
+
  private:
   std::string GetFormattedExternalErrorBody(
       http::HttpStatus status, std::string external_error_body) const final;
-
-  void ParseRequestData(const http::HttpRequest& request,
-                        request::RequestContext& context) const final;
 };
 
 }  // namespace handlers
