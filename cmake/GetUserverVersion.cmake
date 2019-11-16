055edaff22047b1ab3884ba26ec5ff7248d4632f
Andrey Bugaevskiy <bugaevskiy@yandex-team.ru>
2019-11-16 22:03:57 +0300

    typo cmake: fix version commit calculation (#977)
    Relates: TAXICOMMON-1680

--- cmake/GetUserverVersion.cmake
+++ cmake/GetUserverVersion.cmake
@@ -4,16 +4,16 @@ set(USERVER_HASH "unknown")
 
 find_package(Git)
 
-if(GIT_FOUND)
+if(Git_FOUND)
   execute_process(
-    COMMAND ${GIT_EXECUTABLE} rev-parse --short HEAD
-    WORKING_DIRECTORY ${CMAKE_CURRENT_SOURCE_DIR}
+    COMMAND ${Git_EXECUTABLE} rev-parse --short HEAD
+    WORKING_DIRECTORY ${CMAKE_SOURCE_DIR}
     OUTPUT_VARIABLE USERVER_HASH
     ERROR_QUIET
     OUTPUT_STRIP_TRAILING_WHITESPACE
   )
 else()
-  message(STATUS "GIT not found")
+  message(STATUS "Git not found")
 endif()
 
 set(USERVER_VERSION "${USERVER_BUILD_TIME}.${USERVER_HASH}")
