aa5e10fc0349fdbfffe13ebf50bd60aff398396c
Antony Polukhin <antoshkka@yandex-team.ru>
2019-12-11 14:25:33 +0300

    feat core: flush logs before aborting (#1036)
    Tests: протестировано локально тестсьютом
    
    Relates: TAXICOMMON-1581

--- core/src/utils/assert.cpp
+++ core/src/utils/assert.cpp
@@ -12,13 +12,14 @@ void UASSERT_failed(const std::string& expr, const char* file,
                     unsigned int line, const char* function,
                     const std::string& msg) noexcept {
   auto trace = boost::stacktrace::stacktrace();
-  std::string trace_msg = boost::stacktrace::to_string(trace);
 
   std::cerr << file << ":" << line << ":" << (function ? function : "")
             << ": Assertion '" << expr << "' failed"
             << (msg.empty() ? std::string{} : (": " + msg))
             << ".  Stacktrace:\n"
-            << trace_msg << "\n";
+            << trace << "\n";
+
+  logging::LogFlush();
   abort();
 }
 
