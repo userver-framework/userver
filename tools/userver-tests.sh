ea90651a8a8be147f9a40e52f11f407bde9e7021
Vasily Kulikov <segoon@yandex-team.ru>
2020-07-24 01:01:07 +0300

    feat build: pass MAKE_OPTS (#1424)
    Relates: TAXITOOLS-2495
    Tests: на сервисы не влияет

--- tools/userver-tests.sh
+++ tools/userver-tests.sh
@@ -16,12 +16,12 @@ chmod 777 "$CORES_DIR"
 echo "$CORES_DIR/core-%e-%s-%u-%g-%p-%t" > /proc/sys/kernel/core_pattern
 if [ "$PULL_REQUEST_TYPE" = "ASAN" ]; then
     echo "Build userver with asan"
-    /taxi/tools/run_as_user.sh make build-with-asan
+    /taxi/tools/run_as_user.sh make $MAKE_OPTS build-with-asan
 elif [ "$PULL_REQUEST_TYPE" = "UBSAN" ]; then
     echo "Build userver with ubsan"
-    /taxi/tools/run_as_user.sh make build-with-ubsan
+    /taxi/tools/run_as_user.sh make $MAKE_OPTS build-with-ubsan
 elif [ "$PULL_REQUEST_TYPE" = "SANITIZE" ]; then
     echo "Build userver with default set of sanitizers"
-    /taxi/tools/run_as_user.sh make build-with-sanitizers
+    /taxi/tools/run_as_user.sh make $MAKE_OPTS build-with-sanitizers
 fi
 /taxi/tools/run_as_user.sh bash -c 'ulimit -c unlimited; make test'
