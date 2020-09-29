535cc617592c0810c52acd85a100dfa8e39799a7
Antony Polukhin <antoshkka@yandex-team.ru>
2020-09-29 20:41:10 +0300

    feat all: disable INFO and WARN logs in benchmarks (#1565)
    Tests: протестировано локально и тестсьютом
    Relates: TAXICOMMON-2949

---
+++ core/src/core_benchmark.cpp
@@ -0,0 +1,11 @@
+#include <benchmark/benchmark.h>
+
+#include <logging/log.hpp>
+
+int main(int argc, char** argv) {
+  logging::SetDefaultLoggerLevel(logging::Level::kError);
+
+  ::benchmark::Initialize(&argc, argv);
+  if (::benchmark::ReportUnrecognizedArguments(argc, argv)) return 1;
+  ::benchmark::RunSpecifiedBenchmarks();
+}
