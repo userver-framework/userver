48020f145d0d8da36d4816dd50e76c06da030ea0
Antony Polukhin <antoshkka@yandex-team.ru>
2020-10-30 19:10:33 +0300

    fix build: allow building outside the git tree (#1635)
    Tests: протестировано локально и тестсьютом
    Relates: TAXICOMMON-3114

--- cmake/GetUserverVersion.cmake
+++ cmake/GetUserverVersion.cmake
@@ -9,9 +9,15 @@ if(Git_FOUND)
     COMMAND ${Git_EXECUTABLE} rev-parse --short HEAD
     WORKING_DIRECTORY ${CMAKE_SOURCE_DIR}
     OUTPUT_VARIABLE USERVER_HASH
+    RESULT_VARIABLE STATUS
     ERROR_QUIET
     OUTPUT_STRIP_TRAILING_WHITESPACE
   )
+
+  if(STATUS)
+    set(USERVER_HASH "unknown")
+    message(STATUS "Failed to retrive git short hash")
+  endif()
 else()
   message(STATUS "Git not found")
 endif()
