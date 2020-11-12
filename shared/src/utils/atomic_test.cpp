f8a3739506f36b43b206a3fc8c312b5d8ca1ae63
Anton Zhilin <antonyzhilin@gmail.com>
2020-11-12 20:09:12 +0300

    feat core: add utils::AtomicUpdate + tests (#1655)
    Tests: Протестировано юнит-тестами
    Relates: TAXICOMMON-3164

---
+++ shared/src/utils/atomic_test.cpp
@@ -0,0 +1,30 @@
+#include <utils/atomic.hpp>
+
+#include <gtest/gtest.h>
+
+TEST(AtomicTest, AtomicUpdate) {
+  std::atomic<int> atomic(10);
+
+  utils::AtomicUpdate(atomic, [](int x) { return x * 2 + 2; });
+  EXPECT_EQ(atomic.load(), 22);
+}
+
+TEST(AtomicTest, AtomicMin) {
+  std::atomic<int> atomic(10);
+
+  utils::AtomicMin(atomic, 5);
+  EXPECT_EQ(atomic.load(), 5);
+
+  utils::AtomicMin(atomic, 20);
+  EXPECT_EQ(atomic.load(), 5);
+}
+
+TEST(AtomicTest, AtomicMax) {
+  std::atomic<int> atomic(10);
+
+  utils::AtomicMax(atomic, 5);
+  EXPECT_EQ(atomic.load(), 10);
+
+  utils::AtomicMax(atomic, 20);
+  EXPECT_EQ(atomic.load(), 20);
+}
