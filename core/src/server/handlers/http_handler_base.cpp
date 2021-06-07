f5e658487e7008fa7fc6a28effc8dfaf74bedd33
Aliaksei Yarashou <aeroshov@yandex-team.ru>
2021-06-07 11:43:58 +0300

    feat server: add X-YaTraceId header to responses of all services (#2081)
    Relates: EDACOMMON-856

--- core/src/server/handlers/http_handler_base.cpp
+++ core/src/server/handlers/http_handler_base.cpp
@@ -357,6 +357,7 @@ void HttpHandlerBase::HandleRequest(request::RequestBase& request,
 
     auto span = tracing::Span::MakeSpan("http/" + HandlerName(), trace_id,
                                         parent_span_id);
+    response.SetHeader(::http::headers::kXYaTraceId, span.GetTraceId());
 
     span.SetLocalLogLevel(log_level_);
 
