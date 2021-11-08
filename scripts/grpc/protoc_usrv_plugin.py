2b7085946b5860ca8d3181c8c826b584cf30d136
Anton Zhilin <antonyzhilin@yandex-team.ru>
2021-11-08 19:58:38 +0300

    refactor grpc: clean up codegen (#2426)
    Tests: протестировано юнит-тестами
    Relates: TAXICOMMON-4263

--- scripts/grpc/protoc_usrv_plugin.py
+++ scripts/grpc/protoc_usrv_plugin.py
@@ -6,8 +6,8 @@ This script is a Protobuf protoc plugin that generates userver asynchronous
 wrappers for gRPC clients.
 
 For each `path/X.proto` file that contains gRPC services, we generate
-`path/X_client.usrv.pb.{hpp,cpp}` using
-`client.usrv.pb.{hpp,cpp}.jinja` templates.
+`path/X_{client,handler}.usrv.pb.{hpp,cpp}` using
+`{client,handler}.usrv.pb.{hpp,cpp}.jinja` templates.
 """
 
 import itertools
@@ -28,26 +28,7 @@ def _generate_code(jinja_env, proto_file, response):
 
     data = {
         'source_file': proto_file.name,
-        'includes_client_hpp': [
-            '<userver/clients/grpc/impl/client_data.hpp>',
-            '<userver/clients/grpc/rpc.hpp>',
-        ],
-        'includes_client_cpp': [
-            '"{}"'.format(
-                proto_file.name.replace('.proto', '_client.usrv.pb.hpp'),
-            ),
-        ],
-        'includes_handler_hpp': [
-            '<userver/server/grpc/reactor.hpp>',
-            '<userver/server/grpc/rpc.hpp>',
-        ],
-        'includes_handler_cpp': [
-            '"{}"'.format(
-                proto_file.name.replace('.proto', '_handler.usrv.pb.hpp'),
-            ),
-            '<userver/server/grpc/impl/listen_async.hpp>',
-            '<userver/server/grpc/impl/reactor_data.hpp>',
-        ],
+        'source_file_without_ext': proto_file.name.replace('.proto', ''),
         'generated_include': '"{}"'.format(
             proto_file.name.replace('.proto', '.grpc.pb.h'),
         ),
