e7da486ed974bc39ba9ccbee0b885a583b887b5f
Antony Polukhin <antoshkka@yandex-team.ru>
2021-12-15 01:05:11 +0300

    feat utils: support overaligned types in FixedArray (#2392)
    Tests: протестировано тестсьютом и локально
    Relates: TAXICOMMON-4630

--- shared/include/userver/utils/fixed_array.hpp
+++ shared/include/userver/utils/fixed_array.hpp
@@ -3,7 +3,9 @@
 /// @file userver/utils/fixed_array.hpp
 /// @brief @copybrief utils::FixedArray
 
-#include <memory>
+#include <memory>   // std::allocator
+#include <utility>  // std::swap
+
 #include <userver/utils/assert.hpp>
 
 USERVER_NAMESPACE_BEGIN
@@ -24,62 +26,85 @@ class FixedArray final {
   template <class... Args>
   explicit FixedArray(std::size_t size, Args&&... args);
 
-  ~FixedArray() { std::destroy(begin(), end()); }
+  FixedArray(FixedArray&& other) noexcept;
+  FixedArray& operator=(FixedArray&& other) noexcept;
+
+  FixedArray(const FixedArray&) = delete;
+  FixedArray& operator=(const FixedArray&) = delete;
+
+  ~FixedArray();
 
   std::size_t size() const noexcept { return size_; }
 
   const T& operator[](std::size_t i) const noexcept {
     UASSERT(i < size_);
-    return get()[i];
+    return data()[i];
   }
 
   T& operator[](std::size_t i) noexcept {
     UASSERT(i < size_);
-    return get()[i];
+    return data()[i];
   }
 
-  T& front() noexcept { return *get(); }
-  const T& front() const noexcept { return *get(); }
+  T& front() noexcept { return *data(); }
+  const T& front() const noexcept { return *data(); }
 
-  T& back() noexcept { return *(get() + size_ - 1); }
-  const T& back() const noexcept { return *(get() + size_ - 1); }
+  T& back() noexcept { return *(data() + size_ - 1); }
+  const T& back() const noexcept { return *(data() + size_ - 1); }
 
-  T* data() noexcept { return get(); }
-  const T* data() const noexcept { return get(); }
+  T* data() noexcept { return storage_; }
+  const T* data() const noexcept { return storage_; }
 
-  T* begin() noexcept { return get(); }
-  T* end() noexcept { return get() + size_; }
-  const T* begin() const noexcept { return get(); }
-  const T* end() const noexcept { return get() + size_; }
-  const T* cbegin() const noexcept { return get(); }
-  const T* cend() const noexcept { return get() + size_; }
+  T* begin() noexcept { return data(); }
+  T* end() noexcept { return data() + size_; }
+  const T* begin() const noexcept { return data(); }
+  const T* end() const noexcept { return data() + size_; }
+  const T* cbegin() const noexcept { return data(); }
+  const T* cend() const noexcept { return data() + size_; }
 
  private:
-  T* get() const noexcept { return reinterpret_cast<T*>(storage_.get()); }
-
-  std::unique_ptr<unsigned char[]> storage_;
+  T* storage_;
   std::size_t size_;
 };
 
 template <class T>
 template <class... Args>
 FixedArray<T>::FixedArray(std::size_t size, Args&&... args)
-    : storage_(new unsigned char[sizeof(T) * size]), size_(size) {
+    : storage_(std::allocator<T>{}.allocate(size)), size_(size) {
   UASSERT(size > 0);
   if (!size) {
     return;
   }
 
-  auto* begin = get();
+  auto* begin = data();
   try {
     for (auto* end = begin + size - 1; begin != end; ++begin) {
       new (begin) T(args...);
     }
     new (begin) T(std::forward<Args>(args)...);
   } catch (...) {
-    std::destroy(get(), begin);
+    std::destroy(data(), begin);
+    std::allocator<T>{}.deallocate(storage_, size);
     throw;
   }
+}
+
+template <class T>
+FixedArray<T>::FixedArray(FixedArray&& other) noexcept
+    : storage_(std::exchange(other.storage_, nullptr)),
+      size_(std::exchange(other.size_, 0)) {}
+
+template <class T>
+FixedArray<T>& FixedArray<T>::operator=(FixedArray&& other) noexcept {
+  std::swap(storage_, other.storage_);
+  std::swap(size_, other.size_);
+  return *this;
+}
+
+template <class T>
+FixedArray<T>::~FixedArray() {
+  std::destroy(begin(), end());
+  std::allocator<T>{}.deallocate(storage_, size_);
 }
 
 }  // namespace utils
--- shared/src/utils/fixed_array_test.cpp
+++ shared/src/utils/fixed_array_test.cpp
@@ -6,9 +6,9 @@ USERVER_NAMESPACE_BEGIN
 
 TEST(FixedArray, Sample) {
   /// [Sample FixedArray]
-  class NondefaultConstructible final {
+  class NonDefaultConstructible final {
    public:
-    explicit NondefaultConstructible(std::string str) : str_(std::move(str)){};
+    explicit NonDefaultConstructible(std::string str) : str_(std::move(str)) {}
 
     std::string GetString() const { return str_; }
 
@@ -16,7 +16,7 @@ TEST(FixedArray, Sample) {
     std::string str_;
   };
 
-  utils::FixedArray<NondefaultConstructible> array{42, "The string"};
+  utils::FixedArray<NonDefaultConstructible> array{42, "The string"};
   ASSERT_EQ(array.size(), 42);
   ASSERT_EQ(array[0].GetString(), "The string");
   ASSERT_EQ(array[14].GetString(), "The string");
@@ -31,6 +31,15 @@ TEST(FixedArray, Iteration) {
     sum += value;
   }
   ASSERT_EQ(sum, 8);
+}
+
+TEST(FixedArray, Overaligned) {
+  constexpr std::size_t align = alignof(std::max_align_t) * 8;
+  struct Overaligned {
+    alignas(align) char c;
+  };
+  utils::FixedArray<Overaligned> array(4);
+  ASSERT_TRUE((reinterpret_cast<std::uintptr_t>(array.data()) % align) == 0);
 }
 
 USERVER_NAMESPACE_END
