13033de6cadc3bda1f5ff10faca243bcdc0ebf15
Anton Zhilin <antonyzhilin@yandex-team.ru>
2022-01-22 18:47:12 +0300

    refactor utils: use userver/utils/rand.hpp for uuid generation (#2562)
    Relates: TAXICOMMON-4744

--- core/src/utils/boost_uuid4.cpp
+++ core/src/utils/boost_uuid4.cpp
@@ -1,23 +1,23 @@
 #include <userver/utils/boost_uuid4.hpp>
 
-#include <boost/random/mersenne_twister.hpp>
 #include <boost/uuid/random_generator.hpp>
 #include <boost/uuid/uuid.hpp>
 #include <boost/uuid/uuid_io.hpp>
+
+#include <userver/utils/rand.hpp>
 
 USERVER_NAMESPACE_BEGIN
 
 namespace utils::generators {
 
 boost::uuids::uuid GenerateBoostUuid() {
-  // Can not use std::mt19937_64 because of the different seed() signature
-  using RBGenerator = boost::random::mt19937_64;
-  thread_local boost::uuids::basic_random_generator<RBGenerator> generator;
-
+  thread_local boost::uuids::basic_random_generator<RandomBase> generator(
+      DefaultRandom());
   return generator();
 }
 
 namespace impl {
+
 std::string ToString(const boost::uuids::uuid& uuid) {
   return boost::uuids::to_string(uuid);
 }
