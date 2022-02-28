3b1d6efb23f744252d8a0f3506ea62f3201e3b04
Anton Zhilin <antonyzhilin@yandex-team.ru>
2022-02-28 14:02:57 +0300

    fix dynamic_config: add a legacy header for backwards compatibility
    Tests: на функциональность не влияет, собралось
    Relates: TAXICOMMON-4881

---
+++ core/include/userver/clients/config/client.hpp
@@ -0,0 +1,14 @@
+#pragma once
+
+#include <userver/dynamic_config/client/client.hpp>
+#include <userver/taxi_config/value.hpp>
+
+USERVER_NAMESPACE_BEGIN
+
+namespace clients::taxi_config {
+
+using Client = dynamic_config::Client;
+
+}  // namespace clients::taxi_config
+
+USERVER_NAMESPACE_END
