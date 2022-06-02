4d4737b5828d11ec84c55fa1ca871b12d2855adf
Antony Polukhin <antoshkka@yandex-team.ru>
2022-06-02 16:05:06 +0300

    better test

--- core/src/engine/subprocess/process_starter_test.cpp
+++ core/src/engine/subprocess/process_starter_test.cpp
@@ -1,9 +1,13 @@
 #include <userver/utest/utest.hpp>
 
+#include <unistd.h>
+
 #include <chrono>
 #include <string>
 #include <thread>
 #include <vector>
+
+#include <boost/dll/runtime_symbol_info.hpp>
 
 #include <engine/ev/thread_control.hpp>
 #include <engine/ev/thread_pool.hpp>
@@ -14,6 +18,7 @@
 #include <userver/fs/blocking/file_descriptor.hpp>
 #include <userver/fs/blocking/temp_file.hpp>
 #include <userver/utest/current_process_open_files.hpp>
+#include <userver/utils/text.hpp>
 #include <utils/check_syscall.hpp>
 
 USERVER_NAMESPACE_BEGIN
@@ -48,7 +53,7 @@ UTEST(Subprocess, False) {
 }
 
 UTEST(Subprocess, CheckSpdlogClosesFds) {
-  auto file = fs::blocking::TempFile::Create();
+  auto file = fs::blocking::TempFile::Create("/tmp", "spdlog_closeexec_test");
   auto logger = logging::MakeFileLogger("to_file", file.GetPath(),
                                         logging::Format::kTskv);
   LOG_ERROR_TO(logger) << "This must be logged";
@@ -70,13 +75,32 @@ UTEST(Subprocess, CheckSpdlogClosesFds) {
     EXPECT_EQ(return_code, 0);
   } else {
     // in child thread
-    const auto opened_files = utest::CurrentProcessOpenFiles();
-    const auto it =
-        std::find(opened_files.begin(), opened_files.end(), file.GetPath());
-    if (it != opened_files.end()) std::exit(1);
-    if (opened_files.size() > 4) std::exit(2);
-    std::exit(0);
+    auto self = boost::dll::program_location();
+    char* newargv[] = {
+        const_cast<char*>(self.c_str()),
+        const_cast<char*>("--gtest_also_run_disabled_tests"),
+        const_cast<char*>(
+            "--gtest_filter=Subprocess.DISABLED_CheckSpdlogClosesFdsFromChild"),
+        nullptr,
+    };
+    char* newenviron[] = {nullptr};
+    execve(self.c_str(), newargv, newenviron);
+    std::exit(3);
   }
+}
+
+TEST(Subprocess, DISABLED_CheckSpdlogClosesFdsFromChild) {
+  static const std::string kSpdlogFilePrefix = "/tmp/spdlog_closeexec_test";
+
+  const auto opened_files = utest::CurrentProcessOpenFiles();
+  for (const auto& file : opened_files) {
+    if (utils::text::StartsWith(file, kSpdlogFilePrefix)) {
+      std::exit(1);
+    }
+  }
+
+  if (opened_files.size() > 4) std::exit(2);
+  std::exit(0);
 }
 
 USERVER_NAMESPACE_END
