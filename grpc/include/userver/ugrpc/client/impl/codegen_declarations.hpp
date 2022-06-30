0a1284ed0fd60942f9461b85565b36d5ae6f3acd
Anton Zhilin <antonyzhilin@yandex-team.ru>
2022-06-30 09:41:40 +0000

    refactor grpc: collect codegen includes in special headers
    Relates: TAXICOMMON-5366

---
+++ grpc/include/userver/ugrpc/client/impl/codegen_declarations.hpp
@@ -0,0 +1,14 @@
+#pragma once
+
+// This header contains all static includes for code-generated
+// *_client.usrv.pb.hpp files.
+//
+// Do not include this header in your code, use non-impl includes instead!
+
+#include <grpcpp/completion_queue.h>
+
+#include <userver/ugrpc/client/impl/channel_cache.hpp>
+#include <userver/ugrpc/client/impl/client_data.hpp>
+#include <userver/ugrpc/client/rpc.hpp>
+#include <userver/ugrpc/impl/static_metadata.hpp>
+#include <userver/ugrpc/impl/statistics.hpp>
---
+++ grpc/include/userver/ugrpc/client/impl/codegen_definitions.hpp
@@ -0,0 +1,9 @@
+#pragma once
+
+// This header contains all static includes for code-generated
+// *_client.usrv.pb.cpp files.
+//
+// Do not include this header in your code, use non-impl includes instead!
+
+#include <string_view>
+#include <utility>
