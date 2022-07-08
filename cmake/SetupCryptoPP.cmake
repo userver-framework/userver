5bca5c5065d7853d06e8335e6868fb308f10b604
Antony Polukhin <antoshkka@yandex-team.ru>
2022-07-08 18:21:20 +0300

    feat build: add CryptoPP download into third_party
    Tests: протестировано локально и CI
    Relates:

---
+++ cmake/SetupCryptoPP.cmake
@@ -0,0 +1,38 @@
+option(USERVER_DOWNLOAD_PACKAGE_CRYPTOPP "Download and setup CryptoPP if no CryptoPP of matching version was found" ${USERVER_DOWNLOAD_PACKAGES})
+if (USERVER_DOWNLOAD_PACKAGE_CRYPTOPP)
+    find_package(CryptoPP)
+else()
+    find_package(CryptoPP REQUIRED)
+endif()
+
+if (CryptoPP_FOUND)
+    return()
+endif()
+
+include(FetchContent)
+FetchContent_Declare(
+  fmt_external_project
+  GIT_REPOSITORY https://github.com/weidai11/cryptopp.git
+  TIMEOUT 10
+  GIT_TAG CRYPTOPP_8_6_0
+  SOURCE_DIR ${USERVER_ROOT_DIR}/third_party/cryptopp
+)
+
+FetchContent_GetProperties(fmt_external_project)
+if (NOT fmt_external_project_POPULATED)
+  message(STATUS "Downloading CryptoPP from remote")
+  FetchContent_Populate(fmt_external_project)
+
+  file(DOWNLOAD
+    https://raw.githubusercontent.com/noloader/cryptopp-cmake/master/CMakeLists.txt
+    ${USERVER_ROOT_DIR}/third_party/cryptopp/CMakeLists.txt
+  )
+endif()
+
+set(cryptopp_DISPLAY_CMAKE_SUPPORT_WARNING OFF CACHE BOOL "")
+set(BUILD_SHARED OFF CACHE BOOL "")
+set(BUILD_TESTING OFF CACHE BOOL "")
+set(USE_INTERMEDIATE_OBJECTS_TARGET OFF CACHE BOOL "")
+set(USE_OPENMP OFF CACHE BOOL "")
+add_subdirectory(${USERVER_ROOT_DIR}/third_party/cryptopp "${CMAKE_BINARY_DIR}/third_party/cryptopp")
+set(CryptoPP_VERSION "8.6.0" CACHE STRING "Version of the CryptoPP")
