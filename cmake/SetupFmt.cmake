be8ee045497c6c37fcc1db182c0c9ad72054cd59
Anton Zhilin <antonyzhilin@yandex-team.ru>
2022-07-13 13:44:00 +0300

    fix cmake: minor fixes for open-source cmake scripts
    - Download fmt 8.0, because it's required for the downloaded spdlog version
    - Use OSS protoc in OSS build
    - Unify jemalloc handling between core and universal
    
    Relates: TAXICOMMON-NNNN

--- cmake/SetupFmt.cmake
+++ cmake/SetupFmt.cmake
@@ -1,6 +1,6 @@
 option(USERVER_DOWNLOAD_PACKAGE_FMT "Download and setup Fmt if no Fmt of matching version was found" ${USERVER_DOWNLOAD_PACKAGES})
 if (USERVER_DOWNLOAD_PACKAGE_FMT)
-    find_package(fmt "7.1")
+    find_package(fmt "8.0")
 else()
     find_package(fmt "7.1" REQUIRED)
 endif()
