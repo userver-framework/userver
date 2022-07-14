83e8acf997f000a7d190f3f7bac2d831ec4d4e5a
Anton Zhilin <antonyzhilin@yandex-team.ru>
2022-07-14 11:00:29 +0000

    fix cmake: minor fixes for open-source cmake scripts
    - Download fmt 8.0, because it's required for the downloaded spdlog version
    - Use OSS protoc in OSS build
    - Use OSS jemalloc in OSS build
    - Silence CMP69 warnings from downloaded libs
    
    Relates: TAXICOMMON-5435

--- cmake/SetupCAres.cmake
+++ cmake/SetupCAres.cmake
@@ -1,3 +1,5 @@
+set(CMAKE_POLICY_DEFAULT_CMP0069 NEW)
+
 if (NOT USERVER_OPEN_SOURCE_BUILD)
     find_package_required(yandex-c-ares "libyandex-taxi-c-ares-dev")
     add_library(c-ares ALIAS yandex-c-ares)  # Unify link names
