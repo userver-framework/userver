5d14eb6ac0d0a7f0f1a03ff2ed5f2b6c35f3e756
Ivan Trofimov <i.trofimow@yandex.ru>
2022-07-30 20:56:43 +0300

    break hello_service

--- core/src/engine/sleep.cpp
+++ core/src/engine/sleep.cpp
@@ -33,7 +33,7 @@ void SleepUntil(Deadline deadline) {
   InterruptibleSleepUntil(deadline);
 }
 
-void Yield() { SleepUntil(Deadline::Passed()); }
+void Yield() { SleepUntil(Deadline::FromDuration(std::chrono::milliseconds{1})); }
 
 }  // namespace engine
 
--- samples/hello_service/hello_service.cpp
+++ samples/hello_service/hello_service.cpp
@@ -4,6 +4,10 @@
 #include <userver/components/minimal_server_component_list.hpp>
 #include <userver/server/handlers/http_handler_base.hpp>
 #include <userver/utils/daemon_run.hpp>
+#include <userver/engine/sleep.hpp>
+#include <userver/utils/periodic_task.hpp>
+
+#include <thread>
 
 namespace samples::hello {
 
@@ -12,14 +16,31 @@ class Hello final : public server::handlers::HttpHandlerBase {
   // `kName` is used as the component name in static config
   static constexpr std::string_view kName = "handler-hello-sample";
 
-  // Component is valid after construction and is able to accept requests
-  using HttpHandlerBase::HttpHandlerBase;
+  Hello(const components::ComponentConfig& config,
+        const components::ComponentContext& context)
+      : server::handlers::HttpHandlerBase{config, context},
+        background_{engine::AsyncNoSpan([] {
+          while (!engine::current_task::ShouldCancel()) {
+            std::this_thread::sleep_for(std::chrono::milliseconds{2});
+            engine::Yield();
+          }
+        })} {
+
+  }
+
+  ~Hello() override {
+    background_.SyncCancel();
+  }
 
   std::string HandleRequestThrow(
       const server::http::HttpRequest&,
       server::request::RequestContext&) const override {
+    std::this_thread::sleep_for(std::chrono::milliseconds{10});
     return "Hello world!\n";
   }
+
+ private:
+  engine::TaskWithResult<void> background_;
 };
 
 }  // namespace samples::hello
