f1421790a6fa2b6ad9f53990bd8879fa2a478ea6
Anton Zhilin <antonyzhilin@yandex-team.ru>
2022-08-09 17:55:31 +0300

    Fix?

--- core/src/engine/impl/wait_list_light.cpp
+++ core/src/engine/impl/wait_list_light.cpp
@@ -23,17 +23,17 @@ static_assert(boost::atomic<Waiter>::is_always_lock_free);
 
 Waiter AtomicLoad(boost::atomic<Waiter>& waiter,
                   boost::memory_order order) noexcept {
-  if constexpr (sizeof(void*) == 8 && BOOST_VERSION < 106900) {
-    // boost::atomic<Waiter>::load fails to compile on older Boost versions
-    // https://github.com/boostorg/atomic/issues/15
-    Waiter expected{reinterpret_cast<TaskContext*>(1), 0};
-    const bool success = waiter.compare_exchange_strong(
-        expected, expected, boost::memory_order_relaxed, order);
-    UASSERT(!success);
-    return expected;
-  } else {
-    return waiter.load(order);
-  }
+#if BOOST_VERSION < 106900
+  // boost::atomic<Waiter>::load fails to compile on older Boost versions
+  // https://github.com/boostorg/atomic/issues/15
+  Waiter expected{reinterpret_cast<TaskContext*>(1), 0};
+  const bool success = waiter.compare_exchange_strong(
+      expected, expected, boost::memory_order_relaxed, order);
+  UASSERT(!success);
+  return expected;
+#else
+  return waiter.load(order);
+#endif
 }
 
 }  // namespace
