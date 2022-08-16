123af1b1c6ad4d27284368a2a60c6856d360567f
Anton Zhilin <antonyzhilin@yandex-team.ru>
2022-08-16 23:58:15 +0300

    Fix

--- cmake/RequireDWCAS.cmake
+++ cmake/RequireDWCAS.cmake
@@ -1,7 +1,7 @@
 include(CheckCXXCompilerFlag)
-check_cxx_compiler_flag("-mcx16" HAS_MCX16)
+check_cxx_compiler_flag("-mcx16" HAS_mcx16)
 
-if(HAS_MCX16)
+if(HAS_mcx16)
   set(MCX16_ARG "-mcx16")
 else()
   set(MCX16_ARG "")
@@ -13,8 +13,13 @@ try_run(
   "${CMAKE_SOURCE_DIR}/cmake/RequireDWCAS.cpp"
   CMAKE_FLAGS "-DINCLUDE_DIRECTORIES=${Boost_INCLUDE_DIR}"
   COMPILE_DEFINITIONS "-std=c++17" ${MCX16_ARG}
+  COMPILE_OUTPUT_VARIABLE COMPILE_OUTPUT
 )
 
-if(NOT "${RUN_RESULT}" STREQUAL "0")
+if(NOT "${COMPILE_RESULT}")
+  message(SEND_ERROR "Error while compiling DWCAS test: ${COMPILE_OUTPUT}")
+elseif(NOT "${RUN_RESULT}" STREQUAL "0")
   message(SEND_ERROR "We don't know how to make DWCAS work on this platform")
+else()
+  message(STATUS "DWCAS works")
 endif()
