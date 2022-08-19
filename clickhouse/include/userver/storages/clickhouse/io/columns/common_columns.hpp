edfbaef032a676f2faa94541e06c5412308b3a25
Antony Polukhin <antoshkka@yandex-team.ru>
2022-08-19 12:45:43 +0000

    feat clickhouse: add int8 support
    Int8 support helps to insert integral values (1/0) instead of boolean.
    Using UInt8 value or other numeric values insted of Int8 doesnt' work :(
    That can be used as a kind of aworkaround because the original contrib
    library - 'clickhouse-cpp' doesn't support 'Boolean' at all.
    
    Author: Vasily Shuman @basil-shuman
    Tests: протестировано CI
    Relates: CCTV-222

--- clickhouse/include/userver/storages/clickhouse/io/columns/common_columns.hpp
+++ clickhouse/include/userver/storages/clickhouse/io/columns/common_columns.hpp
@@ -9,6 +9,7 @@
 #include <userver/storages/clickhouse/io/columns/float64_column.hpp>
 #include <userver/storages/clickhouse/io/columns/int32_column.hpp>
 #include <userver/storages/clickhouse/io/columns/int64_column.hpp>
+#include <userver/storages/clickhouse/io/columns/int8_column.hpp>
 #include <userver/storages/clickhouse/io/columns/string_column.hpp>
 #include <userver/storages/clickhouse/io/columns/uint32_column.hpp>
 #include <userver/storages/clickhouse/io/columns/uint64_column.hpp>
