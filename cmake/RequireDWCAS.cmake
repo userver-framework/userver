444fe5bd69c72d5e42b0e488ba957f4cf09c09c3
Anton Zhilin <antonyzhilin@yandex-team.ru>
2022-09-02 15:09:28 +0000

    fix cmake: fix build on Clang Boost 1.67
    Relates: TAXICOMMON-5613

--- cmake/RequireDWCAS.cmake
+++ cmake/RequireDWCAS.cmake
@@ -1,15 +1,43 @@
 option(USERVER_FEATURE_DWCAS "Require double-width compare-exchange-swap" ON)
 
+if(MACOS)
+  # All CPUs, which can run macOS, provide a DWCAS instruction.
+  # On the other hand, emulation via libatomic is not accessible there.
+  set(USERVER_FEATURE_DWCAS ON)
+endif()
+
 if(NOT USERVER_FEATURE_DWCAS)
-  add_compile_definitions(BOOST_ATOMIC_NO_CMPXCHG16B=1)
   message(STATUS "DWCAS disabled")
   return()
 endif()
 
+set(BOOST_CMAKE_VERSION "${Boost_MAJOR_VERSION}.${Boost_MINOR_VERSION}.${Boost_SUBMINOR_VERSION}")
+set(TEST_DEFINITIONS)
+set(TEST_LIBRARIES)
+
+if(NOT CMAKE_CXX_COMPILER_ID MATCHES "Clang" AND "${BOOST_CMAKE_VERSION}" VERSION_GREATER_EQUAL "1.66")
+  # Clang's std::atomic already emits DWCAS instructions for x86,
+  # x86_64 and armv8-a (a.k.a. ARM64) architectures (both libstdc++ and libc++).
+  #
+  # GCC's std::atomic falls back to libatomic:
+  # https://gcc.gnu.org/bugzilla/show_bug.cgi?id=80878
+  # https://gcc.gnu.org/bugzilla/show_bug.cgi?id=84522
+  # Boost.Atomic 1.66+ produces correct DWCAS instructions on x86, x86_64
+  # and ARM64 (Boost 1.74+) architectures.
+  add_compile_definitions(USERVER_USE_BOOST_DWCAS=1)
+  list(APPEND TEST_DEFINITIONS "-DUSERVER_USE_BOOST_DWCAS=1")
+endif()
+
 include(CheckCXXCompilerFlag)
-check_cxx_compiler_flag("-Wunused-command-line-argument -Werror -mcx16" HAS_mcx16)
+if(CMAKE_CXX_COMPILER_ID MATCHES "Clang")
+  check_cxx_compiler_flag("-Wunused-command-line-argument -Werror -mcx16" HAS_mcx16)
+else()
+  check_cxx_compiler_flag("-Werror -mcx16" HAS_mcx16)
+endif()
+
 if(HAS_mcx16)
   add_compile_options("-mcx16")
+  list(APPEND TEST_DEFINITIONS "-mcx16")
 endif()
 
 if(USERVER_IMPL_DWCAS_CHECKED)
@@ -17,18 +45,8 @@ if(USERVER_IMPL_DWCAS_CHECKED)
   return()
 endif()
 
-if(HAS_mcx16)
-  set(MCX16_ARG "-mcx16")
-else()
-  set(MCX16_ARG "")
-endif()
-
-set(BOOST_CMAKE_VERSION "${Boost_MAJOR_VERSION}.${Boost_MINOR_VERSION}.${Boost_SUBMINOR_VERSION}")
-
-if(CMAKE_CXX_COMPILER_ID STREQUAL "GNU" AND "${BOOST_CMAKE_VERSION}" VERSION_LESS "1.66")
-  set(TEST_LIBRARIES atomic)
-else()
-  set(TEST_LIBRARIES)
+if(NOT MACOS)
+  list(APPEND TEST_LIBRARIES "atomic")
 endif()
 
 # Make try_run honor parent CMAKE_CXX_STANDARD
@@ -39,13 +57,15 @@ try_run(
   "${CMAKE_CURRENT_BINARY_DIR}/require_dwcas"
   "${USERVER_ROOT_DIR}/cmake/RequireDWCAS.cpp"
   CMAKE_FLAGS "-DINCLUDE_DIRECTORIES=${Boost_INCLUDE_DIR}"
-  COMPILE_DEFINITIONS ${MCX16_ARG}
+  COMPILE_DEFINITIONS ${TEST_DEFINITIONS}
   LINK_LIBRARIES ${TEST_LIBRARIES}
   COMPILE_OUTPUT_VARIABLE COMPILE_OUTPUT
 )
 
 if(NOT "${COMPILE_RESULT}")
-  message(SEND_ERROR "Error while compiling DWCAS test: ${COMPILE_OUTPUT}")
+  message(FATAL_ERROR "Error while compiling DWCAS test: ${COMPILE_OUTPUT}")
+elseif("${RUN_RESULT}" STREQUAL "1")
+  message(WARNING "DWCAS seems not to work on the host platform, userver may have suboptimal performance")
 elseif(NOT "${RUN_RESULT}" STREQUAL "0")
   message(WARNING "DWCAS does not work on the host platform, userver may crash with SIGILL")
 else()
--- cmake/RequireDWCAS.cpp
+++ cmake/RequireDWCAS.cpp
@@ -1,40 +1,38 @@
+#include <atomic>
 #include <cstdint>
-#include <cstdlib>
 
+#ifdef USERVER_USE_BOOST_DWCAS
 #include <boost/atomic/atomic.hpp>
-#include <boost/version.hpp>
+#endif
 
 struct alignas(sizeof(std::uintptr_t) * 2) A final {
   std::uintptr_t x{};
   std::uintptr_t y{};
 };
 
-extern boost::atomic<A> a;
-boost::atomic<A> a{A{1, 2}};
-
-#if defined(__GNUC__) && !defined(__clang__) && defined(__x86_64__) && \
-    BOOST_VERSION < 106600
-// Older Boost.Atomic fails to provide DWCAS on x86_64 GCC and goes to
-// libatomic, given -mcx16. At the same time, is_lock_free always returns
-// 'false' there (incorrectly). We can only hope that libatomic correctly picks
-// a lock-free implementation at runtime. Last time we checked, it did.
-
-// NOLINTNEXTLINE(cppcoreguidelines-macro-usage)
-#define USERVER_IMPL_DWCAS_WORKAROUND
+#ifdef USERVER_USE_BOOST_DWCAS
+template <typename T>
+using Atomic = boost::atomic<T>;
+#else
+template <typename T>
+using Atomic = std::atomic<T>;
 #endif
+
+extern Atomic<A> a;
+Atomic<A> a{A{1, 2}};
 
 int main() {
-#ifndef USERVER_IMPL_DWCAS_WORKAROUND
-  if (!a.is_lock_free()) return EXIT_FAILURE;
-#endif
+  // Clang reports is_always_lock_free == true (which is correct),
+  // but is_lock_free == false (which makes no sense).
+  if (!Atomic<A>::is_always_lock_free && !a.is_lock_free()) return 1;
 
   A expected{1, 2};
   A desired{3, 4};
-  if (!a.compare_exchange_strong(expected, desired)) return EXIT_FAILURE;
+  if (!a.compare_exchange_strong(expected, desired)) return 2;
 
   expected = {3, 4};
   desired = {5, 6};
-  if (!a.compare_exchange_strong(expected, desired)) return EXIT_FAILURE;
+  if (!a.compare_exchange_strong(expected, desired)) return 2;
 
-  return EXIT_SUCCESS;
+  return 0;
 }
