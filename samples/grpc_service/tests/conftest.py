1d8f10ef86a127b649be6c2d155d044682ca7ba8
Victor Makarov <vitja@yandex-team.ru>
2022-09-20 17:42:21 +0000

    feat testsuite: grpc support
    Relates: TESTSUITE-86

--- samples/grpc_service/tests/conftest.py
+++ samples/grpc_service/tests/conftest.py
@@ -1,1 +1,29 @@
-pytest_plugins = ['pytest_userver.plugins', 'pytest_userver.plugins.samples']
+import sys
+
+import grpc
+import pytest
+
+pytest_plugins = [
+    'pytest_userver.plugins',
+    'pytest_userver.plugins.samples',
+    'pytest_userver.plugins.grpc',
+]
+
+
+@pytest.fixture(scope='session')
+def greeter_protos():
+    return grpc.protos('greeter.proto')
+
+
+@pytest.fixture(scope='session')
+def greeter_services():
+    return grpc.services('greeter.proto')
+
+
+@pytest.fixture
+def grpc_service(grpc_channel, greeter_services, service_client):
+    return greeter_services.GreeterServiceStub(grpc_channel)
+
+
+def pytest_configure(config):
+    sys.path.append(str(config.rootpath / 'grpc_service/proto/samples'))
--- samples/grpc_service/tests/test_grpc.py
+++ samples/grpc_service/tests/test_grpc.py
@@ -1,3 +1,9 @@
+async def test_say_hello(greeter_protos, grpc_service):
+    request = greeter_protos.GreetingRequest(name='Python')
+    response = await grpc_service.SayHello(request)
+    assert response.greeting == 'Hello, Python!'
+
+
 async def test_grpc(service_client):
     response = await service_client.post(
         '/hello', data='tests', headers={'Content-type': 'text/plain'},
