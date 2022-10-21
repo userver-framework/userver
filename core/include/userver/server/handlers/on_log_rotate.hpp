2cfeee7470bb01134425bc96f04a61374ada4ba3
dyusudakov <dyusudakov@userver.tech>
2022-10-21 15:03:06 +0300

    feat docs: Add OnLogRotate docs

--- core/include/userver/server/handlers/on_log_rotate.hpp
+++ core/include/userver/server/handlers/on_log_rotate.hpp
@@ -1,3 +1,8 @@
+#pragma once
+
+/// @file userver/server/handlers/on_log_rotate.hpp
+/// @brief @copybrief server::handlers::OnLogRotate
+
 #include <userver/server/handlers/http_handler_base.hpp>
 
 USERVER_NAMESPACE_BEGIN
@@ -7,6 +12,26 @@ class Logging;
 }  // namespace components
 
 namespace server::handlers {
+// clang-format off
+
+/// @ingroup userver_components userver_http_handlers
+///
+/// @brief Handler that controlls logging levels of all the loggers.
+///
+/// The component has no service configuration except the
+/// @ref userver_http_handlers "common handler options".
+///
+/// ## Static configuration example:
+///
+/// @snippet components/common_server_component_list_test.cpp  Sample handler log level component config
+///
+/// ## Scheme
+/// POST request reopens log file for all loggers.
+/// Returns 200 status code after successful operation. 
+/// If at least one of files was not successfully reopened returns 500 status code and 
+/// error messages separated by comma in response body
+
+// clang-format on
 class OnLogRotate final : public HttpHandlerBase {
  public:
   OnLogRotate(const components::ComponentConfig& config,
