3aab1b9e8198d3e562c1dcecfddfd3f09d17052c
Ivan Trofimov <i.trofimow@yandex.ru>
2022-10-25 15:44:54 +0000

    feat core: further reduce recv count in http processing
    Continuation of https://github.com/userver-framework/userver/pull/151
    
    strace for ~150k requests at current develop:
    ```
    % time     seconds  usecs/call     calls    errors syscall
    ------ ----------- ----------- --------- --------- ----------------
     36.91   12.514873          53    232364           epoll_wait
     14.37    4.870545          16    299806    149896 recvfrom
     13.89    4.710287          31    149892           writev
     13.38    4.535186          17    261423     36011 epoll_ctl
     10.53    3.569648          34    104391      8320 futex
      5.71    1.935202          20     94390           write
      4.83    1.638761          17     94394           read
    ```
    
    strace for ~150k requests with the patch:
    ```
    % time     seconds  usecs/call     calls    errors syscall
    ------ ----------- ----------- --------- --------- ----------------
     33.15   10.125214          48    210911           epoll_wait
     16.73    5.109284          34    149900           writev
     16.27    4.969470          19    256595     36838 epoll_ctl
     15.87    4.847892          39    122595      9407 futex
      9.29    2.836485          18    149922         6 recvfrom
      4.44    1.356292          20     66796           write
      4.06    1.241195          18     66826           read
    ```
    
    It can be seen that `recvfrom` count halved and its errors went to basically zero, which proves the motivation behind `kOnce`
    
    With my setup i'm seeing 2-4% improvements when bombarding `hello_service` with this patch
    
    Tests: протестировано CI
    
    Pull Request resolved: #152

--- core/src/engine/io/tls_wrapper.cpp
+++ core/src/engine/io/tls_wrapper.cpp
@@ -464,8 +464,7 @@ bool TlsWrapper::WaitWriteable(Deadline deadline) {
 
 size_t TlsWrapper::RecvSome(void* buf, size_t len, Deadline deadline) {
   impl_->CheckAlive();
-  return impl_->PerformSslIo(&SSL_read_ex, buf, len,
-                             impl::TransferMode::kPartial,
+  return impl_->PerformSslIo(&SSL_read_ex, buf, len, impl::TransferMode::kOnce,
                              InterruptAction::kPass, deadline, "RecvSome");
 }
 
