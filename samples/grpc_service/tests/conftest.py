0d4b93db9c22a22df229977e7e5acf23ea722338
vitja <vitja@yandex-team.com>
2022-10-29 14:35:01 +0300

    feat testsuite: grpc mockserver support

--- samples/grpc_service/tests/conftest.py
+++ samples/grpc_service/tests/conftest.py
@@ -3,10 +3,12 @@ import sys
 import grpc
 import pytest
 
+USERVER_CONFIG_HOOKS = ['_prepare_service_config']
 pytest_plugins = [
     'pytest_userver.plugins',
     'pytest_userver.plugins.samples',
     'pytest_userver.plugins.grpc',
+    'pytest_userver.plugins.grpc_mockserver',
 ]
 
 
@@ -23,6 +25,32 @@ def greeter_services():
 @pytest.fixture
 def grpc_service(grpc_channel, greeter_services, service_client):
     return greeter_services.GreeterServiceStub(grpc_channel)
+
+
+@pytest.fixture(scope='session')
+def mock_grpc_greeter_session(
+        greeter_services, grpc_mockserver, create_grpc_mock,
+):
+    mock = create_grpc_mock(greeter_services.GreeterServiceServicer)
+    greeter_services.add_GreeterServiceServicer_to_server(
+        mock.servicer, grpc_mockserver,
+    )
+    return mock
+
+
+@pytest.fixture
+def mock_grpc_greeter(mock_grpc_greeter_session):
+    with mock_grpc_greeter_session.mock() as mock:
+        yield mock
+
+
+@pytest.fixture(scope='session')
+def _prepare_service_config(grpc_mockserver_endpoint):
+    def patch_config(config, config_vars):
+        components = config['components_manager']['components']
+        components['greeter-client']['endpoint'] = grpc_mockserver_endpoint
+
+    return patch_config
 
 
 def pytest_configure(config):
