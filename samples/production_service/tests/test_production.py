e12e6a6b5325d79a88127b8c4da3e7cbaad77c22
antonyzhilin <antonyzhilin@userver.tech>
2022-11-10 21:31:55 +0300

    bug statistics: restore old httpclient metrics

--- samples/production_service/tests/test_production.py
+++ samples/production_service/tests/test_production.py
@@ -1,8 +1,3 @@
 async def test_monitor(monitor_client):
     response = await monitor_client.get('/service/log-level/')
     assert response.status == 200
-
-
-async def test_metrics_portability(service_client):
-    warnings = await service_client.metrics_portability()
-    assert not warnings, warnings
