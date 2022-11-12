3075326f5de883cf13982439976a92d92938ac0c
Airat Zalialiev <zalyaliev.air@yandex.ru>
2022-11-12 18:11:58 +0300

    template in userver side for etcd component is added

---
+++ etcd/src/storages/etcd/client.cpp
@@ -0,0 +1,15 @@
+#include <userver/storages/etcd/client.hpp>
+#include <stdexcept>
+
+USERVER_NAMESPACE_BEGIN
+
+namespace storages::etcd {
+
+  void Client::Remove(const std::string& /*key*/)
+  {
+    throw std::runtime_error("Not implemented");
+  }
+
+}  // namespace storages::etcd
+
+USERVER_NAMESPACE_END
