58deec032a56b21893625fdc49f0c8dc63252aa3
Ruslan <ruslankhakimov67@gmail.com>
2022-11-15 01:45:33 +0300

    add: more tests for bloom

--- shared/src/cache/bloom_test.cpp
+++ shared/src/cache/bloom_test.cpp
@@ -6,9 +6,36 @@
 
 USERVER_NAMESPACE_BEGIN
 
+using cache::impl::FrequencySketch;
+
+TEST(Bloom, Reset) {
+  FrequencySketch<int, cache::FrequencySketchPolicy::Bloom> bloom(64);
+
+  bool was_reset = false;
+  for (int i = 0; i <= 1000 * 64; i++) {
+    bloom.RecordAccess(i);
+    if (bloom.Size() != i) {
+      was_reset = true;
+      break;
+    }
+  }
+
+  EXPECT_TRUE(was_reset);
+}
+
+TEST(Bloom, Full) {
+  FrequencySketch<int, cache::FrequencySketchPolicy::Bloom> bloom(16, 1000);
+
+  for (int i = 0; i < 100'000; i++)
+    bloom.RecordAccess(i);
+
+  for (int i = 0; i < 100'000; i++)
+    EXPECT_EQ(bloom.GetFrequency(i), 15);
+}
+
 TEST(Bloom, HeavyHitters) {
-  cache::impl::FrequencySketch<double, cache::FrequencySketchPolicy::Bloom>bloom(512);
-  for (int i = 100; i < 100000; i++) {
+  FrequencySketch<double, cache::FrequencySketchPolicy::Bloom> bloom(512);
+  for (int i = 100; i < 100'000; i++) {
     bloom.RecordAccess(static_cast<double>(i));
   }
   for (int i = 0; i < 10; i += 2) {
