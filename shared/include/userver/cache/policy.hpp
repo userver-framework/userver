5f613afff50db2bcd1e7866341d18fc11ecc7645
Ruslan <ruslankhakimov67@gmail.com>
2022-11-15 18:00:37 +0300

    refactor:

--- shared/include/userver/cache/policy.hpp
+++ shared/include/userver/cache/policy.hpp
@@ -5,14 +5,17 @@ USERVER_NAMESPACE_BEGIN
 namespace cache {
 enum CachePolicy {
   kLRU = 0,
+  kSLRU,
   kLFU,
   kTinyLFU,
   kWTinyLFU
 };
+// TODO: DoorkeeperBloom
 enum FrequencySketchPolicy {
   Trivial = 0,
   Bloom,
+  DoorkeeperBloom
 };
-}
+} // namespace cache
 
 USERVER_NAMESPACE_END
