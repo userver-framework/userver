102a24566c88076ab068852739861b4ed7b7e1ad
Ruslan <ruslankhakimov67@gmail.com>
2022-11-15 12:17:22 +0300

    add: draft tiny_lfu

--- shared/include/userver/cache/impl/tiny_lfu.hpp
+++ shared/include/userver/cache/impl/tiny_lfu.hpp
@@ -1,7 +1,8 @@
 #pragma once
 
 #include <userver/cache/policy.hpp>
-#include <userver/cache/impl/base.hpp>
+#include <userver/cache/impl/lru.hpp>
+#include <userver/cache/impl/frequency_sketch.hpp>
 
 USERVER_NAMESPACE_BEGIN
 
@@ -10,10 +11,40 @@ namespace cache::impl {
 template <typename T, typename U, typename Hash, typename Equal>
 class LruBase<T, U, Hash, Equal, CachePolicy::kTinyLFU> {
  public:
-  explicit LruBase(size_t max_size, const Hash hash, const Equal& equal);
+  explicit LruBase(size_t max_size, const Hash& hash, const Equal& equal);
+
+  LruBase(LruBase&& other) noexcept = default;
+  LruBase& operator=(LruBase&& other) noexcept = default;
+  LruBase(const LruBase& lru) = delete;
+  LruBase& operator=(const LruBase& lru) = delete;
+
+  bool Put(const T& key, U value);
+  template <typename... Args>
+  U* Emplace(const T&, Args&&... args);
+  void Erase(const T& key);
+  U* Get(const T& key);
+  const T* GetLeastUsedKey();
+  U* GetLeastUsedValue();
+  void SetMaxSize(size_t new_max_size);
+  void Clear() noexcept;
+  template <typename Function>
+  void VisitAll(Function&& func) const;
+  template <typename Function>
+  void VisitAll(Function&& func);
+  size_t GetSize() const;
+
  private:
+  FrequencySketch<T, FrequencySketchPolicy::Bloom> proxy_;
   LruBase<T, U, Hash, Equal, CachePolicy::kLRU> main_;
 };
+
+template <typename T, typename U, typename Hash, typename Equal>
+LruBase<T, U, Hash, Equal, CachePolicy::kTinyLFU>::LruBase(size_t max_size,
+                                                           const Hash& hash,
+                                                           const Equal& equal)
+    : main_(max_size, hash, equal),
+      proxy_(max_size) {
+}
 
 }
 
