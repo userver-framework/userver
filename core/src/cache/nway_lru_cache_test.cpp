7dc4075e9c8cb23a40862c5a39b50ca017da54fc
Ruslan <ruslankhakimov67@gmail.com>
2022-11-17 15:33:01 +0300

    add: parametrized NWayLRU tests

--- core/src/cache/nway_lru_cache_test.cpp
+++ core/src/cache/nway_lru_cache_test.cpp
@@ -1,18 +1,32 @@
 #include <userver/utest/utest.hpp>
 
+#include <type_traits>
+
 #include <userver/cache/nway_lru_cache.hpp>
 
 USERVER_NAMESPACE_BEGIN
 
-using Cache = cache::NWayLRU<int, int>;
+template <typename T>
+class NWayLRU : public ::testing::Test {};
 
-UTEST(NWayLRU, Ctr) {
+using policy_types = ::testing::Types<
+    std::integral_constant<cache::CachePolicy, cache::CachePolicy::kLRU>,
+    std::integral_constant<cache::CachePolicy, cache::CachePolicy::kSLRU>,
+    std::integral_constant<cache::CachePolicy, cache::CachePolicy::kTinyLFU>>;
+
+TYPED_UTEST_SUITE(NWayLRU, policy_types);
+
+TYPED_UTEST(NWayLRU, Ctr) {
+  using Cache = cache::NWayLRU<int, int, std::hash<int>, std::equal_to<int>,
+                               TypeParam::value>;
   UEXPECT_NO_THROW(Cache(1, 10));
   UEXPECT_NO_THROW(Cache(10, 10));
   UEXPECT_THROW(Cache(0, 10), std::logic_error);
 }
 
-UTEST(NWayLRU, Set) {
+TYPED_UTEST(NWayLRU, Set) {
+  using Cache = cache::NWayLRU<int, int, std::hash<int>, std::equal_to<int>,
+                               TypeParam::value>;
   Cache cache(1, 1);
   EXPECT_EQ(0, cache.GetSize());
 
@@ -20,18 +34,23 @@ UTEST(NWayLRU, Set) {
   EXPECT_EQ(1, cache.GetSize());
 
   cache.Put(2, 2);
+  if constexpr (TypeParam::value == cache::CachePolicy::kTinyLFU)
+    cache.Put(2, 2);
 
   EXPECT_EQ(2, cache.Get(2));
   EXPECT_EQ(1, cache.GetSize());
   EXPECT_FALSE(cache.Get(1).has_value());
 }
 
-UTEST(NWayLRU, GetExpired) {
+TYPED_UTEST(NWayLRU, GetExpired) {
+  using Cache = cache::NWayLRU<int, int, std::hash<int>, std::equal_to<int>,
+                               TypeParam::value>;
   Cache cache(1, 2);
   cache.Put(1, 1);
   cache.Put(2, 2);
 
   EXPECT_EQ(1, cache.Get(1));
+  EXPECT_EQ(2, cache.Get(2));
   EXPECT_EQ(2, cache.GetSize());
 
   EXPECT_FALSE(cache.Get(1, [](int) { return false; }).has_value());
@@ -44,7 +63,9 @@ UTEST(NWayLRU, GetExpired) {
   EXPECT_EQ(0, cache.GetSize());
 }
 
-UTEST(NWayLRU, SetMultipleWays) {
+TYPED_UTEST(NWayLRU, SetMultipleWays) {
+  using Cache = cache::NWayLRU<int, int, std::hash<int>, std::equal_to<int>,
+                               TypeParam::value>;
   Cache cache(2, 1);
   cache.Put(1, 1);
   cache.Put(2, 2);
