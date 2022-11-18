8ad96156a579a223dbaf46706b282d15e86c3b96
Alexandr Zaykov <alexzaykov@gmail.com>
2022-11-18 14:30:59 +0300

    etcd client based on grpc_client
    Related #10

--- etcd/src/storages/etcd/client_impl.cpp
+++ etcd/src/storages/etcd/client_impl.cpp
@@ -1,19 +1,26 @@
 #include "client_impl.hpp"
+#include <etcd/api/etcdserverpb/rpc.pb.h>
 #include <stdexcept>
 #include <string>
+
+#include <userver/ugrpc/client/client_factory_component.hpp>
+#include <userver/components/component.hpp>
+#include <etcd/api/etcdserverpb/rpc_client.usrv.pb.hpp>
+#include <userver/storages/etcd/client_fwd.hpp>
+#include <vector>
+#include "userver/storages/etcd/request.hpp"
 
 USERVER_NAMESPACE_BEGIN
 
 namespace storages::etcd {
 
-  ClientImpl::ClientImpl(const std::string& endpoint) :
-    endpoint_(endpoint)
+  ClientImpl::ClientImpl(etcdserverpb::KVClientUPtr grpc_client) :
+    grpc_client_(std::move(grpc_client))
   {}
 
   Request ClientImpl::GetRange(const std::string& /*key_begin*/, const std::string& /*key_end*/)
   {
     return Request{};
-    // throw std::runtime_error("Not implemented");
   }
 
 }  // namespace storages::etcd
