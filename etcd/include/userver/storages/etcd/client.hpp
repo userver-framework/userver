3aea4656febbf18d0851a00a3d3c64584cb4f392
Alexandr Zaykov <alexzaykov@gmail.com>
2022-11-19 14:47:36 +0300

    added range and prefix methods for etcd Get and Delete operations
    Closes #11

--- etcd/include/userver/storages/etcd/client.hpp
+++ etcd/include/userver/storages/etcd/client.hpp
@@ -13,13 +13,14 @@ namespace storages::etcd {
 class Client {
  public:
   virtual ~Client() = default;
-
-  virtual Response GetRange(const std::string& key_begin,
-                           const std::string& key_end) const = 0;
-
+  virtual Response Get(const std::string& key) const = 0;
+  virtual Response GetRange(const std::string& key_begin, const std::string& key_end) const = 0;
+  virtual Response GetByPrefix(const std::string& prefix) const = 0;
   virtual void Put(const std::string& key, const std::string& value) const = 0;
 
   virtual void Delete(const std::string& key) const = 0;
+  virtual void DeleteRange(const std::string& key_begin, const std::string& key_end) const = 0;
+  virtual void DeleteByPrefix(const std::string& key) const = 0; 
 };
 
 }  // namespace storages::etcd
