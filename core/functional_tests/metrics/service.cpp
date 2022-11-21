ec11e076dcdad4d40e08576f29326b4fa716b621
antoshkka <antoshkka@userver.tech>
2022-11-21 16:36:13 +0300

    feat core: add tests on LRU cache metrics
    Tests: протестировано локально и CI

--- core/functional_tests/metrics/service.cpp
+++ core/functional_tests/metrics/service.cpp
@@ -1,6 +1,7 @@
 #include <userver/utest/using_namespace_userver.hpp>
 
 #include <userver/cache/caching_component_base.hpp>
+#include <userver/cache/lru_cache_component_base.hpp>
 #include <userver/components/common_component_list.hpp>
 #include <userver/components/common_server_component_list.hpp>
 #include <userver/dump/common.hpp>
@@ -14,20 +15,18 @@ namespace functional_tests {
 using KeyTranslation = std::unordered_map<std::string, std::string>;
 static_assert(dump::CheckDumpable<KeyTranslation>());
 
-class HttpCachedTranslations final
+class CachedTranslations final
     : public components::CachingComponentBase<KeyTranslation> {
  public:
   static constexpr std::string_view kName = "sample-cache";
 
-  HttpCachedTranslations(const components::ComponentConfig& config,
-                         const components::ComponentContext& context)
+  CachedTranslations(const components::ComponentConfig& config,
+                     const components::ComponentContext& context)
       : CachingComponentBase(config, context) {
     CacheUpdateTrait::StartPeriodicUpdates();
   }
 
-  ~HttpCachedTranslations() override {
-    CacheUpdateTrait::StopPeriodicUpdates();
-  }
+  ~CachedTranslations() override { CacheUpdateTrait::StopPeriodicUpdates(); }
 
   void Update(cache::UpdateType /*type*/,
               const std::chrono::system_clock::time_point& /*last_update*/,
@@ -40,6 +39,19 @@ class HttpCachedTranslations final
   }
 };
 
+class LruCachedTranslations final
+    : public cache::LruCacheComponent<std::string, std::string> {
+ public:
+  static constexpr auto kName = "sample-lru-cache";
+
+  LruCachedTranslations(const components::ComponentConfig& config,
+                        const components::ComponentContext& context)
+      : LruCacheComponent(config, context) {}
+
+ private:
+  std::string DoGetByKey(const std::string& /*key*/) override { return "1"; }
+};
+
 }  // namespace functional_tests
 
 int main(int argc, const char* const argv[]) {
@@ -48,7 +60,8 @@ int main(int argc, const char* const argv[]) {
           .AppendComponentList(components::CommonComponentList())
           .AppendComponentList(components::CommonServerComponentList())
           .Append<components::Secdist>()
-          .Append<functional_tests::HttpCachedTranslations>()
+          .Append<functional_tests::CachedTranslations>()
+          .Append<functional_tests::LruCachedTranslations>()
           .Append<server::handlers::Ping>();
   return utils::DaemonMain(argc, argv, component_list);
 }
