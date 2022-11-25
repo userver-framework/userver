af9704ab0307b9a58ca1a8e5c55f5b08fb384809
Ruslan <ruslankhakimov67@gmail.com>
2022-11-25 18:00:54 +0300

    add: new aged sketch test

--- shared/src/cache/impl/sketch/sketch_aged_test.cpp
+++ shared/src/cache/impl/sketch/sketch_aged_test.cpp
@@ -29,4 +29,29 @@ TEST(CacheSketch, AgedReset) {
   EXPECT_TRUE(was_reset);
 }
 
+TEST(CacheSketch, AgedHeavyHitters) {
+  AgedSketch counter(512, Hash{});
+  for (int i = 100; i < 100'000; i++) {
+    counter.Increment(static_cast<double>(i));
+  }
+  for (int i = 0; i < 10; i += 2) {
+    for (int j = 0; j < i; j++) {
+      counter.Increment(static_cast<double>(i));
+    }
+  }
+
+  // A perfect popularity count yields an array [0, 0, 2, 0, 4, 0, 6, 0, 8, 0]
+  std::vector<int> popularity(10);
+  for (int i = 0; i < 10; i++) {
+    popularity[i] = counter.Estimate(static_cast<double>(i));
+  }
+  EXPECT_TRUE(popularity[2] <= popularity[4]);
+  EXPECT_TRUE(popularity[4] <= popularity[6]);
+  EXPECT_TRUE(popularity[6] <= popularity[8]);
+  for (int i = 0; i < 10; i++)
+    if ((i == 0) || (i == 1) || (i == 3) || (i == 5) || (i == 7) || (i == 9)) {
+      EXPECT_TRUE(popularity[i] <= popularity[2]);
+    }
+}
+
 USERVER_NAMESPACE_END
