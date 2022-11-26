121272b27d3dc36bf94c1398a873200e3be79379
Ruslan <ruslankhakimov67@gmail.com>
2022-11-26 12:08:31 +0300

    edit end of line for stewkk

--- shared/include/userver/cache/impl/hash.hpp
+++ shared/include/userver/cache/impl/hash.hpp
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 #pragma once
 
 #include <cstddef>
@@ -9,10 +10,7 @@ USERVER_NAMESPACE_BEGIN
 
 namespace cache::impl::tools {
 size_t NextPowerOfTwo(size_t n);
-<<<<<<< HEAD
 
-=======
->>>>>>> 044bfb53 (refactor)
 // TODO: Murmur3
 template <typename T>
 struct Jenkins {
@@ -40,3 +38,39 @@ struct Jenkins {
 >>>>>>> 044bfb53 (refactor)
 
 USERVER_NAMESPACE_END
+=======
+#pragma once
+
+#include <cstddef>
+#include <cstdint>
+
+#include <userver/utils/assert.hpp>
+
+USERVER_NAMESPACE_BEGIN
+
+namespace cache::impl::tools {
+size_t NextPowerOfTwo(size_t n);
+// TODO: Murmur3
+template <typename T>
+struct Jenkins {
+  uint32_t operator()(const T& item) {
+    const char* data = reinterpret_cast<const char*>(&item);
+    uint32_t hash = 0;
+
+    for (std::size_t i = 0; i < static_cast<size_t>(sizeof item); ++i) {
+      hash += data[i];
+      hash += hash << 10;
+      hash ^= hash >> 6;
+    }
+
+    hash += hash << 3;
+    hash ^= hash >> 11;
+    hash += hash << 15;
+
+    return hash;
+  }
+};
+}  // namespace cache::impl::tools
+
+USERVER_NAMESPACE_END
+>>>>>>> 8018f090 (edit end of line for stewkk)
