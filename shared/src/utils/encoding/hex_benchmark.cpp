b407163b08bc7b9862308c7529fc700a57773690
Ivan Trofimov <i.trofimow@yandex.ru>
2022-12-03 01:27:14 +0300

    + no-alloc benchmark

--- shared/src/utils/encoding/hex_benchmark.cpp
+++ shared/src/utils/encoding/hex_benchmark.cpp
@@ -4,17 +4,40 @@
 
 USERVER_NAMESPACE_BEGIN
 
-void to_hex_benchmark(benchmark::State& state) {
+namespace {
+
+std::string GenerateSource(size_t size) {
   std::string source;
-  source.reserve(state.range(0));
-  for (ssize_t i = 0; i < state.range(0); ++i) {
+  source.reserve(size);
+  for (size_t i = 0; i < size; ++i) {
     source.push_back('a' + i % 26);
   }
+
+  return source;
+}
+
+}  // namespace
+
+void to_hex_benchmark(benchmark::State& state) {
+  const auto source = GenerateSource(state.range(0));
 
   for (auto _ : state) {
     benchmark::DoNotOptimize(utils::encoding::ToHex(source));
   }
 }
 BENCHMARK(to_hex_benchmark)->RangeMultiplier(2)->Range(8, 512);
+
+void to_hex_benchmark_no_alloc(benchmark::State& state) {
+  const auto source = GenerateSource(state.range(0));
+
+  std::string out;
+  out.reserve(state.range(0) * 2);
+  benchmark::DoNotOptimize(out);
+
+  for (auto _ : state) {
+    utils::encoding::ToHex(source, out);
+  }
+}
+BENCHMARK(to_hex_benchmark_no_alloc)->RangeMultiplier(2)->Range(8, 512);
 
 USERVER_NAMESPACE_END
