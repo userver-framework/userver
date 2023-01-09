db6980bb1bd1806e1e92718f77a229782fb22d02
segoon <segoon@userver.tech>
2023-01-09 21:43:21 +0300

    bug fs: create temp file in fs TP

--- core/src/fs/temp_file.cpp
+++ core/src/fs/temp_file.cpp
@@ -1,20 +1,32 @@
 #include <userver/fs/temp_file.hpp>
 
-#include <userver/utils/async.hpp>
+#include <userver/engine/async.hpp>
 
 USERVER_NAMESPACE_BEGIN
 
 namespace fs {
 
 TempFile TempFile::Create(engine::TaskProcessor& fs_task_processor) {
-  return {fs_task_processor, blocking::TempFile::Create()};
+  return {
+      fs_task_processor,
+      engine::AsyncNoSpan(fs_task_processor,
+                          [] { return blocking::TempFile::Create(); })
+          .Get(),
+  };
 }
 
 TempFile TempFile::Create(std::string_view parent_path,
                           std::string_view name_prefix,
                           engine::TaskProcessor& fs_task_processor) {
-  return {fs_task_processor,
-          blocking::TempFile::Create(parent_path, name_prefix)};
+  return {
+      fs_task_processor,
+      engine::AsyncNoSpan(fs_task_processor,
+                          [&parent_path, &name_prefix] {
+                            return blocking::TempFile::Create(parent_path,
+                                                              name_prefix);
+                          })
+          .Get(),
+  };
 }
 
 TempFile::~TempFile() { std::move(*this).Remove(); }
@@ -27,7 +39,7 @@ TempFile TempFile::Adopt(std::string path,
 const std::string& TempFile::GetPath() const { return temp_file_.GetPath(); }
 
 void TempFile::Remove() && {
-  utils::Async(fs_task_processor_, "temp_file_remove", [this] {
+  engine::AsyncNoSpan(fs_task_processor_, [this] {
     std::move(temp_file_).Remove();
   }).Get();
 }
