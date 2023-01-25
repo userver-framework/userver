807e4ae94f1fff13adec056525f656024866755d
theoldmop <theoldmop@yandex-team.com>
2023-01-25 13:36:07 +0300

    feat secdist: add component fwd

---
+++ core/include/userver/storages/secdist/fwd.hpp
@@ -0,0 +1,19 @@
+#pragma once
+
+/// @file storages/secdist/fwd.hpp
+/// @brief @copybrief components::Secdist
+
+USERVER_NAMESPACE_BEGIN
+
+namespace components {
+// NOLINTNEXTLINE(bugprone-forward-declaration-namespace)
+class Secdist;
+}  // namespace components
+
+namespace storages::secdist {
+// NOLINTNEXTLINE(bugprone-forward-declaration-namespace)
+class Secdist;
+class SecdistConfig;
+}  // namespace storages::secdist
+
+USERVER_NAMESPACE_END
