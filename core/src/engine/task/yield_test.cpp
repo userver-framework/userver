fa04b47516ac6cb9237b71550d8d4dbc3707562c
Ivan Trofimov <i.trofimow@yandex.ru>
2023-01-26 13:30:05 +0300

    cleanup

--- core/src/engine/task/yield_test.cpp
+++ core/src/engine/task/yield_test.cpp
@@ -7,12 +7,14 @@
 
 USERVER_NAMESPACE_BEGIN
 
+// This test fails consistently under current (at this commit
+// b70ef2679b5ed68b915912167635db6eab3451c7) scheduling policy.
 UTEST_MT(EngineYield, IsBroken, 2) {
   // so we have 2 threads here
 
   // alright, let's make the second thread just yield
   auto yield_task = engine::AsyncNoSpan([] {
-    for (size_t i = 0; i < 150 && !engine::current_task::ShouldCancel(); ++i) {
+    while (!engine::current_task::ShouldCancel()) {
       std::this_thread::sleep_for(std::chrono::milliseconds{20});
       engine::Yield();
     }
@@ -26,24 +28,23 @@ UTEST_MT(EngineYield, IsBroken, 2) {
   // 2. one reasonably fast task
   auto slow_task = engine::AsyncNoSpan(
       [] { std::this_thread::sleep_for(std::chrono::milliseconds{500}); });
-  const auto fast_task_duration_ms =
+  const auto fast_task_duration =
       engine::AsyncNoSpan([start = std::chrono::steady_clock::now()] {
         std::this_thread::sleep_for(std::chrono::milliseconds{2});
         const auto finish = std::chrono::steady_clock::now();
         return std::chrono::duration_cast<std::chrono::milliseconds>(finish -
-                                                                     start)
-            .count();
+                                                                     start);
       }).Get();
 
   // One would expect that fast task will be executed withing some
   // moderate amount of time, because no matter which thread gets blocked
   // by the slow_task, there is another one which
   // is never blocked for more than 20ms
-  EXPECT_LT(fast_task_duration_ms, 50);
+  EXPECT_LT(fast_task_duration.count(), std::chrono::milliseconds{200}.count());
   // This ^ assert fails miserably
 
   yield_task.SyncCancel();
-  slow_task.Wait();
+  slow_task.Get();
 }
 
 USERVER_NAMESPACE_END
