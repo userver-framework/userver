d1f7dbc5f366f9652d3faeb3541a6d1174c27831
antonyzhilin <antonyzhilin@userver.tech>
2023-02-01 18:29:11 +0300

    fix build: relax_cpu.hpp build on M1

--- shared/src/compiler/relax_cpu.hpp
+++ shared/src/compiler/relax_cpu.hpp
@@ -8,7 +8,15 @@ class RelaxCpu final {
  public:
   // Should be called in spin-wait loops to reduce contention and allow other
   // threads to execute.
-  void operator()() noexcept { __builtin_ia32_pause(); }
+  void operator()() noexcept {
+#if defined(__x86_64__) || defined(__amd64__) || defined(__i386__)
+    __builtin_ia32_pause();
+#elif defined(__aarch64__)
+    __asm__ __volatile__("yield");
+#else
+#error "Unsupported CPU architecture"
+#endif
+  }
 };
 
 }  // namespace compiler
