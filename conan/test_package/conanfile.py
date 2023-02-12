f042cdd90abc4d05e4d12be296b0318b7be9a1f6
atom <tomasiche@gmail.com>
2023-02-12 23:45:08 +0300

    conan v2 migration init

--- conan/test_package/conanfile.py
+++ conan/test_package/conanfile.py
@@ -1,17 +1,26 @@
-import conans  # pylint: disable=import-error
+from conan import ConanFile
+from conan.tools.build import can_run
+from conan.tools.cmake import CMake, cmake_layout
+import os
 
 
-class UserverTestConan(conans.ConanFile):
-    settings = 'os', 'compiler', 'build_type', 'arch'
-    generators = 'cmake_find_package'
+class TestPackageConan(ConanFile):
+    settings = "os", "arch", "compiler", "build_type"
+    generators = "CMakeToolchain", "CMakeDeps", "VirtualRunEnv"
+    test_type = "explicit"
+
+    def layout(self):
+        cmake_layout(self)
 
     def requirements(self):
-        self.requires('userver/1.0.0')
+        self.requires(self.tested_reference_str)
 
     def build(self):
-        cmake = conans.CMake(self)
+        cmake = CMake(self)
         cmake.configure()
         cmake.build()
 
     def test(self):
-        self.run('./hello_service')
+        if can_run(self):
+            bin_path = os.path.join(self.cpp.build.bindirs[0], "hello_service")
+            self.run(bin_path, env="conanrun")
--- conanfile.py
+++ conanfile.py
@@ -1,13 +1,19 @@
 # pylint: disable=no-member
 
-import os
+from conan import ConanFile
+from conan.errors import ConanInvalidConfiguration
+from conan.tools.build import cross_building
+from conan.tools.cmake import CMake, CMakeDeps, CMakeToolchain, cmake_layout
+from conan.tools.env import VirtualBuildEnv, VirtualRunEnv
+from conan.tools.files import apply_conandata_patches, collect_libs, copy, export_conandata_patches, get, rename, replace_in_file, rmdir, save
+from conan.tools.microsoft import is_msvc, is_msvc_static_runtime
 
-import conans  # pylint: disable=import-error
+import os
 
 required_conan_version = '>=1.51.0, <2.0.0'  # pylint: disable=invalid-name
 
 
-class UserverConan(conans.ConanFile):
+class UserverConan(ConanFile):
     name = 'userver'
     version = '1.0.0'
     description = 'The C++ Asynchronous Framework'
@@ -16,7 +22,6 @@ class UserverConan(conans.ConanFile):
     homepage = 'https://userver.tech/'
     license = 'Apache-2.0'
     exports_sources = '*'
-    generators = ('cmake_find_package', 'cmake_paths')
 
     settings = 'os', 'arch', 'compiler', 'build_type'
     options = {
@@ -69,7 +74,7 @@ class UserverConan(conans.ConanFile):
 
     @property
     def _build_subfolder(self):
-        return 'build_subfolder'
+        return os.path.join(self.build_folder, 'userver')
 
     def configure(self):
         if self.options.shared:
@@ -81,6 +86,10 @@ class UserverConan(conans.ConanFile):
             )
         if not self.options.namespace_end:
             self.options.namespace_end = '}'
+
+
+    def layout(self):
+        cmake_layout(self)
 
     def requirements(self):
         self.requires('boost/1.79.0')
@@ -117,47 +126,42 @@ class UserverConan(conans.ConanFile):
             self.requires('gtest/1.12.1')
             self.requires('benchmark/1.6.2')
 
-    def _configure_cmake(self):
-        cmake = conans.CMake(self)
-        cmake.definitions['CMAKE_FIND_DEBUG_MODE'] = 'OFF'
 
-        cmake.definitions['USERVER_IS_THE_ROOT_PROJECT'] = 'OFF'
-        cmake.definitions['USERVER_DOWNLOAD_PACKAGES'] = 'ON'
-        cmake.definitions['USERVER_FEATURE_DWCAS'] = 'ON'
-        cmake.definitions['USERVER_NAMESPACE'] = self.options.namespace
-        cmake.definitions[
+
+    def generate(self):
+        tc = CMakeToolchain(self)
+        tc.variables['CMAKE_FIND_DEBUG_MODE'] = False
+
+        tc.variables['USERVER_OPEN_SOURCE_BUILD'] = True
+        tc.variables['USERVER_CONAN'] = True
+        tc.variables['USERVER_IS_THE_ROOT_PROJECT'] = False
+        tc.variables['USERVER_DOWNLOAD_PACKAGES'] = True
+        tc.variables['USERVER_FEATURE_DWCAS'] = True
+        tc.variables['USERVER_NAMESPACE'] = self.options.namespace
+        tc.variables[
             'USERVER_NAMESPACE_BEGIN'
         ] = self.options.namespace_begin
-        cmake.definitions['USERVER_NAMESPACE_END'] = self.options.namespace_end
+        tc.variables['USERVER_NAMESPACE_END'] = self.options.namespace_end
 
-        if not self.options.lto:
-            cmake.definitions['USERVER_LTO'] = 'OFF'
-        if not self.options.with_jemalloc:
-            cmake.definitions['USERVER_FEATURE_JEMALLOC'] = 'OFF'
-        if not self.options.with_mongodb:
-            cmake.definitions['USERVER_FEATURE_MONGODB'] = 'OFF'
-        if not self.options.with_postgresql:
-            cmake.definitions['USERVER_FEATURE_POSTGRESQL'] = 'OFF'
-        if not self.options.with_postgresql_extra:
-            cmake.definitions['USERVER_FEATURE_PATCH_LIBPQ'] = 'OFF'
-        if not self.options.with_redis:
-            cmake.definitions['USERVER_FEATURE_REDIS'] = 'OFF'
-        if not self.options.with_grpc:
-            cmake.definitions['USERVER_FEATURE_GRPC'] = 'OFF'
-        if not self.options.with_clickhouse:
-            cmake.definitions['USERVER_FEATURE_CLICKHOUSE'] = 'OFF'
-        if not self.options.with_universal:
-            cmake.definitions['USERVER_FEATURE_UNIVERSAL'] = 'OFF'
-        if not self.options.with_rabbitmq:
-            cmake.definitions['USERVER_FEATURE_RABBITMQ'] = 'OFF'
-        if not self.options.with_utest:
-            cmake.definitions['USERVER_FEATURE_UTEST'] = 'OFF'
+        tc,variables['USERVER_LTO'] = self.options.lto
+        tc.variables['USERVER_FEATURE_JEMALLOC'] = self.options.with_jemalloc
+        tc.variables['USERVER_FEATURE_MONGODB'] = self.options.with_mongodb
+        tc.variables['USERVER_FEATURE_POSTGRESQL'] = self.options.with_postgresql
+        tc.variables['USERVER_FEATURE_PATCH_LIBPQ'] = self.options.with_postgresql_extra
+        tc.variables['USERVER_FEATURE_REDIS'] = self.options.with_redis
+        tc.variables['USERVER_FEATURE_GRPC'] = self.options.with_grpc
+        tc.variables['USERVER_FEATURE_CLICKHOUSE'] = self.options.with_clickhouse
+        tc.variables['USERVER_FEATURE_UNIVERSAL'] = self.options.with_universal
+        tc.variables['USERVER_FEATURE_RABBITMQ'] = self.options.with_rabbitmq
+        tc.variables['USERVER_FEATURE_UTEST'] = self.options.with_utest
 
-        cmake.configure(build_folder=self._build_subfolder)
-        return cmake
+        tc.generate()
+
+        CMakeDeps(self).generate()
 
     def build(self):
-        cmake = self._configure_cmake()
+        cmake = CMake(self)
+        cmake.configure()
         cmake.build()
 
     def package(self):
@@ -169,11 +173,16 @@ class UserverConan(conans.ConanFile):
         self.copy(
             pattern='*', dst='include', src='shared/include', keep_path=True,
         )
+        print(self._build_subfolder)
 
+        if self.options.with_universal:
+            copy(self, pattern='*', dst='include', src='universal/include', keep_path=True)
+            copy(self, pattern='*.a', dst=os.path.join(self.package_folder, "lib"), src=os.path.join(self._build_subfolder,'universal'), keep_path=False)
+            copy(self, pattern='*.so', dst=os.path.join(self.package_folder, "lib"), src=os.path.join(self._build_subfolder, 'universal'), keep_path=False)
         if self.options.with_grpc:
-            self.copy(
-                pattern='*', dst='include', src='grpc/include', keep_path=True,
-            )
+            copy(self, pattern='*', dst='include', src='grpc/include', keep_path=True)
+            copy(self, pattern='*.a', dst=os.path.join(self.package_folder, "lib"), src=os.path.join(self._build_subfolder,'grpc'), keep_path=False)
+            copy(self, pattern='*.so', dst=os.path.join(self.package_folder, "lib"), src=os.path.join(self._build_subfolder, 'grpc'), keep_path=False)
         if self.options.with_utest:
             self.copy(
                 pattern='*',
@@ -183,13 +192,6 @@ class UserverConan(conans.ConanFile):
             )
             self.copy(
                 pattern='*', dst='testsuite', src='testsuite', keep_path=True,
-            )
-        if self.options.with_postgresql:
-            self.copy(
-                pattern='*',
-                dst='include',
-                src='postgresql/include',
-                keep_path=True,
             )
             self.copy(
                 pattern='UserverTestsuite.cmake',
@@ -203,57 +205,184 @@ class UserverConan(conans.ConanFile):
                 src='cmake',
                 keep_path=True,
             )
-        if self.options.with_mongodb:
-            self.copy(
-                pattern='*',
-                dst='include',
-                src='mongo/include',
-                keep_path=True,
-            )
-        if self.options.with_redis:
-            self.copy(
-                pattern='*',
-                dst='include',
-                src='redis/include',
-                keep_path=True,
-            )
-        if self.options.with_rabbitmq:
-            self.copy(
-                pattern='*',
-                dst='include',
-                src='rabbitmq/include',
-                keep_path=True,
-            )
-        if self.options.with_clickhouse:
-            self.copy(
-                pattern='*',
-                dst='include',
-                src='clickhouse/include',
-                keep_path=True,
-            )
+        if self.options.with_postgresql:
+            copy(self, pattern='*', dst='include', src='postgresql/include', keep_path=True)
+            copy(self, pattern='*.a', dst=os.path.join(self.package_folder, "lib"), src=os.path.join(self._build_subfolder,'postgresql'), keep_path=False)
+            copy(self, pattern='*.so', dst=os.path.join(self.package_folder, "lib"), src=os.path.join(self._build_subfolder, 'postgresql'), keep_path=False)
 
-        self.copy(
-            pattern='*.a',
-            dst='lib',
-            src=os.path.join(self._build_subfolder, 'userver'),
-            keep_path=False,
-        )
-        self.copy(
-            pattern='*.so',
-            dst='lib',
-            src=os.path.join(self._build_subfolder, 'userver'),
-            keep_path=False,
-        )
+        if self.options.with_mongodb:
+            copy(self, pattern='*', dst='include', src='mongo/include', keep_path=True)
+            copy(self, pattern='*.a', dst=os.path.join(self.package_folder, "lib"), src=os.path.join(self._build_subfolder,'mongo'), keep_path=False)
+            copy(self, pattern='*.so', dst=os.path.join(self.package_folder, "lib"), src=os.path.join(self._build_subfolder, 'mongo'), keep_path=False)
+        if self.options.with_redis:
+            copy(self, pattern='*', dst='include', src='redis/include', keep_path=True)
+            copy(self, pattern='*.a', dst=os.path.join(self.package_folder, "lib"), src=os.path.join(self._build_subfolder,'redis'), keep_path=False)
+            copy(self, pattern='*.so', dst=os.path.join(self.package_folder, "lib"), src=os.path.join(self._build_subfolder, 'redis'), keep_path=False)
+        if self.options.with_rabbitmq:
+            copy(self, pattern='*', dst='include', src='rabbitmq/include', keep_path=True)
+            copy(self, pattern='*.a', dst=os.path.join(self.package_folder, "lib"), src=os.path.join(self._build_subfolder,'rabbitmq'), keep_path=False)
+            copy(self, pattern='*.so', dst=os.path.join(self.package_folder, "lib"), src=os.path.join(self._build_subfolder, 'rabbitmq'), keep_path=False)
+        if self.options.with_clickhouse:
+            copy(self, pattern='*', dst='include', src='clickhouse/include', keep_path=True)
+            copy(self, pattern='*.a', dst=os.path.join(self.package_folder, "lib"), src=os.path.join(self._build_subfolder,'clickhouse'), keep_path=False)
+            copy(self, pattern='*.so', dst=os.path.join(self.package_folder, "lib"), src=os.path.join(self._build_subfolder, 'clickhouse'), keep_path=False)
+
+        copy(self, pattern='*.a', dst=os.path.join(self.package_folder, "lib"), src=os.path.join(self._build_subfolder,'core'), keep_path=False)
+        copy(self, pattern='*.so', dst=os.path.join(self.package_folder, "lib"), src=os.path.join(self._build_subfolder, 'core'), keep_path=False)
+
+    @property
+    def _userver_components(self):
+
+        def grpc():
+            return ["grpc::grpc"] if self.options.with_grpc else []
+
+        def fmt():
+            return ["fmt::fmt"]
+        def cctz():
+            return ["cctz::cctz"]
+        def boost():
+            return ["boost::boost"]
+        def concurrentqueue():
+            return ["concurrentqueue::concurrentqueue"]
+        def yaml():
+            return ["yaml-cpp::yaml-cpp"]
+        def libev():
+            return ["libev::libev"]
+        def http_parser():
+            return ["http_parser::http_parser"]    
+        def grpc():
+            return ["grpc::grpc"] if self.options.with_grpc else []
+        def postgresql():
+            return ["libpq::pq"] if self.options.with_postgresql else []
+        def gtest():
+            return ["gtest::gtest"] if self.options.with_utest else []
+        def benchmark():
+            return ["benchmark::benchmark"] if self.options.with_utest else []            
+        def mongo():
+            return ["mongo::mongoc_static"] if self.options.with_mongodb else []                                    
+        def hiredis():
+            return ["hiredis::hiredis"] if self.options.with_redis else []
+        def amqpcpp():
+            return ["amqpcpp"] if self.options.with_rabbitmq else []  
+
+        userver_components = [
+            {"target": "userver-core-internal",       "lib": "core-internal",       "requires": [] },
+            {"target": "userver-core",       "lib": "core",       "requires": ["userver-core-internal"] + fmt() + cctz() + boost() + concurrentqueue() + yaml() + libev() + http_parser() }
+        ]
+        if self.options.with_universal:
+            userver_components.extend([
+                {"target": "userver-universal", "lib": "universal", "requires": ["userver-core"] }
+            ])        
+        if self.options.with_grpc:
+            userver_components.extend([
+                {"target": "userver-grpc", "lib": "grpc", "requires": ["userver-core"] + grpc() },
+                {"target": "userver-api-common-protos",      "lib": "api-common-protos",      "requires": ["userver-grpc"] }
+            ])
+        if self.options.with_utest:
+            userver_components.extend([
+                {"target": "userver-utest", "lib": "utest", "requires": ["userver-core"] + gtest() },
+                {"target": "userver-ubench", "lib": "ubench", "requires": ["userver-core"] + benchmark() }
+            ])
+        if self.options.with_postgresql:
+            userver_components.extend([
+                {"target": "userver-postgresql", "lib": "postgresql", "requires": ["userver-core"] + postgresql() }
+            ])
+        # if self.options.with_mongodb:
+        #     userver_components.extend([
+        #         {"target": "userver-mongo", "lib": "mongo", "requires": ["userver-core"] + mongo() }
+        #     ])
+        if self.options.with_redis:
+            userver_components.extend([
+                {"target": "userver-redis", "lib": "redis", "requires": ["userver-core"] + hiredis() }
+            ])       
+        # if self.options.with_rabbitmq:
+        #     userver_components.extend([
+        #         {"target": "userver-rabbitmq", "lib": "rabbitmq", "requires": ["userver-core"] + amqpcpp() }
+        #     ])                                            
+        return userver_components 
+
 
     def package_info(self):
-        self.cpp_info.libs = conans.tools.collect_libs(self)
+        print(self.folders)
+      #  self.cpp_info.libs = conans.tools.collect_libs(self)
+        debug = "d" if self.settings.build_type == "Debug" and self.settings.os == "Windows" else ""
 
-        self.cpp_info.defines.append(
+        def get_lib_name(module):
+            return f"userver-{module}{debug}"
+
+        def add_components(components):
+            for component in components:
+                conan_component = component["target"]
+                cmake_target = component["target"]
+                cmake_component = component["lib"]
+                lib_name = get_lib_name(component["lib"])
+                requires = component["requires"]
+                # TODO: we should also define COMPONENTS names of each target for find_package() but not possible yet in CMakeDeps
+                #       see https://github.com/conan-io/conan/issues/10258
+                self.cpp_info.components[conan_component].set_property("cmake_target_name", cmake_target)
+                self.cpp_info.components[conan_component].libs = [lib_name]
+                if self.settings.os == "Linux":
+                    self.cpp_info.components[conan_component].system_libs = ["pthread"]
+
+                # TODO: to remove in conan v2 once cmake_find_package* generators removed
+                self.cpp_info.components[conan_component].names["cmake_find_package"] = cmake_target
+                self.cpp_info.components[conan_component].names["cmake_find_package_multi"] = cmake_target
+                print(self.cpp_info.components[conan_component].requires)
+                print(requires)
+                self.cpp_info.components[conan_component].requires = requires
+             #   self.cpp_info.components[conan_component].build_modules["cmake_find_package"] = [self._module_file_rel_path]
+             #   self.cpp_info.components[conan_component].build_modules["cmake_find_package_multi"] = [self._module_file_rel_path]
+                if cmake_component != cmake_target:
+                    conan_component_alias = conan_component + "_alias"
+                    self.cpp_info.components[conan_component_alias].names["cmake_find_package"] = cmake_component
+                    self.cpp_info.components[conan_component_alias].names["cmake_find_package_multi"] = cmake_component
+                    self.cpp_info.components[conan_component_alias].requires = [conan_component]
+                    self.cpp_info.components[conan_component_alias].bindirs = []
+                    self.cpp_info.components[conan_component_alias].includedirs = []
+                    self.cpp_info.components[conan_component_alias].libdirs = []
+
+        self.cpp_info.components["userver-core-internal"].defines.append(
             f'USERVER_NAMESPACE={self.options.namespace}',
         )
-        self.cpp_info.defines.append(
+        self.cpp_info.components["userver-core-internal"].defines.append(
             f'USERVER_NAMESPACE_BEGIN={self.options.namespace_begin}',
         )
-        self.cpp_info.defines.append(
+        self.cpp_info.components["userver-core-internal"].defines.append(
             f'USERVER_NAMESPACE_END={self.options.namespace_end}',
         )
+
+        self.cpp_info.set_property("cmake_file_name", "userver") 
+
+        add_components(self._userver_components)
+        #self.cpp_info.components["core"].set_property("cmake_target_name", "userver-core")
+        #self.cpp_info.components["core"].libs = ["userver-core", "userver-core-internal"]
+
+        # self.cpp_info.components["universal"].set_property("cmake_target_name", "userver-universal")
+        # self.cpp_info.components["universal"].libs = ["userver-universal"]
+
+        # if self.options.with_grpc:
+        #     self.cpp_info.components["grpc"].set_property("cmake_target_name", "userver-grpc")
+        #     self.cpp_info.components["grpc"].libs = ["userver-grpc"]
+
+        # if self.options.with_utest:
+        #     self.cpp_info.components["utest"].set_property("cmake_target_name", "userver-utest")
+        #     self.cpp_info.components["utest"].libs = ["userver-utest"]
+        
+        # if self.options.with_postgresql:
+        #     self.cpp_info.components["postgresql"].set_property("cmake_target_name", "userver-postgresql")
+        #     self.cpp_info.components["postgresql"].libs = ["userver-postgresql"]
+            
+        # if self.options.with_mongodb:
+        #     self.cpp_info.components["mongo"].set_property("cmake_target_name", "userver-mongo")
+        #     self.cpp_info.components["mongo"].libs = ["userver-mongo"]
+
+        # if self.options.with_redis:
+        #     self.cpp_info.components["redis"].set_property("cmake_target_name", "userver-redis")
+        #     self.cpp_info.components["redis"].libs = ["userver-redis"]
+
+        # if self.options.with_rabbitmq:
+        #     self.cpp_info.components["rabbitmq"].set_property("cmake_target_name", "userver-rabbitmq")
+        #     self.cpp_info.components["rabbitmq"].libs = ["userver-rabbitmq"]
+
+        self.cpp_info.filenames["cmake_find_package"] = "userver"
+        self.cpp_info.filenames["cmake_find_package_multi"] = "userver"
