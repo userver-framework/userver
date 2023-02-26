e06263f24c944dedea1fc6b8e434ac43335394df
atom <tomasiche@gmail.com>
2023-02-26 16:26:38 +0300

    enable testsuite tests

--- conan/test_package/conanfile.py
+++ conan/test_package/conanfile.py
@@ -26,5 +26,20 @@ class TestPackageConan(ConanFile):
             self.run(bin_path, env="conanrun")
             bin_path = os.path.join(self.cpp.build.bindirs[0], "PackageTest_utest")
             self.run(bin_path, env="conanrun")
-            bin_path = os.path.join(self.cpp.build.bindirs[0], "PackageTest_ubench")
+            bin_path = os.path.join(self.cpp.build.bindirs[0], "PackageTest_grpc")
             self.run(bin_path, env="conanrun")
+            bin_path = os.path.join(self.cpp.build.bindirs[0], "PackageTest_mongo")
+            self.run(bin_path, env="conanrun")
+            bin_path = os.path.join(self.cpp.build.bindirs[0], "PackageTest_postgresql")
+            self.run(bin_path, env="conanrun")
+            bin_path = os.path.join(self.cpp.build.bindirs[0], "PackageTest_rabbitmq")
+            self.run(bin_path, env="conanrun")
+            bin_path = os.path.join(self.cpp.build.bindirs[0], "PackageTest_redis")
+            self.run(bin_path, env="conanrun")
+
+            bin_path = os.path.join(self.cpp.build.bindirs[0], "testsuite-support", "runtests-testsuite-userver-samples-testsuite-support")
+            command = " "
+            folder = os.path.join(self.recipe_folder, "..", "..", "samples", "testsuite-support", "tests")
+            args = [bin_path, "--service-logs-pretty", "-vv", folder]
+            self.run(command.join(args), env="conanrun")
+
