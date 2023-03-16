5b8152d86a1867cdb857b20b47252faf5a68c476
lfaktorovich <lfaktorovich@yandex-team.com>
2023-03-16 11:55:26 +0300

    fix json: change stack size for testing

--- core/src/formats/json/stack_overflow_test.cpp
+++ core/src/formats/json/stack_overflow_test.cpp
@@ -40,10 +40,10 @@ std::string MakeStringOfDeepArray(std::size_t depth) {
 TEST(FormatsJson, DeepObjectFromString) {
   constexpr std::size_t kWorkerThreads = 1;
   engine::TaskProcessorPoolsConfig config;
-  config.coro_stack_size = 32 * 1024ULL;
+  config.coro_stack_size = 128 * 1024ULL;
 
   engine::RunStandalone(kWorkerThreads, config, [] {
-    constexpr std::size_t kDepth = 16000;
+    constexpr std::size_t kDepth = 16 * 1024;
     auto value = formats::json::FromString(MakeStringOfDeepObject(kDepth));
 
     for (std::size_t i = 0; i < kDepth; ++i) {
@@ -56,10 +56,10 @@ TEST(FormatsJson, DeepObjectFromString) {
 TEST(FormatsJson, DeepArrayFromString) {
   constexpr std::size_t kWorkerThreads = 1;
   engine::TaskProcessorPoolsConfig config;
-  config.coro_stack_size = 32 * 1024ULL;
+  config.coro_stack_size = 128 * 1024ULL;
 
   engine::RunStandalone(kWorkerThreads, config, [] {
-    constexpr std::size_t kDepth = 16000;
+    constexpr std::size_t kDepth = 16 * 1024;
     auto value = formats::json::FromString(MakeStringOfDeepArray(kDepth));
 
     for (std::size_t i = 0; i < kDepth; ++i) {
