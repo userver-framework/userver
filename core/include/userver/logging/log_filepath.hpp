30b8fd8e17b11f2c96564f9337d682479e5c0f4f
lfaktorovich <lfaktorovich@yandex-team.com>
2023-03-27 11:56:18 +0300

    fix logs: change the trimming of file paths

--- core/include/userver/logging/log_filepath.hpp
+++ core/include/userver/logging/log_filepath.hpp
@@ -18,7 +18,7 @@ namespace logging::impl {
 
 // May have different macro values for different translation units, hence static
 // TODO: consteval
-static constexpr size_t PathBaseSize(std::string_view path) noexcept {
+static constexpr std::size_t PathBaseSize(std::string_view path) noexcept {
   for (const std::string_view base : {
 #ifdef USERVER_LOG_BUILD_PATH_BASE
            USERVER_LOG_FILEPATH_STRINGIZE(USERVER_LOG_BUILD_PATH_BASE),
@@ -29,8 +29,25 @@ static constexpr size_t PathBaseSize(std::string_view path) noexcept {
            ""  // default in case none were defined
        }) {
     if (path.substr(0, base.size()) == base) {
-      size_t base_size = base.size();
-      while (base_size < path.size() && path[base_size] == '/') ++base_size;
+      const bool default_case = base.empty();
+      std::size_t base_size = path.find_first_not_of('/', base.size());
+      if (base_size == std::string_view::npos) {
+        base_size = path.size();
+      }
+
+      if (default_case) {
+        return base_size;
+      }
+
+      constexpr std::string_view kUserverName = "userver";
+      if (path.substr(base_size, kUserverName.size()) != kUserverName) {
+        const std::size_t not_slash = path.find_last_not_of('/', base_size - 1);
+        if (not_slash == std::string_view::npos) {
+          return base_size;
+        }
+        const std::size_t prev_slash = path.rfind('/', not_slash);
+        return prev_slash == std::string_view::npos ? 0 : (prev_slash + 1);
+      }
       return base_size;
     }
   }
