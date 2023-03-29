90381bd8ec5e8ff0a309eee6ad504fe0a870790c
a11ax <a11ax@yandex-team.com>
2023-03-29 21:52:50 +0300

    feat uservices: do not peerdir grpc if not needed

--- testsuite/pytest_plugins/pytest_userver/plugins/grpc/__init__.py
+++ testsuite/pytest_plugins/pytest_userver/plugins/grpc/__init__.py
@@ -7,6 +7,6 @@ testsuite; see @ref md_en_userver_functional_testing for an introduction.
 
 pytest_plugins = [
     'pytest_userver.plugins.core',
-    'pytest_userver.plugins.grpc_client',
-    'pytest_userver.plugins.grpc_mockserver',
+    'pytest_userver.plugins.grpc.client',
+    'pytest_userver.plugins.grpc.mockserver',
 ]
