4320cafddcd3050b636bc8d45afbdbb5194506b7
segoon <segoon@userver.tech>
2023-05-03 23:06:28 +0300

    feat codegen: packetize u-ctl

---
+++ scripts/uctl.bash_completion.sh
@@ -0,0 +1,39 @@
+#!/usr/bin/bash
+
+_uctl_commands=(
+  'dnsclient reload-hosts'
+  'dnsclient flush-cache'
+  'dnsclient flush-cache-full'
+  'log-dynamic-debug'
+  'log-level get'
+  'log-level set trace'
+  'log-level set debug'
+  'log-level set info'
+  'log-level set warning'
+  'log-level set error'
+  'log-level set none'
+  'log-level set default'
+  'on-logrotate'
+)
+
+_uctl_complete()
+{
+  if [ "${#COMP_WORDS[@]}" != "$((1+${COMP_CWORD}))" ]; then
+    return
+  fi
+
+  cmdline=$(echo ${COMP_LINE} | sed 's/^[^ ]* //')
+
+  guess=()
+  for i in ${!_uctl_commands[@]}; do
+    cmd=${_uctl_commands[$i]}
+    if echo $cmd | grep ^"$cmdline" -q; then
+      arr=($cmd)
+      guess+=(${arr[$COMP_CWORD-1]})
+    fi
+  done
+
+  COMPREPLY=($(compgen -W "${guess[*]}" "${COMP_WORDS[${COMP_CWORD}]}"))
+}
+
+complete -F _uctl_complete uctl
