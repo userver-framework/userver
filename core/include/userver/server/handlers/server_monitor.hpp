523c9bb2785b31e22addcfb9ba69b9484cd99bb8
lfaktorovich <lfaktorovich@userver.tech>
2023-05-04 18:43:32 +0300

    fix documentation: add pretty format

--- core/include/userver/server/handlers/server_monitor.hpp
+++ core/include/userver/server/handlers/server_monitor.hpp
@@ -29,7 +29,7 @@ namespace server::handlers {
 ///
 /// Accepts a path arguments 'format', 'labels', 'path' and 'prefix':
 /// * format - "prometheus", "prometheus-untyped", "graphite",
-///   "json", "solomon" and internal (default) format is
+///   "json", "solomon", "pretty" and internal (default) format is
 ///   supported. For more info see the
 ///   documentation for utils::statistics::ToPrometheusFormat,
 ///   utils::statistics::ToPrometheusFormatUntyped,
