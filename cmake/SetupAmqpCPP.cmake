b0be6953a0d56f2125a5aa20cee46b5f3b07cc3c
Anton Zhilin <antonyzhilin@yandex-team.ru>
2023-05-11 14:04:09 +0300

    refactor cmake: use system gRPCConfig.cmake

--- cmake/SetupAmqpCPP.cmake
+++ cmake/SetupAmqpCPP.cmake
@@ -3,7 +3,8 @@ set(CMAKE_POLICY_DEFAULT_CMP0069 NEW)
 option(USERVER_DOWNLOAD_PACKAGE_AMQPCPP "Download and setup amqp-cpp" ${USERVER_DOWNLOAD_PACKAGES})
 
 if(NOT USERVER_IMPL_USE_DOWNLOADED_AMQPCPP)
-  find_package(amqpcpp)
+  # QUIET because there is no system package we can advise to install.
+  find_package(amqpcpp QUIET)
   if (amqpcpp_FOUND)
     return()
   endif()
