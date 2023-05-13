3d105739259bcab8e8506bc3dc951c4479e5eedf
itrofimow <itrofimow@userver.tech>
2023-05-14 00:58:38 +0300

    feat build: link with asan statically under gcc
    By default gcc links with libasan dynamically, which leads to all sorts of initialization order/loading order problems when we intercept dl_iterate_phdr in exception_hacks.cpp, because ASAN uses that function in initialization to ensure ASAN is the first item in .so list.
    
    Related: https://github.com/scylladb/seastar/commit/37316f1b0890d68a9c57dd241280b5660c3090af

--- cmake/Sanitizers.cmake
+++ cmake/Sanitizers.cmake
@@ -61,7 +61,13 @@ if (USERVER_SANITIZE)
 
     # https://clang.llvm.org/docs/AddressSanitizer.html
     set(SANITIZE_ASAN_ENABLED ON)
-    set(SANITIZE_BUILD_FLAGS ${SANITIZE_BUILD_FLAGS} -fsanitize=address)
+    if (CLANG)
+      set(SANITIZE_BUILD_FLAGS ${SANITIZE_BUILD_FLAGS} -fsanitize=address)
+    else()
+      # gcc links with ASAN dynamically by default, and that leads to all sorts of problems
+      # when we intercept dl_iterate_phdr, which ASAN uses in initialziation before main.
+      set(SANITIZE_BUILD_FLAGS ${SANITIZE_BUILD_FLAGS} -fsanitize=address -static-libasan)
+    endif()
     set(SANITIZE_CXX_FLAGS -fno-omit-frame-pointer)
   endif()
 
