67aca46e2dc33156fd4b3189904f846c951a8145
antoshkka <antoshkka@userver.tech>
2023-06-08 16:36:17 +0300

    feat redis: run pubsub tests without integration tests infrastructure
    Tests: протестировано локально и CI

--- redis/functional_tests/pubsub/tests/test_redis_pubsub.py
+++ redis/functional_tests/pubsub/tests/test_redis_pubsub.py
@@ -1,56 +1,57 @@
-import json
+import asyncio
 
 import pytest
 
 import redis
 
+# Some messages may be lost (it's a Redis limitation)
+REQUESTS_RETRIES = 42
+REQUESTS_RELAX_TIME = 1.0
 
-INPUT_CHANNEL_NAME = (
-    'input_channel'  # target channel. Same constant in c++ code
-)
+# Target channel. Same constant in c++ code
+INPUT_CHANNEL_NAME = 'input_channel'
 
 
-async def do_test_redis_services_pubsub(
-        service_client, redis_services, queued,
-):
+async def _validate_pubsub(redis_db, service_client, msg, queued, redis_type):
+    url = f'/redis-{redis_type}'
+
+    for _ in range(REQUESTS_RETRIES):
+        redis_db.publish(INPUT_CHANNEL_NAME, msg)
+
+        response = await service_client.get(url, params={'queued': queued})
+
+        assert response.status == 200
+        data = response.json()['data']
+
+        if data:
+            assert msg in data
+            await service_client.delete(url)
+            return
+
+        await asyncio.sleep(REQUESTS_RELAX_TIME)
+
+    assert False, 'Retries exceeded'
+
+
+@pytest.mark.skip(reason='Never worked, fix in TAXICOMMON-6778')
+@pytest.mark.parametrize('queued', ['yes', 'no'])
+async def test_happy_path_sentinel(service_client, redis_store, queued):
+    msg = 'sentinel_message'
+    await _validate_pubsub(
+        redis_store, service_client, msg, queued, 'sentinel',
+    )
+
+
+@pytest.mark.parametrize('queued', ['yes', 'no'])
+async def test_happy_path_cluster(service_client, redis_cluster_store, queued):
     async def _test_pubsub(port_number, prefix):
-        msg = f'{prefix}_{redis_number}'
+        msg = f'{prefix}_{port_number}'
 
-        with redis.StrictRedis(
-                host='localhost', port=port_number,
-        ) as redis_connection:
-            redis_connection.publish(INPUT_CHANNEL_NAME, msg)
+        with redis.StrictRedis(host='localhost', port=port_number) as db:
+            await _validate_pubsub(db, service_client, msg, queued, 'cluster')
 
-        result = await service_client.get(
-            '/redis-cluster', params={'queued': queued},
-        )
+    for primary_node in redis_cluster_store.get_primaries():
+        await _test_pubsub(primary_node.port, 'primary')
 
-        data = sorted(json.loads(result)['data'])
-
-        await service_client.delete('/redis-cluster')
-
-        assert data == [msg]
-
-    for redis_number in range(len(redis_services.masters)):
-        _test_pubsub(redis_services.master_port(redis_number), 'master')
-
-    for redis_number in range(len(redis_services.replicas)):
-        _test_pubsub(redis_services.replica_port(redis_number), 'replica')
-
-
-@pytest.mark.parametrize('queued', ['yes', 'no'])
-async def test_happy_path_sentinel(
-        service_client, redis_sentinel_services, queued,
-):
-    do_test_redis_services_pubsub(
-        service_client, redis_sentinel_services, queued,
-    )
-
-
-@pytest.mark.parametrize('queued', ['yes', 'no'])
-async def test_happy_path_cluster(
-        service_client, redis_cluster_services, queued,
-):
-    do_test_redis_services_pubsub(
-        service_client, redis_cluster_services, queued,
-    )
+    for replica_node in redis_cluster_store.get_replicas():
+        await _test_pubsub(replica_node.port, 'replica')
