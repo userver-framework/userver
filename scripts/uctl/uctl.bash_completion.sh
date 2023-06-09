2d6a72ce758e7445b22d48ab7c0e30b373c98258
alexeybykov <alexeybykov@yandex-team.com>
2023-06-09 15:27:10 +0300

    feat uctl: add log lines completion to uctl
    Tested: проверено в тестинге

--- scripts/uctl/uctl.bash_completion.sh
+++ scripts/uctl/uctl.bash_completion.sh
@@ -20,6 +20,12 @@ _uctl_commands=(
   'access-top'
 )
 
+_uctl_log_dynamic_debug_commands=(
+  'log-dynamic-debug force-on'
+  'log-dynamic-debug force-off'
+  'log-dynamic-debug set-default'
+)
+
 _uctl_complete()
 {
   if [ "${#COMP_WORDS[@]}" != "$((1+${COMP_CWORD}))" ]; then
@@ -37,7 +43,23 @@ _uctl_complete()
     fi
   done
 
-  COMPREPLY=($(compgen -W "${guess[*]}" "${COMP_WORDS[${COMP_CWORD}]}"))
+  log_lines=""
+  for i in ${!_uctl_log_dynamic_debug_commands[@]}; do
+    cmd=${_uctl_log_dynamic_debug_commands[$i]}
+    if echo $cmdline | grep ^"$cmd" -q; then
+      cache_path_dir="$HOME/.cache/uctl"
+      cache_path="$cache_path_dir/log_lines.txt"
+      if [ ! -f $cache_path ] || [ $(find $cache_path -mmin +60 -type f -print) ]; then
+        mkdir -p $cache_path_dir
+        $(/usr/bin/${COMP_WORDS[0]} log-dynamic-debug list > $cache_path)
+      fi
+
+      log_lines=$(cat $cache_path | cut -f1 | sed -z 's/\n/ /g')
+      break
+    fi
+  done
+
+  COMPREPLY=($(compgen -W "${guess[*]} $log_lines" "${COMP_WORDS[${COMP_CWORD}]}"))
 }
 
 complete -F _uctl_complete uctl
