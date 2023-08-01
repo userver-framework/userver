0f4f6ccb48a81dbe8bb03e0687410c820fd340fa
antonyzhilin <antonyzhilin@userver.tech>
2023-08-01 19:31:24 +0300

    fix tests: disable flaky gRPC test_close_connection

--- grpc/functional_tests/basic_chaos/tests-grpcclient/test_close.py
+++ grpc/functional_tests/basic_chaos/tests-grpcclient/test_close.py
@@ -5,5 +5,7 @@ import requests_client
 
 @pytest.mark.parametrize('case', requests_client.ALL_CASES)
 async def test_close_clonnection(grpc_ch, service_client, gate, case):
+    if case == 'say_hello_streams':
+        pytest.skip('Flaky TAXICOMMON-7319')
     await requests_client.close_connection(gate, grpc_ch)
     await requests_client.check_200_for(case)(grpc_ch, service_client, gate)
