d5bdd914be9c2cc18f394e54fedea93c1ba049ed
GasikPasik <sarbash2004@gmail.com>
2023-08-06 17:14:21 +0300

    Fix conflict

--- src/handlers/users.cpp
+++ src/handlers/users.cpp
@@ -5,14 +5,28 @@
 #include <userver/server/handlers/http_handler_base.hpp>
 #include "users.h"
 
+#include <userver/storages/postgres/cluster.hpp>
+#include <userver/storages/postgres/component.hpp>
+
+
 namespace service_template {
 
 class Users final : public userver::server::handlers::HttpHandlerBase {
  public:
   static constexpr std::string_view kName = "handler-users";
 
-  using HttpHandlerBase::HttpHandlerBase;
 
+    checkerItem(const userver::components::ComponentConfig& config,
+                const userver::components::ComponentContext& component_context)
+                : HttpHandlerBase(config, component_context),
+                pg_cluster_(
+                    component_context
+                      .FindComponent<userver::components::Postgres>("postgres-db-1")
+                      .GetCluster()) {}
+
+
+  using HttpHandlerBase::HttpHandlerBase;
+  
   std::string HandleRequestThrow(
       const userver::server::http::HttpRequest& request,
       userver::server::request::RequestContext&) const override {
