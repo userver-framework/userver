701172a742371d6d9e839d4294e463f94f80258e
Konstantin Artemev <rumxcola@yandex.ru>
2023-08-06 14:18:13 +0000

    база под докером

---
+++ postgresql/schemas/db_1.sql
@@ -0,0 +1,72 @@
+CREATE EXTENSION IF NOT EXISTS CITEXT;
+CREATE EXTENSION IF NOT EXISTS "uuid-ossp";
+
+DROP SCHEMA IF EXISTS realworld CASCADE;
+CREATE SCHEMA IF NOT EXISTS realworld;
+
+CREATE TABLE IF NOT EXISTS realworld.users (
+    user_id TEXT PRIMARY KEY DEFAULT uuid_generate_v4(),
+	username CITEXT NOT NULL,
+	email VARCHAR(255) NOT NULL,
+	bio TEXT,
+	image VARCHAR(255),
+	password_hash VARCHAR(255) NOT NULL,
+	CONSTRAINT uniq_username UNIQUE(username),
+	CONSTRAINT uniq_email UNIQUE(email)
+);
+
+CREATE TABLE IF NOT EXISTS realworld.articles (
+	article_id TEXT PRIMARY KEY DEFAULT uuid_generate_v4(),
+	title VARCHAR(255) NOT NULL,
+	slug VARCHAR(255) NOT NULL,
+	body TEXT NOT NULL,
+	description TEXT NOT NULL,
+	created_at TIMESTAMPTZ NOT NULL DEFAULT NOW(),
+	updated_at TIMESTAMPTZ NOT NULL DEFAULT NOW(),
+	favorites_count INT,
+	user_id TEXT NOT NULL,
+	CONSTRAINT fk_article_author FOREIGN KEY(user_id) REFERENCES realworld.users(user_id),
+	CONSTRAINT uniq_slug UNIQUE(slug)
+);
+
+CREATE TABLE IF NOT EXISTS realworld.tag_list (
+	tag_id TEXT PRIMARY KEY DEFAULT uuid_generate_v4(),
+	tag_name VARCHAR(255),
+	CONSTRAINT uniq_name UNIQUE(tag_name)
+);
+
+CREATE TABLE IF NOT EXISTS realworld.article_tag (
+	article_id TEXT NOT NULL,
+	tag_id TEXT NOT NULL,
+	CONSTRAINT pk_article_tags PRIMARY KEY(article_id, tag_id),
+	CONSTRAINT fk_article FOREIGN KEY(article_id) REFERENCES realworld.articles(article_id) ON DELETE CASCADE,
+	CONSTRAINT fk_tag FOREIGN KEY(tag_id) REFERENCES realworld.tag_list(tag_id) ON DELETE CASCADE
+);
+
+CREATE TABLE IF NOT EXISTS realworld.favorites (
+	article_id TEXT NOT NULL,
+	user_id TEXT NOT NULL,
+	CONSTRAINT pk_favorites PRIMARY KEY(user_id, article_id),
+	CONSTRAINT fk_user FOREIGN KEY(user_id) REFERENCES realworld.users(user_id),
+	CONSTRAINT fk_article FOREIGN KEY(article_id) REFERENCES realworld.articles(article_id)
+);
+
+CREATE TABLE IF NOT EXISTS realworld.followers (
+	followed_user_id TEXT NOT NULL,
+	follower_user_id TEXT NOT NULL,
+	CONSTRAINT pk_followers PRIMARY KEY(follower_user_id, followed_user_id),
+	CONSTRAINT fk_follower FOREIGN KEY(follower_user_id) REFERENCES realworld.users(user_id),
+	CONSTRAINT fk_followed FOREIGN KEY(followed_user_id) REFERENCES realworld.users(user_id)
+);
+
+CREATE TABLE IF NOT EXISTS realworld.comments (
+	comment_id SERIAL,
+	created_at TIMESTAMPTZ NOT NULL DEFAULT NOW(),
+	updated_at TIMESTAMPTZ NOT NULL DEFAULT NOW(),
+	body VARCHAR(16384) NOT NULL,
+	user_id TEXT NOT NULL,
+	article_id TEXT NOT NULL,
+	CONSTRAINT pk_comments PRIMARY KEY(comment_id),
+	CONSTRAINT fk_article FOREIGN KEY(article_id) REFERENCES realworld.articles(article_id) ON DELETE CASCADE,
+	CONSTRAINT fk_author FOREIGN KEY(user_id) REFERENCES realworld.users(user_id) ON DELETE CASCADE
+);
--- src/main.cpp
+++ src/main.cpp
@@ -3,20 +3,22 @@
 #include <userver/clients/http/component.hpp>
 #include <userver/server/handlers/ping.hpp>
 #include <userver/server/handlers/tests_control.hpp>
+#include <userver/storages/postgres/component.hpp>
 #include <userver/testsuite/testsuite_support.hpp>
 #include <userver/utils/daemon_run.hpp>
 
-#include "handlers/profiles.h"
+#include "hello.hpp"
 
 int main(int argc, char* argv[]) {
   auto component_list = userver::components::MinimalServerComponentList()
                             .Append<userver::server::handlers::Ping>()
                             .Append<userver::components::TestsuiteSupport>()
                             .Append<userver::components::HttpClient>()
+                            .Append<userver::components::Postgres>("realworld-database")
                             .Append<userver::clients::dns::Component>()
                             .Append<userver::server::handlers::TestsControl>();
 
-  service_template::AppendProfiles(component_list);
+  service_template::AppendHello(component_list);
 
   return userver::utils::DaemonMain(argc, argv, component_list);
 }
