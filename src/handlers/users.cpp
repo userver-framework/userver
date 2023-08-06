fa31bbd9dcc14a82d5b3795c76a1048e51353b1b
GasikPasik <sarbash2004@gmail.com>
2023-08-06 17:44:19 +0300

    Compliting db

--- src/handlers/users.cpp
+++ src/handlers/users.cpp
@@ -8,6 +8,8 @@
 #include <userver/storages/postgres/cluster.hpp>
 #include <userver/storages/postgres/component.hpp>
 
+#include <userver/components/component_config.hpp>
+#include <userver/components/component_context.hpp>
 
 namespace service_template {
 
@@ -15,14 +17,13 @@ class Users final : public userver::server::handlers::HttpHandlerBase {
  public:
   static constexpr std::string_view kName = "handler-users";
 
-
-    checkerItem(const userver::components::ComponentConfig& config,
-                const userver::components::ComponentContext& component_context)
-                : HttpHandlerBase(config, component_context),
-                pg_cluster_(
-                    component_context
-                      .FindComponent<userver::components::Postgres>("postgres-db-1")
-                      .GetCluster()) {}
+   Users(const userver::components::ComponentConfig& config,
+                  const userver::components::ComponentContext& component_context)
+            : HttpHandlerBase(config, component_context),
+            pg_cluster_(
+                component_context
+                    .FindComponent<userver::components::Postgres>("postgres-db-1")
+                    .GetCluster()) {}
 
 
   using HttpHandlerBase::HttpHandlerBase;
@@ -32,7 +33,10 @@ class Users final : public userver::server::handlers::HttpHandlerBase {
       userver::server::request::RequestContext&) const override {
     return "";
   }
-};
+
+  userver::storages::postgres::ClusterPtr pg_cluster_;
+
+};  
 
 void AppendUsers(userver::components::ComponentList& component_list) {
   component_list.Append<Users>();
