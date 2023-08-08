d5823b00fff8a7e56ca2bcc9121633a327716e28
Даниил Борискин <Riferus@yandex.ru>
2023-08-08 22:51:26 +0300

    add user login endpoint

--- src/db/sql.hpp
+++ src/db/sql.hpp
@@ -3,6 +3,7 @@
 #include <string_view>
 
 namespace real_medium::sql {
+
 inline constexpr std::string_view kInsertUser = R"~(
 INSERT INTO real_medium.users(username, email, password_hash) 
 VALUES($1, $2, $3)
@@ -10,7 +11,7 @@ RETURNING *
 )~";
 
 inline constexpr std::string_view kSelectUserByEmailAndPassword = R"~(
-SELECT * FROM realworld.users
+SELECT * FROM real_medium.users
 WHERE email = $1 AND password_hash = $2
 )~";
 
