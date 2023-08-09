26fe5ebceb944dae9e757286aac877a0a400bacd
Vadim Romanyuk <vadprof10@gmail.com>
2023-08-09 21:47:27 +0300

    update user registration

--- src/handlers/users/users.cpp
+++ src/handlers/users/users.cpp
@@ -10,7 +10,9 @@
 #include "userver/components/component_context.hpp"
 
 #include "db/sql.hpp"
+#include "dto/user.hpp"
 #include "models/user.hpp"
+#include "utils/make_error.hpp"
 #include "validators/user_validators.hpp"
 
 namespace real_medium::handlers::users::post {
@@ -18,52 +20,51 @@ namespace real_medium::handlers::users::post {
 RegisterUser::RegisterUser(
     const userver::components::ComponentConfig& config,
     const userver::components::ComponentContext& component_context)
-    : HttpHandlerBase(config, component_context),
+    : HttpHandlerJsonBase(config, component_context),
       pg_cluster_(component_context
                       .FindComponent<userver::components::Postgres>(
                           "realmedium-database")
                       .GetCluster()) {}
 
-std::string RegisterUser::HandleRequestThrow(
+userver::formats::json::Value RegisterUser::HandleRequestJsonThrow(
     const userver::server::http::HttpRequest& request,
-    userver::server::request::RequestContext&) const {
-  auto request_body =
-      userver::formats::json::FromString(request.RequestBody())["user"];
+    const userver::formats::json::Value& request_json,
+    userver::server::request::RequestContext& context) const {
+  auto user_register =
+      userver::formats::json::FromString(request.RequestBody())["user"]
+          .As<dto::UserRegistrationDTO>();
 
-  auto username = request_body["username"].As<std::string>();
-  auto email = request_body["email"].As<std::string>();
-  auto password = request_body["password"].As<std::string>();
-
-  if (!validators::ValidateEmail(email)) {
+  if (!validators::ValidateEmail(user_register.email)) {
     auto& response = request.GetHttpResponse();
     response.SetStatus(userver::server::http::HttpStatus::kUnprocessableEntity);
-    return {};
+    return utils::error::MakeError("email", "Wrong email entered");
   }
 
-  if (!validators::ValidatePassword(password)) {
+  if (!validators::ValidatePassword(user_register.password)) {
     auto& response = request.GetHttpResponse();
     response.SetStatus(userver::server::http::HttpStatus::kUnprocessableEntity);
-    return {};
+    return utils::error::MakeError("password", "Wrong password entered");
   }
 
-  auto hash_password = userver::crypto::hash::Sha256(password);
+  auto hash_password = userver::crypto::hash::Sha256(user_register.password);
   models::User result_user;
   try {
     auto query_result = pg_cluster_->Execute(
         userver::storages::postgres::ClusterHostType::kMaster,
-        sql::kInsertUser.data(), username, email, hash_password);
+        sql::kInsertUser.data(), user_register.username, user_register.email,
+        hash_password);
     result_user = query_result.AsSingleRow<models::User>(
         userver::storages::postgres::kRowTag);
   } catch (const userver::storages::postgres::UniqueViolation& ex) {
     auto& response = request.GetHttpResponse();
     response.SetStatus(userver::server::http::HttpStatus::kUnprocessableEntity);
-    return "Constraint: " + ex.GetServerMessage().GetConstraint() +
-           " message: " + ex.GetServerMessage().GetDetail();
+    return utils::error::MakeError(ex.GetServerMessage().GetConstraint(),
+                                   ex.GetServerMessage().GetDetail());
   }
 
   userver::formats::json::ValueBuilder builder;
   builder["user"] = result_user;
-  return userver::formats::json::ToString(builder.ExtractValue());
+  return builder.ExtractValue();
 }
 
 }  // namespace real_medium::handlers::users::post
--- src/handlers/users/users.hpp
+++ src/handlers/users/users.hpp
@@ -6,7 +6,9 @@
 #include <string_view>
 
 #include "users.hpp"
+#include "userver/formats/json/value.hpp"
 #include "userver/server/handlers/http_handler_base.hpp"
+#include "userver/server/handlers/http_handler_json_base.hpp"
 
 #include "userver/storages/postgres/cluster.hpp"
 #include "userver/storages/postgres/component.hpp"
@@ -16,15 +18,17 @@
 
 namespace real_medium::handlers::users::post {
 
-class RegisterUser final : public userver::server::handlers::HttpHandlerBase {
+class RegisterUser final
+    : public userver::server::handlers::HttpHandlerJsonBase {
  public:
   static constexpr std::string_view kName = "handler-register-user";
   RegisterUser(const userver::components::ComponentConfig& config,
                const userver::components::ComponentContext& component_context);
-  std::string HandleRequestThrow(
+  userver::formats::json::Value HandleRequestJsonThrow(
       const userver::server::http::HttpRequest& request,
-      userver::server::request::RequestContext&) const override;
-  using HttpHandlerBase::HttpHandlerBase;
+      const userver::formats::json::Value& request_json,
+      userver::server::request::RequestContext& context) const override final;
+  using HttpHandlerJsonBase::HttpHandlerJsonBase;
 
  private:
   userver::storages::postgres::ClusterPtr pg_cluster_;
