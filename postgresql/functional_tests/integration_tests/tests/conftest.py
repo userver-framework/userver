8e138f5975b6e062a6734a66607306e8948d50d3
bgabbasov <bgabbasov@yandex-team.com>
2023-08-10 16:04:23 +0300

    integration testing framework v3
    integration testing framework v3

--- postgresql/functional_tests/integration_tests/tests/conftest.py
+++ postgresql/functional_tests/integration_tests/tests/conftest.py
@@ -1,34 +1,43 @@
-import pytest
+import pathlib
 
-from testsuite.databases.pgsql import discover
+import pytest_asyncio
+
+import taxi.integration_testing as it
 
 
 pytest_plugins = [
+    'testsuite.pytest_plugin',
+    'pytest_userver.plugins.caches',
     'pytest_userver.plugins.config',
+    'pytest_userver.plugins.dumps',
     'pytest_userver.plugins.dynamic_config',
+    'pytest_userver.plugins.log_capture',
     'pytest_userver.plugins.service',
     'pytest_userver.plugins.service_client',
-    'taxi.integration_testing.pytest_plugin',
-    'taxi.uservices.testsuite.integration_testing.pytest_plugin',
+    'pytest_userver.plugins.testpoint',
 ]
 
 
 USERVER_CONFIG_HOOKS = ['userver_pg_config']
 
 
-@pytest.fixture(scope='session')
-def pgsql_local(service_source_dir, pgsql_local_create):
-    databases = discover.find_schemas(
-        'pg', [service_source_dir.joinpath('schemas/postgresql')],
+@pytest_asyncio.fixture(scope='session')
+async def testenv(
+        service_source_dir: pathlib.Path,
+) -> it.databases.pgsql.EnvPgsqlDocker:
+    env = it.databases.pgsql.EnvPgsqlDocker(pgsql_replicas=1)
+    env.pgsql.discover_schemas(
+        service_source_dir.joinpath('schemas/postgresql'), service_name='pg',
     )
-    return pgsql_local_create(list(databases.values()))
+    async with env.run():
+        yield env
 
 
-@pytest.fixture(scope='session')
-def userver_pg_config(_pgsql_service_settings):
+@pytest_asyncio.fixture(scope='session')
+async def userver_pg_config(testenv: it.databases.pgsql.EnvPgsqlDocker):
+    settings = await testenv.get_pgsql_conn_info()
+
     def _hook_db_config(config_yaml, config_vars):
-        settings = _pgsql_service_settings.get_conninfo()
-
         components = config_yaml['components_manager']['components']
         db = components['key-value-database']
         db['dbconnection'] = (
--- postgresql/functional_tests/integration_tests/tests/test_failover.py
+++ postgresql/functional_tests/integration_tests/tests/test_failover.py
@@ -2,12 +2,16 @@ import asyncio
 
 import pytest
 
+import taxi.integration_testing as it
+
 
 FAILOVER_DEADLINE_SEC = 30  # maximum time allowed to finish failover
 
 
 @pytest.mark.skip(reason='Flacky fix in TAXICOMMON-6756')
-async def test_hard_failover(service_client, postgresql_primary_is_healthy):
+async def test_hard_failover(
+        service_client, testenv: it.databases.pgsql.EnvPgsqlDocker,
+):
     response = await service_client.post('/v1/key-value?key=foo&value=bar')
     assert response.status == 201
     assert response.content == b'bar'
@@ -16,7 +20,8 @@ async def test_hard_failover(service_client, postgresql_primary_is_healthy):
     assert response.status == 200
     assert response.content == b'bar'
 
-    postgresql_primary_is_healthy.container.kill()
+    await asyncio.sleep(30)
+    testenv.pgsql_container.kill()
 
     for _ in range(FAILOVER_DEADLINE_SEC):
         response = await service_client.get('/v1/key-value?key=foo')
