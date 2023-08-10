38e59cc09159a9cc4981dbf9545dc4f9901f1692
Daniil Shvalov <shvalovich@gmail.com>
2023-08-10 14:24:24 +0300

    docs: more details

--- universal/include/userver/utils/meta_light.hpp
+++ universal/include/userver/utils/meta_light.hpp
@@ -102,8 +102,8 @@ template <typename T>
 inline constexpr bool kIsInteger =
     std::is_integral_v<T> && !kIsCharacter<T> && !std::is_same_v<T, bool>;
 
-/// Returns `true` if the type is a true number type (integer or floating point)
-/// `signed char` and `unsigned char` are integer types
+/// Returns `true` if the type is a true number type (integer or floating point,
+/// not `*char*` or `bool`) `signed char` and `unsigned char` are integer types
 template <typename T>
 inline constexpr bool kIsNumber = kIsNumber<T> || std::is_floating_point_v<T>;
 
