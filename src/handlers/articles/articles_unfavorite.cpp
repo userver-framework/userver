ef7f408ccf7d4b665f5d2d9222aa27dde9ee03ea
Nikita Semenov <nikitasemenov0388@gmail.com>
2023-08-13 15:46:24 +0300

    implement DELETE /api/articles/:slug/favorite

---
+++ src/handlers/articles/articles_unfavorite.cpp
@@ -0,0 +1,55 @@
+#include "articles_unfavorite.hpp"
+
+#include "db/sql.hpp"
+#include "dto/article.hpp"
+
+namespace real_medium::handlers::articles_favorite::del {
+
+Handler::Handler(const userver::components::ComponentConfig& config,
+                 const userver::components::ComponentContext& component_context)
+    : HttpHandlerJsonBase(config, component_context),
+      pg_cluster_(component_context
+                      .FindComponent<userver::components::Postgres>(
+                          "realmedium-database")
+                      .GetCluster()) {}
+
+userver::formats::json::Value Handler::HandleRequestJsonThrow(
+    const userver::server::http::HttpRequest& request,
+    const userver::formats::json::Value&,
+    userver::server::request::RequestContext& context) const {
+  auto& user_id = context.GetData<std::string>("id");
+  auto& slug = request.GetPathArg("slug");
+
+  auto transaction =
+      pg_cluster_->Begin("unfavorite_article_transaction",
+                         userver::storages::postgres::ClusterHostType::kMaster,
+                         userver::storages::postgres::Transaction::RW);
+
+  auto res =
+      transaction.Execute(sql::kDeleteFavoritePair.data(), user_id, slug);
+
+  if (!res.IsEmpty()) {
+    auto article_id = res.AsSingleRow<std::string>();
+
+    transaction.Execute(sql::kDecrementFavoritesCount.data(), article_id);
+    transaction.Commit();
+  }
+
+  const auto get_article_res = pg_cluster_->Execute(
+      userver::storages::postgres::ClusterHostType::kSlave,
+      real_medium::sql::kGetArticleWithAuthorProfileBySlug.data(), slug,
+      user_id);
+
+  if (get_article_res.IsEmpty()) {
+    request.SetResponseStatus(userver::server::http::HttpStatus::kNotFound);
+    return {};
+  }
+
+  userver::formats::json::ValueBuilder builder;
+  builder["article"] = real_medium::dto::Article::Parse(
+      get_article_res
+          .AsSingleRow<real_medium::models::TaggedArticleWithProfile>());
+  return builder.ExtractValue();
+}
+
+}  // namespace real_medium::handlers::articles_favorite::del
