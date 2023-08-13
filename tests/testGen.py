fefcad3fad5fbe633d8b55ce999d02eed2824854
GasikPasik <sarbash2004@gmail.com>
2023-08-13 17:29:56 +0300

    a

--- tests/testGen.py
+++ tests/testGen.py
@@ -146,7 +146,7 @@ def fillUsers(con):
 
         dictOfExecute = {"email":emails[i],
                          "username":usernames[i],
-                         "password_hash":randPassHash,
+                         "password_hash":"8d969eef6ecad3c29a3a629280e686cf0c3f5d5a86aff3ca12020c923adc6c92",
                          "bio":randBio,
                          "image":randImage}
         
