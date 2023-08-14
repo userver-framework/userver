c2ab230bca2766f7d3fdff19865f3bd04de0438f
ZenkoWu <107932116+ZenkoWu@users.noreply.github.com>
2023-08-14 17:16:24 +0300

    feat docs: update header style

--- scripts/docs/scripts/header.js
+++ scripts/docs/scripts/header.js
@@ -1,6 +1,7 @@
 const addLinks = () =>  {
     const links = document.createElement('div')
     links.setAttribute('style', 'padding-right: 20px;')
+    links.setAttribute('id', 'links')
     links.innerHTML = `
         <a href="https://github.com/userver-framework/" rel="noopener" target="_blank" class="titlelink">
             <img src="./github_logo.svg" alt="Github" class="gh-logo"/> 
@@ -12,5 +13,24 @@ const addLinks = () =>  {
     document.getElementById('main-nav').after(links)
     const toggler = document.querySelector('[title="Toggle Light/Dark Mode"]')
     document.getElementById('MSearchBox').before(toggler)
+  
 
+}
+
+const addModal = () => {
+    const modalHandler = () => {
+        const modal = document.getElementById('main-nav')
+        const closeBtn = document.getElementsByClassName('sm-dox')[0]
+        if (window.innerWidth < 768) {
+            closeBtn.appendChild(modal)
+        } else {
+            closeBtn.after(modal)
+        }
+    }
+
+    window.addEventListener('resize', () => {
+        modalHandler()
+    })
+
+    modalHandler()
 }
