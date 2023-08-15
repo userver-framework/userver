5c4cb41469d629e6bfa8cdca963b08ce91d6be77
ZenkoWu <107932116+ZenkoWu@users.noreply.github.com>
2023-08-15 13:52:30 +0300

    fix docs: tg and gh header links

--- scripts/docs/scripts/header.js
+++ scripts/docs/scripts/header.js
@@ -1,22 +1,3 @@
-const addLinks = () =>  {
-    const links = document.createElement('div')
-    links.setAttribute('style', 'padding-right: 20px;')
-    links.setAttribute('id', 'links')
-    links.innerHTML = `
-        <a href="https://github.com/userver-framework/" rel="noopener" target="_blank" class="titlelink">
-            <img src="./github_logo.svg" alt="Github" class="gh-logo"/> 
-        </a>
-        <a href="https://t.me/userver_en" rel="noopener" id='telegram_channel' target="_blank" class="titlelink">
-            <img src="./telegram_logo.svg" alt="Telegram"/>
-        </a>
-    `
-    document.getElementById('main-nav').after(links)
-    const toggler = document.querySelector('[title="Toggle Light/Dark Mode"]')
-    document.getElementById('MSearchBox').before(toggler)
-  
-
-}
-
 const addModal = () => {
     const modalHandler = () => {
         const modal = document.getElementById('main-nav')
