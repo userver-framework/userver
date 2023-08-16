8053e8ab64c23c5654a1b11a89e25ccccfbb71e1
Даниил Борискин <Riferus@yandex.ru>
2023-08-16 20:37:26 +0300

    add tests for update user

---
+++ tests/users/test_update_user.py
@@ -0,0 +1,23 @@
+import pytest
+from http import HTTPStatus
+
+from endpoints import register_user, update_user
+from models import User
+from validators import validate_user
+from utils import get_user_token
+
+async def test_update_user(service_client):
+    user = User()
+    response = await register_user(service_client, user)
+    assert response.status == HTTPStatus.OK
+
+    another_user = User()
+    response = await update_user(service_client, another_user, get_user_token(response))
+    assert response.status == HTTPStatus.OK
+    assert validate_user(another_user, response.json())
+
+
+async def test_update_unauthorized(service_client):
+    user = User()
+    response = await update_user(service_client, user, 'some-token')
+    assert response.status == HTTPStatus.UNAUTHORIZED
