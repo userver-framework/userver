556c99883503cc8b10c852a0490b2c439a94689c
Vadim Romanyuk <vadprof10@gmail.com>
2023-08-16 22:51:33 +0300

    add get profile tests

--- tests/helpers/endpoints.py
+++ tests/helpers/endpoints.py
@@ -1,10 +1,12 @@
 from utils import *
 
+
 async def register_user(service_client, user):
     return await service_client.post(
         Routes.REGISTRATION,
         json=model_dump(user, include=RequiredFields.REGISTRATION.value)
     )
+
 
 async def login_user(service_client, user):
     return await service_client.post(
@@ -16,7 +18,7 @@ async def login_user(service_client, user):
 async def get_user(service_client, token):
     return await service_client.get(
         Routes.GET_USER,
-        headers={ 'Authorization' : token },
+        headers={'Authorization': token},
     )
 
 
@@ -24,5 +26,12 @@ async def update_user(service_client, user, token):
     return await service_client.put(
         Routes.UPDATE_USER,
         json=model_dump(user, exclude_none=True),
-        headers={ 'Authorization' : token },
+        headers={'Authorization': token},
+    )
+
+
+async def get_profile(service_client, user, token):
+    return await service_client.get(
+        Routes.GET_PROFILE.format(username=user.username),
+        headers={'Authorization': token}
     )
--- tests/helpers/models.py
+++ tests/helpers/models.py
@@ -4,10 +4,10 @@ from faker import Faker
 fake = Faker()
 fake.seed_instance(4321)
 
+
 class User(BaseModel):
     username: str | None = Field(default_factory=fake.user_name)
-    email : str | None = Field(default_factory=fake.email)
-    password : str | None = Field(default_factory=fake.password)
-    bio : str | None = Field(default_factory=fake.paragraph)
-    image : str | None = Field(default_factory=fake.image_url)
-
+    email: str | None = Field(default_factory=fake.email)
+    password: str | None = Field(default_factory=fake.password)
+    bio: str | None = Field(default_factory=fake.paragraph)
+    image: str | None = Field(default_factory=fake.image_url)
