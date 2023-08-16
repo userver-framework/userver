28aa196895ff65d7392e31801b29f6bf6113f6d9
Даниил Борискин <Riferus@yandex.ru>
2023-08-16 17:04:19 +0300

    fix bio image default_factory

--- tests/helpers/models.py
+++ tests/helpers/models.py
@@ -8,6 +8,6 @@ class User(BaseModel):
     username: str = Field(default_factory=fake.user_name)
     email : str = Field(default_factory=fake.email)
     password : str = Field(default_factory=fake.password)
-    bio : str | None = Field(default_factory=fake.user_name)
-    image : str | None = Field(default_factory=fake.user_name)
+    bio : str | None = Field(default_factory=fake.sentences)
+    image : str | None = Field(default_factory=fake.image_url)
 
