caccf873505fbe1c81aaf9acc5e190fa12ff71c2
Niyaz <nmingaripov@gmail.com>
2023-08-19 15:08:27 +0000

    tests: template

--- core/src/server/handlers/auth/auth_checker_stand_alone_test.cpp
+++ core/src/server/handlers/auth/auth_checker_stand_alone_test.cpp
@@ -8,27 +8,40 @@
 #include <exception>
 #include <string_view>
 
-class StandAloneChecker : public AuthCheckerDigestBaseStandAlone{
-    public:
-    using HA1 = utils::NonLoggable<class HA1Tag, std::string>;
-    std::optional<HA1> GetHA1(const std::string& username) const override {
-        return "939e7578ed9e3c518a452acee763bce9";
-    }
-}
+USERVER_NAMESPACE_BEGIN
+
+namespace server::handlers::auth::test {
+
+class StandAloneChecker : public AuthCheckerDigestBaseStandAlone {
+ public:
+  StandAloneChecker(const AuthDigestSettings& digest_settings, Realm&& realm)
+      : AuthCheckerDigestBaseStandAlone(digest_settings, std::move(realm)){};
+
+  std::optional<HA1> GetHA1(const std::string&) const override {
+    return HA1{"939e7578ed9e3c518a452acee763bce9"};
+  }
+  void PushUnnamedNonce(const Nonce&) const override;
+  bool HasUnnamedNonce(const Nonce&) const override { return false; };
+};
 
 // std::optional<std::string> AuthCheckerDigestBase::CalculateDigest(
 //     const server::http::HttpMethod& request_method,
 //     const DigestContextFromClient& client_context) const
 
 TEST(DigestHashChecker, CalculateDigest) {
-    StandAloneChecker checker;
-    userver::server::http::HttpMethod method = userver::server::http::HttpMethod::kGet;
-    server::handlers::auth::DigestContextFromClient client_context;
-    client_context.username = "Mufasa";
-    client_context.realm = "testrealm@host.com";
-    client_context.cnonce = "0a4f113b";
-    client_context.nc = "00000001";
-    client_context.uri = "/dir/index.html";
-    client_context.qop = "auth";
-    EXPECT_EQ(checker.CalculateDigest(method, client_context),"6629fae49393a05397450978507c4ef1");
+  StandAloneChecker checker;
+  auto method = server::http::HttpMethod::kGet;
+  DigestContextFromClient client_context;
+  client_context.username = "Mufasa";
+  client_context.realm = "testrealm@host.com";
+  client_context.cnonce = "0a4f113b";
+  client_context.nc = "00000001";
+  client_context.uri = "/dir/index.html";
+  client_context.qop = "auth";
+  EXPECT_EQ(checker.CalculateDigest(method, client_context),
+            "6629fae49393a05397450978507c4ef1");
 }
+
+}  // namespace server::handlers::auth::test
+
+USERVER_NAMESPACE_END
