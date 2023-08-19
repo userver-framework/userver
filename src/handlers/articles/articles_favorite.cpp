eea1d6980552bf27d85f314b7375865b19e99794
Vadim Romanyuk <vadprof10@gmail.com>
2023-08-19 11:19:24 +0300

    fix get auth id for favorite article

--- src/handlers/articles/articles_favorite.cpp
+++ src/handlers/articles/articles_favorite.cpp
@@ -16,7 +16,7 @@ userver::formats::json::Value Handler::HandleRequestJsonThrow(
     const userver::server::http::HttpRequest& request,
     const userver::formats::json::Value&,
     userver::server::request::RequestContext& context) const {
-  auto& user_id = context.GetData<std::string>("id");
+  auto& user_id = context.GetData<std::optional<std::string>>("id");
   auto& slug = request.GetPathArg("slug");
 
   auto transaction =
@@ -24,10 +24,8 @@ userver::formats::json::Value Handler::HandleRequestJsonThrow(
                          userver::storages::postgres::ClusterHostType::kMaster,
                          userver::storages::postgres::Transaction::RW);
 
-  auto res = transaction.Execute(
-      sql::kInsertFavoritePair.data(),
-      user_id, slug);
-
+  auto res =
+      transaction.Execute(sql::kInsertFavoritePair.data(), user_id, slug);
 
   if (!res.IsEmpty()) {
     auto article_id = res.AsSingleRow<std::string>();
@@ -38,7 +36,8 @@ userver::formats::json::Value Handler::HandleRequestJsonThrow(
 
   const auto get_article_res = pg_cluster_->Execute(
       userver::storages::postgres::ClusterHostType::kSlave,
-      real_medium::sql::kGetArticleWithAuthorProfileBySlug.data(), slug, user_id);
+      real_medium::sql::kGetArticleWithAuthorProfileBySlug.data(), slug,
+      user_id);
 
   if (get_article_res.IsEmpty()) {
     request.SetResponseStatus(userver::server::http::HttpStatus::kNotFound);
@@ -46,8 +45,7 @@ userver::formats::json::Value Handler::HandleRequestJsonThrow(
   }
 
   userver::formats::json::ValueBuilder builder;
-  builder["article"] =
-      real_medium::dto::Article::Parse(
+  builder["article"] = real_medium::dto::Article::Parse(
       get_article_res
           .AsSingleRow<real_medium::models::TaggedArticleWithProfile>());
   return builder.ExtractValue();
