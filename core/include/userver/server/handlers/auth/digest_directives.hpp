6f75329e0619d0f6e09f8ed69556b8110546f077
Konstantin Antoniadi <kos-antoniadi@yandex.ru>
2023-08-20 10:08:37 +0000

    digest_algorithm: change to digest_directives and add qop types

--- core/include/userver/server/handlers/auth/digest_directives.hpp
+++ core/include/userver/server/handlers/auth/digest_directives.hpp
@@ -8,12 +8,21 @@ namespace server::handlers::auth {
 
 enum class HashAlgTypes { kMD5, kSHA256, kSHA512, kUnknown };
 
+enum class QopTypes { kAuth, kUnknown };
+
 constexpr utils::TrivialBiMap kHashAlgToType = [](auto selector) {
   return selector()
       .Case("md5", HashAlgTypes::kMD5)
       .Case("sha256", HashAlgTypes::kSHA256)
       .Case("sha512", HashAlgTypes::kSHA512);
 };
+
+constexpr utils::TrivialBiMap kQopToType = [](auto selector) {
+  return selector()
+      .Case("auth", QopTypes::kAuth);
+};
+
+// enum 
 
 }  // namespace server::handlers::auth
 
