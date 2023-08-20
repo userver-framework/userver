ec48cce1815bf345d43d4021bf121528097412af
Konstantin Antoniadi <kos-antoniadi@yandex.ru>
2023-08-20 14:57:21 +0000

    digest directives: add documentation

--- core/include/userver/server/handlers/auth/digest_directives.hpp
+++ core/include/userver/server/handlers/auth/digest_directives.hpp
@@ -6,9 +6,20 @@ USERVER_NAMESPACE_BEGIN
 
 namespace server::handlers::auth {
 
-enum class HashAlgTypes { kMD5, kSHA256, kSHA512, kUnknown };
+/// @brief Supported hashing algorithms
+enum class HashAlgTypes {
+  kMD5,     ///< MD5 algorithm
+  kSHA256,  ///< SHA256 algorithm
+  kSHA512,  ///< SHA512 algortihm
+  kUnknown  ///< Unknown algorithm
+};
 
-enum class QopTypes { kAuth, kUnknown };
+/// @brief Supported qop-options
+enum class QopTypes {
+  kAuth,     ///< `The value "auth" indicates authentication` from
+             ///< https://datatracker.ietf.org/doc/html/rfc2617#section-3.2.1
+  kUnknown,  ///< Unknown qop-value
+};
 
 constexpr utils::TrivialBiMap kHashAlgToType = [](auto selector) {
   return selector()
@@ -18,11 +29,10 @@ constexpr utils::TrivialBiMap kHashAlgToType = [](auto selector) {
 };
 
 constexpr utils::TrivialBiMap kQopToType = [](auto selector) {
-  return selector()
-      .Case("auth", QopTypes::kAuth);
+  return selector().Case("auth", QopTypes::kAuth);
 };
 
-// enum 
+// enum
 
 }  // namespace server::handlers::auth
 
