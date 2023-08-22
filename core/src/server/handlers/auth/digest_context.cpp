94cb565f46a339ed232dc028b14c54459598c2e1
bashkirian <exaggeratethismotion@gmail.com>
2023-08-22 20:32:36 +0300

    newline and std::string constructor

--- core/src/server/handlers/auth/digest_context.cpp
+++ core/src/server/handlers/auth/digest_context.cpp
@@ -10,17 +10,17 @@ namespace server::handlers::auth {
 DigestContextFromClient Parse(
     std::unordered_map<std::string, std::string> directive_mapping) {
   return DigestContextFromClient{
-      directive_mapping[directives::kUsername.data()],
-      directive_mapping[directives::kRealm.data()],
-      directive_mapping[directives::kNonce.data()],
-      directive_mapping[directives::kUri.data()],
-      directive_mapping[directives::kResponse.data()],
-      directive_mapping[directives::kAlgorithm.data()],
-      directive_mapping[directives::kCnonce.data()],
-      directive_mapping[directives::kOpaque.data()],
-      directive_mapping[directives::kQop.data()],
-      directive_mapping[directives::kNonceCount.data()],
-      directive_mapping[directives::kAuthParam.data()],
+      directive_mapping[std::string(std::string(directives::kUsername.data()))],
+      directive_mapping[std::string(directives::kRealm.data())],
+      directive_mapping[std::string(directives::kNonce.data())],
+      directive_mapping[std::string(directives::kUri.data())],
+      directive_mapping[std::string(directives::kResponse.data())],
+      directive_mapping[std::string(directives::kAlgorithm.data())],
+      directive_mapping[std::string(directives::kCnonce.data())],
+      directive_mapping[std::string(directives::kOpaque.data())],
+      directive_mapping[std::string(directives::kQop.data())],
+      directive_mapping[std::string(directives::kNonceCount.data())],
+      directive_mapping[std::string(directives::kAuthParam.data())],
   };
 }
 
