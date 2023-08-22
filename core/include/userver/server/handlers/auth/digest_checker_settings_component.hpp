fe568c6ee85c9d825cf47574e9d297e3b346ef02
Konstantin Antoniadi <kos-antoniadi@yandex.ru>
2023-08-23 00:17:20 +0300

    feat server: implement component for digest auth
    This PR is part of Digest Checker project completed during Yandex Summer school of Backend Development.
    In the process of working on the Digest Checker, it became necessary to write a component for digest auth settings for directives:
    - algorithm
    - domains
    - qops
    - is_proxy
    - is_session
    - nonce_ttl
    
    Tests: протестировано CI
    
    Pull Request resolved: #340

---
+++ core/include/userver/server/handlers/auth/digest_checker_settings_component.hpp
@@ -0,0 +1,56 @@
+#pragma once
+
+/// @file userver/server/handlers/auth/digest_checker_settings_component.hpp
+/// @brief @copybrief server::handlers::auth::DigestCheckerSettingsComponent
+
+#include <chrono>
+#include <optional>
+#include <string>
+
+#include <userver/components/loggable_component_base.hpp>
+#include <userver/dynamic_config/source.hpp>
+
+#include "auth_digest_settings.hpp"
+
+USERVER_NAMESPACE_BEGIN
+
+namespace server::handlers::auth {
+
+// clang-format off
+
+/// @brief Component that loads digest auth configuration settings from a
+/// static_config.yaml
+/// ## Static options:
+///
+/// Name       | Description                                          | Default value
+/// ---------- | ---------------------------------------------------- | -------------
+/// algorithm  | algorithm for hashing nonce                          | md5
+/// domains    | domains for use                                      | /
+/// qops       | list of qop-options                                  | auth
+/// is-proxy   | if set, the Proxy prefix is inserted into the header | false
+/// is-session | enable sessions                                      | false
+/// nonce-ttl  | ttl for nonce                                        | 10s
+
+// clang-format on
+
+class DigestCheckerSettingsComponent final
+    : public components::LoggableComponentBase {
+ public:
+  static constexpr std::string_view kName = "auth-digest-checker-settings";
+
+  DigestCheckerSettingsComponent(const components::ComponentConfig& config,
+                                 const components::ComponentContext& context);
+
+  ~DigestCheckerSettingsComponent() final;
+
+  const AuthDigestSettings& GetSettings() const;
+
+  static yaml_config::Schema GetStaticConfigSchema();
+
+ private:
+  AuthDigestSettings settings_;
+};
+
+}  // namespace server::handlers::auth
+
+USERVER_NAMESPACE_END
