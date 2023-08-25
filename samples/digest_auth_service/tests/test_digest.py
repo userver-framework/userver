685d28c9d98d695cd6b292d905015aff76d16e84
bashkirian <exaggeratethismotion@gmail.com>
2023-08-25 11:47:02 +0300

    moved auth functions to separate file

--- samples/digest_auth_service/tests/test_digest.py
+++ samples/digest_auth_service/tests/test_digest.py
@@ -1,32 +1,6 @@
 # /// [Functional test]
 import pytest
-from requests.auth import HTTPDigestAuth
-import re
-
-
-# parsing regex
-reg = re.compile(r'(\w+)[:=][\s"]?([^",]+)"?')
-
-
-# construct challenge structer
-def construct_challenge(auth_directives: dict, nonce=''):
-    return {'realm': auth_directives["realm"],
-            'nonce': auth_directives['nonce'] if len(nonce) == 0 else nonce,
-            'algorithm': auth_directives["algorithm"],
-            'qop': "auth"
-            }
-
-
-# construct authorization header sent from client
-def construct_header(username: str, password: str, challenge: dict):
-    digest_auth = HTTPDigestAuth(username, password)
-    digest_auth.init_per_thread_state()
-    digest_auth._thread_local.chal = challenge
-    return digest_auth.build_digest_header('GET', '/v1/hello')
-
-
-def parse_directives(authentication_header: str):
-    return dict(reg.findall(authentication_header))
+from auth_utils import *
 
 
 @pytest.mark.pgsql('auth', files=['test_data.sql'])
