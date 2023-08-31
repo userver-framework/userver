456b82fd857e8fcfe338b7edbbb664dee55f9151
segoon <segoon@userver.tech>
2023-08-31 20:00:43 +0300

    bug grpc: fix access-tskv.log

--- grpc/src/ugrpc/server/rpc.cpp
+++ grpc/src/ugrpc/server/rpc.cpp
@@ -6,6 +6,7 @@
 #include <userver/logging/logger.hpp>
 #include <userver/utils/datetime.hpp>
 #include <userver/utils/encoding/tskv.hpp>
+#include <userver/utils/text.hpp>
 
 #include <ugrpc/impl/internal_tag.hpp>
 
@@ -21,6 +22,19 @@ std::string EscapeForAccessTskvLog(std::string_view str) {
   std::string encoded_str;
   EncodeTskv(encoded_str, str, utils::encoding::EncodeTskvMode::kValue);
   return encoded_str;
+}
+
+std::string ParseIp(std::string_view sv) {
+  static constexpr std::string_view kIpv6 = "ipv6:";
+  if (utils::text::StartsWith(sv, kIpv6)) sv = sv.substr(kIpv6.size());
+
+  auto pos1 = sv.find("%5B");
+  auto pos2 = sv.find("%5D");
+  if (pos1 != std::string::npos && pos2 != std::string::npos) {
+    sv = sv.substr(pos1 + 3, pos2 - pos1 - 3);
+  }
+
+  return std::string{sv};
 }
 
 }  // namespace
@@ -44,8 +58,7 @@ void CallAnyBase::LogFinish(grpc::Status status) const {
     user_agent = std::string(ref.begin(), ref.end());
   }
 
-  // TODO: extract IP
-  auto ip = params_.context.peer();
+  auto ip = ParseIp(params_.context.peer());
 
   auto start_time = params_.call_span.GetStartSystemTime();
 
@@ -60,7 +73,7 @@ void CallAnyBase::LogFinish(grpc::Status status) const {
                   "\t{}"
                   "\tuser_agent={}"
                   "\tip={}"
-                  "\treal_ip={}"
+                  "\tx_real_ip={}"
                   "\trequest={}"
                   "\tupstream_response_time_ms={}"
                   "\tgrpc_status={}\n",
