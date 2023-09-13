f6e55efdcceaf0a9f33064d7ee910c8b12d14581
antoshkka <antoshkka@userver.tech>
2023-09-13 19:56:42 +0300

    feat universal: support new versions of yaml-cpp
    Fixes: https://github.com/userver-framework/userver/issues/371
    Tests: протестировано локально и CI

--- universal/src/formats/yaml/string_view_support.hpp
+++ universal/src/formats/yaml/string_view_support.hpp
@@ -1,19 +1,65 @@
 #pragma once
 
 #include <string_view>
+#include <type_traits>
 
 #include <yaml-cpp/yaml.h>
 
-namespace YAML {
+#include <userver/utils/assert.hpp>
 
-// Makes rapidjson::GenericValue accept std::string_view as keys
-template <>
-struct convert<std::string_view> {
-  static Node encode(const std::string_view& rhs) {
-    return Node(std::string{rhs});
+// yaml-cpp library added convert<std::string_view> specialization at some
+// point. We have to support old and new versions of the library, so the
+// following workaround is required:
+
+USERVER_NAMESPACE_BEGIN
+
+namespace impl {
+
+template <class T>
+constexpr auto IsSpecialized() -> decltype(sizeof(T), std::true_type{}) {
+  return {};
+}
+
+template <class T, class... Args>
+constexpr std::false_type IsSpecialized(const Args&...) {
+  return {};
+}
+
+struct SomeFake {
+  explicit operator std::string() const noexcept {
+    UASSERT_MSG(false,
+                "YAML specialization of convert<std::string_view> is broken. "
+                "Contact the userver maintainers");
+    return {};
   }
 
-  static bool decode(const Node& node, std::string_view& rhs) {
+  template <class T>
+  SomeFake& operator=(const T&) {
+    UASSERT_MSG(false,
+                "YAML specialization of convert<std::string_view> is broken. "
+                "Contact the userver maintainers");
+    return *this;
+  }
+};
+
+using StringViewOrSomeFake =
+    std::conditional_t<impl::IsSpecialized<::YAML::convert<std::string_view>>(),
+                       SomeFake, std::string_view>;
+
+}  // namespace impl
+
+USERVER_NAMESPACE_END
+
+namespace YAML {
+
+// Makes YAML work with std::string_view as keys
+template <>
+struct convert<USERVER_NAMESPACE::impl::StringViewOrSomeFake> {
+  using ConversionType = USERVER_NAMESPACE::impl::StringViewOrSomeFake;
+
+  static Node encode(ConversionType rhs) { return Node(std::string{rhs}); }
+
+  static bool decode(const Node& node, ConversionType& rhs) {
     if (!node.IsScalar()) return false;
     rhs = node.Scalar();
     return true;
