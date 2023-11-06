0f8ee00b8d84f3b3a58cd7165e937a2d2e82c6a0
itrofimow <itrofimow@userver.tech>
2023-11-07 02:12:27 +0300

    feat postgresql: optimize ResultSet parsing/validation

--- postgresql/src/storages/postgres/detail/result_wrapper.cpp
+++ postgresql/src/storages/postgres/detail/result_wrapper.cpp
@@ -34,58 +34,114 @@ constexpr std::pair<const char*, int> kExtraErrorFields[]{
     {"pg_datatype", PG_DIAG_DATATYPE_NAME},
     {"pg_constraint", PG_DIAG_CONSTRAINT_NAME}};
 
-struct CurrentContext {
-  CurrentContext(std::vector<std::string>& context, std::string&& ctx)
+// A scope we use to manage a type-name "path".
+// Only used when we've encountered an unknown buffer category.
+class TypeNameScope final {
+ public:
+  template <typename Fn>
+  TypeNameScope(std::vector<std::string>& context, Fn&& ctx_factory)
       : context_{context} {
-    context_.emplace_back(std::move(ctx));
+    context_.emplace_back(ctx_factory());
   }
-  ~CurrentContext() { context_.pop_back(); }
+  ~TypeNameScope() { context_.pop_back(); }
+
+  static bool Fail(const std::vector<std::string>& context, Oid data_type) {
+    throw UnknownBufferCategory(fmt::to_string(fmt::join(context, " ")),
+                                data_type);
+  }
 
  private:
   std::vector<std::string>& context_;
 };
 
-void AddTypeBufferCategories(Oid data_type, const UserTypes& types,
-                             io::TypeBufferCategory& cats,
-                             std::vector<std::string>& context) {
+// A no-op scope we use when validating buffer categories.
+// If we encounter a parsing failure the TypeNameScope is used.
+class NoopTypeNameScope final {
+ public:
+  template <typename Fn>
+  NoopTypeNameScope(std::vector<std::string>&, Fn&&) {}
+
+  static bool Fail(const std::vector<std::string>&, Oid) { return false; }
+};
+
+template <typename ScopeType>
+bool DoAddTypeBufferCategories(Oid data_type, const UserTypes& types,
+                               io::TypeBufferCategory& cats,
+                               std::vector<std::string>& context) {
   if (cats.count(data_type)) {
-    return;
+    return true;
   }
   auto cat = io::GetBufferCategory(static_cast<io::PredefinedOids>(data_type));
   if (cat == io::BufferCategory::kNoParser) {
     cat = types.GetBufferCategory(data_type);
     if (cat == io::BufferCategory::kNoParser) {
-      throw UnknownBufferCategory(fmt::to_string(fmt::join(context, " ")),
-                                  data_type);
+      return ScopeType::Fail(context, data_type);
     }
   }
+
   cats.insert(std::make_pair(data_type, cat));
+
   if (cat == io::BufferCategory::kArrayBuffer) {
     // Recursively add buffer category for array element
     auto elem_oid = types.FindElementOid(data_type);
-    CurrentContext ctx{context, "array element"};
-    AddTypeBufferCategories(elem_oid, types, cats, context);
+    const ScopeType path_scope{context,
+                               [] { return std::string{"array element"}; }};
+    return DoAddTypeBufferCategories<ScopeType>(elem_oid, types, cats, context);
   } else if (cat == io::BufferCategory::kCompositeBuffer) {
     if (data_type == static_cast<Oid>(io::PredefinedOids::kRecord)) {
       // record is opaque and doesn't have a description
       // load buffer categories for all user types
       const auto& user_categories = types.GetTypeBufferCategories();
       cats.insert(user_categories.begin(), user_categories.end());
-      return;
+      return true;
     }
 
     // Recursively add buffer categories for data members
     const auto& type_desc = types.GetCompositeDescription(data_type);
     auto type_name = types.FindName(data_type);
-    CurrentContext ctx{
-        context, fmt::format(FMT_COMPILE("type `{}`"), type_name.ToString())};
+    const ScopeType path_scope{context, [&type_name] {
+                                 return fmt::format(FMT_COMPILE("type `{}`"),
+                                                    type_name.ToString());
+                               }};
     auto n_fields = type_desc.Size();
     for (std::size_t f_no = 0; f_no < n_fields; ++f_no) {
-      CurrentContext ctx{context, fmt::format(FMT_COMPILE("field `{}`"),
-                                              type_desc[f_no].name)};
-      AddTypeBufferCategories(type_desc[f_no].type, types, cats, context);
+      const ScopeType child_scope{
+          context, [&type_desc, f_no] {
+            return fmt::format(FMT_COMPILE("field `{}`"), type_desc[f_no].name);
+          }};
+      if (!DoAddTypeBufferCategories<ScopeType>(type_desc[f_no].type, types,
+                                                cats, context)) {
+        return false;
+      }
     }
   }
+
+  return true;
+}
+
+template <typename ScopeType, typename GetFieldTypeOidFn,
+          typename GetFieldNameFn>
+bool AddTypeBufferCategories(std::size_t n_fields,
+                             const GetFieldTypeOidFn& get_field_type_oid_fn,
+                             const GetFieldNameFn& get_field_name_fn,
+                             const UserTypes& types,
+                             io::TypeBufferCategory& buffer_categories) {
+  std::vector<std::string> context{};
+
+  for (std::size_t f_no = 0; f_no < n_fields; ++f_no) {
+    auto data_type = get_field_type_oid_fn(f_no);
+    ScopeType path_start_scope{context, [&get_field_name_fn, f_no] {
+                                 return fmt::format("result set field `{}`",
+                                                    get_field_name_fn(f_no));
+                               }};
+
+    if (!DoAddTypeBufferCategories<ScopeType>(data_type, types,
+                                              buffer_categories, context)) {
+      return false;
+    }
+  }
+
+  return true;
 }
 
 }  // namespace
@@ -102,14 +158,30 @@ ResultWrapper::~ResultWrapper() = default;
 
 void ResultWrapper::FillBufferCategories(const UserTypes& types) {
   buffer_categories_.clear();
-  auto n_fields = FieldCount();
-  for (std::size_t f_no = 0; f_no < n_fields; ++f_no) {
-    auto data_type = GetFieldTypeOid(f_no);
 
-    std::vector<std::string> context{
-        fmt::format("result set field `{}`", GetFieldName(f_no))};
+  const auto n_fields = FieldCount();
+  buffer_categories_.reserve(n_fields);
 
-    AddTypeBufferCategories(data_type, types, buffer_categories_, context);
+  const auto oid_fn = [this](auto f_no) { return GetFieldTypeOid(f_no); };
+  const auto name_fn = [this](auto f_no) { return GetFieldName(f_no); };
+
+  // We have to throw an informative exception if some (potentially deeply
+  // nested) type doesn't have a parser.
+  //
+  // The problem: for that exception to be of use we have to preserve a path to
+  // the field, which implies a vector of path chunks, lots of fmt::format-s,
+  // allocations etc., and is NEVER used when everything goes according to plan.
+  //
+  // The solution: run the recursive parsing twice, the first attempt is
+  // performed without preserving paths, and iff it fails the second attempt
+  // re-runs the process from scratch with all the info it needs,
+  // and is guaranteed to throw.
+  if (!AddTypeBufferCategories<NoopTypeNameScope>(n_fields, oid_fn, name_fn,
+                                                  types, buffer_categories_)) {
+    buffer_categories_.clear();
+    AddTypeBufferCategories<TypeNameScope>(n_fields, oid_fn, name_fn, types,
+                                           buffer_categories_);
+    UINVARIANT(false, "Should be unreachable");
   }
 
   cached_buffer_categories_->data.resize(n_fields);
