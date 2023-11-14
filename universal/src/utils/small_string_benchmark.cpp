7f3ba071f99017b6b72fb0789e1dabc0ab9d5f6f
Yobama-png <i.oplachkin@gmail.com>
2023-11-14 15:24:04 +0300

    feat universal: implement small_string::resize_and_overwrite
    Имплементация метода resize_and_overwrite из #406
    Улучшение производительности:
    
    ```
    Benchmark                                    Time             CPU   Iterations
    
    SmallStringResizeAndOverwrite/2           5.01 us         5.03 us       100485
    SmallStringResizeAndOverwrite/8           4.43 us         4.45 us       157111
    SmallStringResizeAndOverwrite/64          5.20 us         5.24 us       132817
    SmallStringResizeAndOverwrite/512         15.9 us         16.0 us        45383
    SmallStringResizeAndOverwrite/2048         206 us          207 us         3385
    
    SmallStringResizeThenOverwrite/2          6.90 us         6.92 us       106967
    SmallStringResizeThenOverwrite/8          6.29 us         6.31 us       114793
    SmallStringResizeThenOverwrite/64         5.77 us         5.81 us        92461
    SmallStringResizeThenOverwrite/512        19.9 us         20.0 us        37919
    SmallStringResizeThenOverwrite/2048        288 us          289 us         2435
    
    ```
    
    Tests: протестировано CI
    
    Pull Request resolved: https://github.com/userver-framework/userver/pull/431

--- universal/src/utils/small_string_benchmark.cpp
+++ universal/src/utils/small_string_benchmark.cpp
@@ -2,6 +2,8 @@
 
 #include <array>
 
+#include <utils/gbench_auxilary.hpp>
+
 #include <benchmark/benchmark.h>
 
 USERVER_NAMESPACE_BEGIN
@@ -14,7 +16,7 @@ std::string GenerateString(size_t size) {
   std::string_view chars{"0123456789"};
   std::string result;
   for (size_t i = 0; i < size; i++) result += chars[i % 10];
-  return result;
+  return Launder(std::move(result));
 }
 
 static void SmallString_Std(benchmark::State& state) {
@@ -96,6 +98,46 @@ static void SmallString_Small_Move(benchmark::State& state) {
   }
 }
 BENCHMARK(SmallString_Small_Move)
+    ->Range(2, 2 << 10)
+    ->Unit(benchmark::kMicrosecond);
+
+static void SmallStringResizeAndOverwrite(benchmark::State& state) {
+  auto s = GenerateString(state.range(0));
+  std::array<utils::SmallString<1000>, kArraySize> str;
+  std::array<utils::SmallString<1>, kArraySize> str2;
+  for (auto& x : str) x = s;
+  for ([[maybe_unused]] auto _ : state) {
+    for (size_t i = 0; i < str.size(); i++)
+      str2[i].resize_and_overwrite(
+          str[i].size(), [&](char* data_, size_t size) {
+            std::copy(str[i].data(), str[i].data() + str[i].size(), data_);
+            return size;
+          });
+    state.PauseTiming();
+    str2.fill({});
+    state.ResumeTiming();
+  }
+}
+BENCHMARK(SmallStringResizeAndOverwrite)
+    ->Range(2, 2 << 10)
+    ->Unit(benchmark::kMicrosecond);
+
+static void SmallStringResizeThenOverwrite(benchmark::State& state) {
+  auto s = GenerateString(state.range(0));
+  std::array<utils::SmallString<1000>, kArraySize> str;
+  std::array<utils::SmallString<1>, kArraySize> str2;
+  for (auto& x : str) x = s;
+  for ([[maybe_unused]] auto _ : state) {
+    for (size_t i = 0; i < str.size(); i++) {
+      str2[i].resize(str[i].size(), '\0');
+      std::copy(str[i].data(), str[i].data() + str[i].size(), str2[i].data());
+    }
+    state.PauseTiming();
+    str2.fill({});
+    state.ResumeTiming();
+  }
+}
+BENCHMARK(SmallStringResizeThenOverwrite)
     ->Range(2, 2 << 10)
     ->Unit(benchmark::kMicrosecond);
 
--- universal/src/utils/small_string_test.cpp
+++ universal/src/utils/small_string_test.cpp
@@ -10,6 +10,14 @@
 
 USERVER_NAMESPACE_BEGIN
 
+struct CheckRvalueCall {
+  std::size_t operator()(char*, std::size_t s) const& {
+    ADD_FAILURE() << "Wrond overload called";
+    return s;
+  }
+  std::size_t operator()(char*, std::size_t s) && { return s; }
+};
+
 TEST(SmallString, Empty) {
   utils::SmallString<10> str;
   EXPECT_TRUE(str.empty());
@@ -68,6 +76,28 @@ TEST(SmallString, SizeCapacity) {
   auto capacity = str.capacity();
   str.reserve(capacity + 1);
   EXPECT_GT(str.capacity(), capacity);
+}
+
+TEST(SmallString, ResizeAndOverwrite) {
+  utils::SmallString<4> small_str("abcd");
+
+  std::size_t count = 3;
+  std::string str = "mnkp";
+
+  small_str.resize_and_overwrite(16, [&](char* data,
+                                         [[maybe_unused]] size_t size) {
+    for (size_t ind = 0; ind < count; ++ind) {
+      std::copy(str.data(), str.data() + str.size(), data + ind * str.size());
+    }
+    return count * str.size();
+  });
+  EXPECT_EQ(small_str, "mnkpmnkpmnkp");
+}
+
+TEST(SmallString, ResizeAndOverwriteRvalueCall) {
+  utils::SmallString<4> small_str("abcd");
+
+  small_str.resize_and_overwrite(16, CheckRvalueCall());
 }
 
 TEST(SmallString, Assign) {
