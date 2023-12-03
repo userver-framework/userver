26822af2ce9d983b285ce67c8b622ba7a4818687
gemut1ich <gemut1ich@users.noreply.github.com>
2023-12-03 18:34:44 -0500

    clang-format the files changed

--- core/include/userver/clients/http/form.hpp
+++ core/include/userver/clients/http/form.hpp
@@ -7,36 +7,36 @@ USERVER_NAMESPACE_BEGIN
 
 namespace curl {
 class form;
-} // namespace curl
+}  // namespace curl
 
 namespace clients::http {
 
 class Form final {
-public:
+ public:
   Form();
   ~Form();
 
-  Form(const Form &) = delete;
-  Form(Form &&) noexcept;
-  Form &operator=(const Form &) = delete;
-  Form &operator=(Form &&) noexcept;
+  Form(const Form&) = delete;
+  Form(Form&&) noexcept;
+  Form& operator=(const Form&) = delete;
+  Form& operator=(Form&&) noexcept;
 
   void AddContent(std::string_view key, std::string_view content);
   void AddContent(std::string_view key, std::string_view content,
-                  const std::string &content_type);
+                  const std::string& content_type);
 
-  void AddBuffer(const std::string &key, const std::string &file_name,
-                 const std::shared_ptr<std::string> &buffer);
-  void AddBuffer(const std::string &key, const std::string &file_name,
-                 const std::shared_ptr<std::string> &buffer,
-                 const std::string &content_type);
+  void AddBuffer(const std::string& key, const std::string& file_name,
+                 const std::shared_ptr<std::string>& buffer);
+  void AddBuffer(const std::string& key, const std::string& file_name,
+                 const std::shared_ptr<std::string>& buffer,
+                 const std::string& content_type);
 
-  curl::form *GetNative() const;
+  curl::form* GetNative() const;
 
-private:
+ private:
   std::unique_ptr<curl::form> impl_;
 };
 
-} // namespace clients::http
+}  // namespace clients::http
 
 USERVER_NAMESPACE_END
--- core/src/clients/http/form.cpp
+++ core/src/clients/http/form.cpp
@@ -9,31 +9,31 @@ namespace clients::http {
 Form::Form() : impl_(std::make_unique<curl::form>()) {}
 Form::~Form() = default;
 
-Form::Form(Form &&) noexcept = default;
-Form &Form::operator=(Form &&) noexcept = default;
+Form::Form(Form&&) noexcept = default;
+Form& Form::operator=(Form&&) noexcept = default;
 
 void Form::AddContent(std::string_view key, std::string_view content) {
   impl_->add_content(key, content);
 }
 
 void Form::AddContent(std::string_view key, std::string_view content,
-                      const std::string &content_type) {
+                      const std::string& content_type) {
   impl_->add_content(key, content, content_type);
 }
 
-void Form::AddBuffer(const std::string &key, const std::string &file_name,
-                     const std::shared_ptr<std::string> &buffer) {
+void Form::AddBuffer(const std::string& key, const std::string& file_name,
+                     const std::shared_ptr<std::string>& buffer) {
   impl_->add_buffer(key, file_name, buffer);
 }
 
-void Form::AddBuffer(const std::string &key, const std::string &file_name,
-                     const std::shared_ptr<std::string> &buffer,
-                     const std::string &content_type) {
+void Form::AddBuffer(const std::string& key, const std::string& file_name,
+                     const std::shared_ptr<std::string>& buffer,
+                     const std::string& content_type) {
   impl_->add_buffer(key, file_name, buffer, content_type);
 }
 
-curl::form *Form::GetNative() const { return impl_.get(); }
+curl::form* Form::GetNative() const { return impl_.get(); }
 
-} // namespace clients::http
+}  // namespace clients::http
 
 USERVER_NAMESPACE_END
