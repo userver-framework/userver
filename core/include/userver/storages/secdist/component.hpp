7ca798b340ec6a37c94bc0a2ac5459eb674f3453
arudenko02 <arudenko02@yandex-team.com>
2023-12-06 18:32:40 +0300

    fix samples: fix static configuration for secdist
    use provider in static configuration and add docs

--- core/include/userver/storages/secdist/component.hpp
+++ core/include/userver/storages/secdist/component.hpp
@@ -19,9 +19,17 @@ namespace components {
 ///
 /// The component must be configured in service config.
 ///
+/// Secdist requires a provider storages::secdist::SecdistProvider
+/// You can implement your own or use components::DefaultSecdistProvider
+///
+/// ## Static configuration example:
+///
+/// @snippet samples/redis_service/static_config.yaml Sample secdist static config
+///
 /// ## Static options:
 /// Name | Description | Default value
 /// ---- | ----------- | -------------
+/// provider | optional secdist provider component name | 'default-secdist-provider'
 /// config | path to the config file with data | ''
 /// format | config format, either `json` or `yaml` | 'json'
 /// missing-ok | do not terminate components load if no file found by the config option | false
