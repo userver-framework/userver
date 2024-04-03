9328deb8a362bdc019be4730e8af9e5cb69cb5a6
akhoroshev <arthoroshev@gmail.com>
2024-04-03 11:14:19 +0300

    fix race condition

--- core/include/userver/concurrent/queue.hpp
+++ core/include/userver/concurrent/queue.hpp
@@ -236,6 +236,8 @@ class GenericQueue final
   void PrepareProducer() {
     std::size_t old_producers_count{};
     utils::AtomicUpdate(producers_count_, [&](auto old_value) {
+      UINVARIANT(QueuePolicy::kIsMultipleProducer || old_value != 1,
+                 "Incorrect usage of queue producers");
       old_producers_count = old_value;
       return old_value == kCreatedAndDead ? 1 : old_value + 1;
     });
@@ -243,13 +245,13 @@ class GenericQueue final
     if (old_producers_count == kCreatedAndDead) {
       consumer_side_.ResumeBlockingOnPop();
     }
-    UINVARIANT(QueuePolicy::kIsMultipleProducer || old_producers_count != 1,
-               "Incorrect usage of queue producers");
   }
 
   void PrepareConsumer() {
     std::size_t old_consumers_count{};
     utils::AtomicUpdate(consumers_count_, [&](auto old_value) {
+      UINVARIANT(QueuePolicy::kIsMultipleConsumer || old_value != 1,
+                 "Incorrect usage of queue consumers");
       old_consumers_count = old_value;
       return old_value == kCreatedAndDead ? 1 : old_value + 1;
     });
@@ -257,8 +259,6 @@ class GenericQueue final
     if (old_consumers_count == kCreatedAndDead) {
       producer_side_.ResumeBlockingOnPush();
     }
-    UINVARIANT(QueuePolicy::kIsMultipleConsumer || old_consumers_count != 1,
-               "Incorrect usage of queue consumers");
   }
 
   void MarkConsumerIsDead() {
