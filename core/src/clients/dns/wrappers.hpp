8a871a6a6216b1668af1244cbd55dc5c7f7a2b5f
bugaevskiy <bugaevskiy@userver.tech>
2024-04-26 15:15:56 +0300

    bug dns: fix UB when using thread-aware c-ares 4b925a245f28d27051897c9fd31fc20db8961c45

--- core/src/clients/dns/wrappers.hpp
+++ core/src/clients/dns/wrappers.hpp
@@ -5,6 +5,8 @@
 
 #include <ares.h>
 
+#include <userver/compiler/thread_local.hpp>
+
 USERVER_NAMESPACE_BEGIN
 
 namespace clients::dns::impl {
@@ -20,8 +22,26 @@ struct ChannelDeleter {
     ::ares_destroy(channel);
   }
 };
-using ChannelPtr =
-    std::unique_ptr<std::remove_pointer_t<ares_channel>, ChannelDeleter>;
+
+class ChannelPtr {
+ public:
+  ChannelPtr() = default;
+  explicit ChannelPtr(ares_channel channel) : channel_{channel} {}
+
+  auto Use() {
+#if ARES_VERSION >= 0x011400
+    // c-ares may lock internal mutexes while working with a channel,
+    // ensure we will not switch threads while using it
+    return compiler::ThreadLocalScope{channel_};
+#else
+    // But in old versions we must allow that
+    return &channel_;
+#endif
+  }
+
+ private:
+  std::unique_ptr<std::remove_pointer_t<ares_channel>, ChannelDeleter> channel_;
+};
 
 struct AddrinfoDeleter {
   void operator()(struct ares_addrinfo* ai) const noexcept {
