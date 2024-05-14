c482cccf1d8d5513d398c34ab466d3e1434d77c6
Egor Bocharov <dofe71@gmail.com>
2024-05-14 15:51:55 +0000

    TaskProcessor overload test

---
+++ core/src/engine/task/task_processor_test.cpp
@@ -0,0 +1,42 @@
+#include <gtest/gtest.h>
+#include <userver/utest/utest.hpp>
+
+#include <engine/task/task_processor.hpp>
+#include <userver/engine/async.hpp>
+#include <userver/engine/run_standalone.hpp>
+#include "engine/task/task_processor_config.hpp"
+#include "userver/engine/task/task_base.hpp"
+#include "userver/engine/task/task_with_result.hpp"
+
+USERVER_NAMESPACE_BEGIN
+
+using namespace std::chrono_literals;
+
+UTEST(TaskProcessor, Overload) {
+  engine::TaskProcessorSettings settings;
+  settings.overload_action =
+      engine::TaskProcessorSettings::OverloadAction::kCancel;
+  settings.wait_queue_length_limit = 500;
+  const std::size_t kCreatedTasksCount = 1100;
+  const std::size_t kMinCanceledTasksCount = 500;
+  engine::current_task::GetTaskProcessor().SetSettings(settings);
+
+  std::vector<engine::TaskWithResult<void>> tasks;
+  tasks.reserve(kCreatedTasksCount);
+
+  for (size_t i = 0; i < kCreatedTasksCount; ++i) {
+    tasks.push_back(engine::AsyncNoSpan([]() {}));
+  }
+
+  std::size_t canceled_tasks_count{0};
+  for (auto& task : tasks) {
+    task.Wait();
+    if (engine::Task::State::kCancelled == task.GetState()) {
+      ++canceled_tasks_count;
+    }
+  }
+
+  EXPECT_GE(canceled_tasks_count, kMinCanceledTasksCount);
+}
+
+USERVER_NAMESPACE_END
