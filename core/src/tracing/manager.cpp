5c8acd74db5a7bbea555e5068b8eb64294e3adfd
crossfider <crossfider@yandex-team.com>
2024-05-14 12:31:44 +0300

    make sampled as default flag for otel tracing header 0f5f9bff6013dd89bd9ee5b6a42285e78c478f73

--- core/src/tracing/manager.cpp
+++ core/src/tracing/manager.cpp
@@ -14,7 +14,9 @@ namespace tracing {
 namespace {
 
 constexpr std::string_view kSampledTag = "sampled";
-constexpr std::string_view kDefaultOtelTraceFlags = "00";
+// default value for Sampled flag is '01' as we always write spans by
+// default
+constexpr std::string_view kDefaultOtelTraceFlags = "01";
 
 // The order matter for TryFillSpanBuilderFromRequest as it returns on first
 // success
