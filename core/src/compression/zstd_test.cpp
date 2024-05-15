b3064e29dd47e87304e5e61481a96b56bf3a8b2f
Yobama-png <i.oplachkin@gmail.com>
2024-05-15 13:41:31 +0300

    Add working tests

--- core/src/compression/zstd_test.cpp
+++ core/src/compression/zstd_test.cpp
@@ -8,22 +8,45 @@ USERVER_NAMESPACE_BEGIN
 
 TEST(Zstd, DecompressSmall) {
   const std::size_t kSize = 8;
-  const char* kBuf = "abcdefg"; // 8 bytes
+  const char* kBuf = "abcdefgh";  // 8 bytes
   std::string str(kBuf);
 
   const std::size_t max_size = ZSTD_compressBound(kSize);
-  char* comp_buf = new char (max_size);
+  char* comp_buf = new char[max_size];
 
-  const std::size_t comp_size = ZSTD_compress(comp_buf, max_size, kBuf, kSize, 1);
+  const std::size_t comp_size =
+      ZSTD_compress(comp_buf, max_size, kBuf, kSize, 1);
 
   if (ZSTD_isError(comp_size)) {
     ADD_FAILURE() << "Couldn't compress data!";
     return;
   }
 
-  auto decomp_str = compression::zstd::Decompress(std::string_view(kBuf), kSize);
+  auto decomp_str = compression::zstd::Decompress(
+      std::string_view(comp_buf, comp_size), max_size);
 
   EXPECT_EQ(str, decomp_str);
+}
+
+TEST(Zstd, DecompressLarge) {
+  const std::size_t kSize = 16'000;
+  std::string str(kSize, 'a');
+
+  const std::size_t max_size = ZSTD_compressBound(kSize);
+  char* comp_buf = new char[max_size];
+
+  const std::size_t comp_size =
+      ZSTD_compress(comp_buf, max_size, str.data(), kSize, 1);
+
+  if (ZSTD_isError(comp_size)) {
+    ADD_FAILURE() << "Couldn't compress data!";
+    return;
   }
+
+  auto decomp_str = compression::zstd::Decompress(
+      std::string_view(comp_buf, comp_size), max_size);
+
+  EXPECT_EQ(str, decomp_str);
+}
 
 USERVER_NAMESPACE_END
