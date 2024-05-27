95d091aa49e110eafd612109e86eb7ae10c52ef4
SidorovichPavel <sidorovich.pasha@gmail.com>
2024-05-27 13:14:24 +0300

    clients::http::Status as alias to http::StatusCode

--- core/include/userver/clients/http/response.hpp
+++ core/include/userver/clients/http/response.hpp
@@ -11,104 +11,13 @@
 #include <userver/http/header_map.hpp>
 #include <userver/server/http/http_response_cookie.hpp>
 #include <userver/utils/str_icase.hpp>
+#include <userver/http/status_code.hpp>
 
 USERVER_NAMESPACE_BEGIN
 
 namespace clients::http {
 
-/// https://en.wikipedia.org/wiki/List_of_HTTP_status_codes
-enum Status : uint16_t {
-  Invalid = 0,
-
-  // 1xx informational response
-  Continue = 100,
-  SwitchingProtocols = 101,
-  Processing = 102,
-  EarlyHints = 103,
-
-  // 2xx success
-  OK = 200,
-  Created = 201,
-  Accepted = 202,
-  NonAuthoritativeInformation = 203,
-  NoContent = 204,
-  ResetContent = 205,
-  PartialContent = 206,
-  MultiStatus = 207,
-  AlreadyReported = 208,
-  ThisIsFine = 218,
-  IMUsed = 226,
-
-  // 3xx redirection
-  MultipleChoices = 300,
-  MovedPermanently = 301,
-  Found = 302,
-  SeeOther = 303,
-  NotModified = 304,
-  UseProxy = 305,
-  SwitchProxy = 306,
-  TemporaryRedirect = 307,
-  PermanentRedirect = 308,
-
-  // 4xx client errors
-  BadRequest = 400,
-  Unauthorized = 401,
-  PaymentRequired = 402,
-  Forbidden = 403,
-  NotFound = 404,
-  MethodNotAllowed = 405,
-  NotAcceptable = 406,
-  ProxyAuthenticationRequired = 407,
-  RequestTimeout = 408,
-  Conflict = 409,
-  Gone = 410,
-  LengthRequired = 411,
-  PreconditionFailed = 412,
-  PayloadTooLarge = 413,
-  UriTooLong = 414,
-  UnsupportedMediaType = 415,
-  RangeNotSatisfiable = 416,
-  ExpectationFailed = 417,
-  ImATeapot = 418,
-  PageExpired = 419,
-  MethodFailure = 420,
-  MisdirectedRequest = 421,
-  UnprocessableEntity = 422,
-  Locked = 423,
-  FailedDependency = 424,
-  TooEarly = 425,
-  UpgradeRequired = 426,
-  PreconditionRequired = 428,
-  TooManyRequests = 429,
-  RequestHeaderFieldsTooLarge = 431,
-  NginxNoResponse = 444,
-  UnavailableForLegalReasons = 451,
-  NginxRequestHeaderTooLarge = 494,
-  NginxSSLCertificateError = 495,
-  NginxSSLCertificateRequired = 496,
-  NginxHTTPRequestSenttoHTTPSPort = 497,
-  NginxClientClosedRequest = 499,
-
-  // 5xx server errors
-  InternalServerError = 500,
-  NotImplemented = 501,
-  BadGateway = 502,
-  ServiceUnavailable = 503,
-  GatewayTimeout = 504,
-  HttpVersionNotSupported = 505,
-  VariantAlsoNegotiates = 506,
-  InsufficientStorage = 507,
-  LoopDetected = 508,
-  BandwidthLimitExceeded = 509,
-  NotExtended = 510,
-  NetworkAuthenticationRequired = 511,
-  WebServerIsDown = 520,
-  ConnectionTimedOut = 522,
-  OriginIsUnreachable = 523,
-  TimeoutOccurred = 524,
-  SslHandshakeFailed = 525,
-  InvalidSslCertificate = 526,
-};
+using Status = USERVER_NAMESPACE::http::StatusCode; 
 
 std::ostream& operator<<(std::ostream& os, Status s);
 
