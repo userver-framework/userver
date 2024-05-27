823ca124d3f49eb647ba15ec89b2446061d1b72e
SidorovichPavel <sidorovich.pasha@gmail.com>
2024-05-27 18:31:17 +0300

    removed serialization functions

--- core/include/userver/server/http/http_status.hpp
+++ core/include/userver/server/http/http_status.hpp
@@ -3,8 +3,6 @@
 /// @file userver/server/http/http_status.hpp
 /// @brief @copybrief server::http::HttpStatus
 
-#include <string_view>
-
 #include <fmt/core.h>
 
 #include <userver/http/status_code.hpp>
@@ -16,9 +14,6 @@ namespace server::http {
 /// @brief HTTP status codes
 using HttpStatus = USERVER_NAMESPACE::http::StatusCode;
 
-std::string_view HttpStatusString(HttpStatus status);
-
-std::string ToString(HttpStatus status);
 }  // namespace server::http
 
 USERVER_NAMESPACE_END
