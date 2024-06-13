ad01c9d48fb53ca80c0e0cabda4195ddce75f6c2
arudenko02 <arudenko02@userver.tech>
2024-06-13 11:49:17 +0300

    feat universal: add constructor from string to TimePointTzBase
    add constructor from string to TimePointTzBase
    8643e78de638c5ae8c23938191905aff3bcde321

--- universal/src/utils/datetime/timepoint_tz.cpp
+++ universal/src/utils/datetime/timepoint_tz.cpp
@@ -2,7 +2,9 @@
 
 #include <tuple>
 
+#include <userver/logging/log_helper.hpp>
 #include <userver/utils/datetime.hpp>
+#include <userver/utils/datetime/from_string_saturating.hpp>
 
 USERVER_NAMESPACE_BEGIN
 
@@ -64,9 +66,15 @@ bool operator>=(const TimePointTzBase::TimePoint& lhs,
   return lhs >= rhs.GetTimePoint();
 }
 
+TimePointTz::TimePointTz(const std::string& timestring)
+    : TimePointTzBase(FromRfc3339StringSaturating(timestring)) {}
+
 logging::LogHelper& operator<<(logging::LogHelper& os, const TimePointTz& v) {
   return os << v.GetTimePoint();
 }
+
+TimePointTzIsoBasic::TimePointTzIsoBasic(const std::string& timestring)
+    : TimePointTzBase(FromStringSaturating(timestring, kDefaultFormat)) {}
 
 logging::LogHelper& operator<<(logging::LogHelper& os,
                                const TimePointTzIsoBasic& v) {
