1f7a943eead45912baf845b90ed928109e73ce20
kpavlov00 <kpavlov00@yandex-team.com>
2024-06-28 14:46:18 +0300

    feat mongo: add unit-test sample 6770adc3d176297aabbd48f142f6fe9ac6412664

---
+++ samples/mongo_service/unittests/mongo_test.cpp
@@ -0,0 +1,13 @@
+#include <userver/storages/mongo/utest/mongo_fixture.hpp>
+#include <userver/utest/using_namespace_userver.hpp>
+
+#include <userver/formats/bson/inline.hpp>
+
+using MongoTest = storages::mongo::utest::MongoTest;
+
+UTEST_F(MongoTest, Sample) {
+  auto collection = GetPool()->GetCollection("xyz");
+  EXPECT_EQ(0, collection.Count({}));
+  collection.InsertOne(formats::bson::MakeDoc("x", 2));
+  EXPECT_EQ(1, collection.Count({}));
+}
