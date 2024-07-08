8b8fbb8039cfa44102a5b98059ebaecef30997a0
kpavlov00 <kpavlov00@yandex-team.com>
2024-07-08 18:14:28 +0300

    feat ydb: upd credentials docs e77a5f3e8b1c0d035cd78897853644d657764fae

--- ydb/include/userver/ydb/credentials.hpp
+++ ydb/include/userver/ydb/credentials.hpp
@@ -17,10 +17,27 @@ USERVER_NAMESPACE_BEGIN
 
 namespace ydb {
 
+// clang-format off
+
+/// @ingroup userver_components
+///
+/// @brief Credentials provider component for creating custom credentials provider factory
+///
+/// Allows use custom credentials provider implementation
+/// Required if `ydb::YdbComponent` comnponent config contains `databases.<dbname>.credentials`
+///
+/// see https://ydb.tech/docs/en/concepts/auth
+
+// clang-format on
+
 class CredentialsProviderComponent : public components::ComponentBase {
  public:
   using components::ComponentBase::ComponentBase;
 
+  /// @brief Create credentials provider factory
+  ///
+  /// @param credentials credentials config (`databases.<dbname>.credentials`
+  /// from `ydb::YdbComponent` component config)
   virtual std::shared_ptr<NYdb::ICredentialsProviderFactory>
   CreateCredentialsProviderFactory(
       const yaml_config::YamlConfig& credentials) const = 0;
