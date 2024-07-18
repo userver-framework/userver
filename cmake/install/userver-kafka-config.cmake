cc3a93d02db30fb350110e71aad2cde67a4e8fc8
Antony Polukhin <antoshkka@userver.tech>
2024-07-18 13:56:01 +0300

    Update userver-kafka-config.cmake

--- cmake/install/userver-kafka-config.cmake
+++ cmake/install/userver-kafka-config.cmake
@@ -9,6 +9,6 @@ find_package(userver REQUIRED COMPONENTS
 )
 
 list(APPEND CMAKE_MODULE_PATH "${CMAKE_CURRENT_LIST_DIR}/..")
-include("${USERVER_CMAKE_DIR}/FindRdKafka.cmake")
+include("${USERVER_CMAKE_DIR}/SetupRdKafka.cmake")
 
 set(userver_kafka_FOUND TRUE)
