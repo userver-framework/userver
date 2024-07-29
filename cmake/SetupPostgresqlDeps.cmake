d3675f2febca5824697a7a246987a92951800e96
Daniil Shvalov <shvalovich@gmail.com>
2024-07-29 15:08:53 +0300

    feat cmake: move postgresql macOS options from Makefile to cmake
    Tests: протестировано CI
    ea910ae6443c8a0f1ed553f4706f6363a9395edd
    
    Pull Request resolved: https://github.com/userver-framework/userver/pull/659

--- cmake/SetupPostgresqlDeps.cmake
+++ cmake/SetupPostgresqlDeps.cmake
@@ -1,5 +1,23 @@
 include_guard(GLOBAL)
 
+function(_set_default_pg_dir variable pg_config_args)
+    if(NOT ${variable})
+        execute_process(
+            COMMAND pg_config ${pg_config_args}
+            OUTPUT_VARIABLE output
+            OUTPUT_STRIP_TRAILING_WHITESPACE
+        )
+        set(${variable} ${output} PARENT_SCOPE)
+    endif()
+endfunction()
+
+if(CMAKE_SYSTEM_NAME MATCHES "Darwin")
+    _set_default_pg_dir(USERVER_PG_INCLUDE_DIR "--includedir")
+    _set_default_pg_dir(USERVER_PG_LIBRARY_DIR "--libdir")
+    _set_default_pg_dir(USERVER_PG_SERVER_INCLUDE_DIR "--includedir-server")
+    _set_default_pg_dir(USERVER_PG_SERVER_LIBRARY_DIR "--pkglibdir")
+endif()
+
 # We need libldap to statically link with libpq
 # There is no FindLdap.cmake and no package config files
 # for ldap library, so need to search for it by hand.
@@ -17,7 +35,7 @@ find_package(Threads REQUIRED)
 find_package(OpenSSL REQUIRED)
 
 include(CheckLibraryExists)
-CHECK_LIBRARY_EXISTS(m sin "" USERVER_HAS_LIB_MATH) 
+CHECK_LIBRARY_EXISTS(m sin "" USERVER_HAS_LIB_MATH)
 set(USERVER_LIB_MATH)
 if(USERVER_HAS_LIB_MATH)
   set(USERVER_LIB_MATH m)
