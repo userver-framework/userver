8930408571d43535295e7c2fd721cc2226f5fff6
Aleksey Ignatev <iae.ignatev@gmail.com>
2024-09-03 09:03:18 +0300

    Testsuite for pub/sub standalone

--- redis/functional_tests/pubsub/tests/test_redis_pubsub.py
+++ redis/functional_tests/pubsub/tests/test_redis_pubsub.py
@@ -37,6 +37,7 @@ async def _validate_pubsub(redis_db, service_client, msg, redis_type):
 
 @pytest.mark.parametrize('db_name', ['sentinel', 'sentinel-with-master'])
 async def test_happy_path_sentinel(service_client, redis_store, db_name):
+
     msg = 'sentinel_message'
     assert await _validate_pubsub(redis_store, service_client, msg, db_name)
 
@@ -71,3 +72,17 @@ async def test_happy_path_cluster(service_client, redis_cluster_store):
 
     if failed:
         assert False, f'Failed after multiple retries: {failed}'
+
+####################################################
+
+async def test_happy_path_standalone(service_client, redis_standalone_store):
+    msg = 'sentinel_message'
+    assert await _validate_pubsub(redis_standalone_store, service_client, msg, 'standalone')
+
+async def test_happy_path_standalone_with_resubscription(
+        service_client, redis_standalone_store
+):
+    msg = 'sentinel_message'
+    response = await service_client.put(_get_url('standalone'))
+    assert response.status == 200
+    assert await _validate_pubsub(redis_standalone_store, service_client, msg, 'standalone')
