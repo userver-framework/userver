741942ac8f45c58bd0a4d8417292411a9f2d66b5
Aleksey Ignatev <iae.ignatev@gmail.com>
2024-09-03 10:24:31 +0300

    Move shared redis standalone plugin in integration_tests

--- redis/functional_tests/integration_tests/tests/conftest.py
+++ redis/functional_tests/integration_tests/tests/conftest.py
@@ -1,58 +1,17 @@
 import json
 import os
-import subprocess
-import asyncio
-import pytest
-from testsuite.utils import callinfo
-from testsuite.daemons import service_daemon
 
-pytest_plugins = ['pytest_userver.plugins.redis']
+import pytest
+
+pytest_plugins = [
+    'pytest_userver.plugins.redis',
+    'redis_standalone_plugin.redis_standalone'
+]
 
 os.environ['TESTSUITE_REDIS_HOSTNAME'] = 'localhost'
 
 @pytest.fixture(scope='session')
-async def redis_standalone_port():
-    return 7000
-
-@pytest.fixture(scope='session')
-def health_check(redis_standalone_port):
-
-    @callinfo.acallqueue
-    async def health_check(*, process, session):
-        print('Healt check called!')
-        if not process:
-            pytest.fail('process does not exist')
-        if not process.pid:
-            pytest.fail('process.pid is not set')
-        return subprocess.run(["redis-cli", "-p", f"{redis_standalone_port}", "--raw", "incr", "ping"]).returncode == 0
-
-    return health_check
-
-@pytest.fixture(scope='session')
-async def redis_standalone_run_command(redis_standalone_port):
-    return [
-                'redis-server',
-                '/source/redis/functional_tests/integration_tests/tests/redis_standalone.conf',
-                '--port',
-                f'{redis_standalone_port}'
-            ]
-
-@pytest.fixture(scope='session')
-async def redis_standalone(
-    health_check,
-    redis_standalone_run_command
-):
-
-    async with service_daemon.start(
-            args=redis_standalone_run_command,
-            health_check=health_check,
-            subprocess_options={'stderr': subprocess.PIPE, 'bufsize': 0}
-    ) as scope:
-        await asyncio.sleep(1.0) # wait_service_started(args=redis_standalone_run_command, health_check=health_check)
-        yield scope
-
-@pytest.fixture(scope='session')
-def service_env(redis_sentinels, redis_cluster_nodes, redis_cluster_replicas, redis_standalone, redis_standalone_port):
+def service_env(redis_sentinels, redis_cluster_nodes, redis_cluster_replicas, redis_standalone):
     cluster_shards = [
         {'name': f'shard{idx}'}
         for idx in range(
@@ -74,12 +33,7 @@ def service_env(redis_sentinels, redis_cluster_nodes, redis_cluster_replicas, re
             },
             'redis-standalone': {
                 'password': '',
-                'sentinels': [
-                    {
-                        "host": "localhost",
-                        "port": redis_standalone_port
-                    },
-                ],
+                'sentinels': redis_standalone,
                 'shards': [{'name': 'test_master1'}],
             },
         },
