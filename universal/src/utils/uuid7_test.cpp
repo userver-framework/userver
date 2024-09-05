2567abccc5fe45539771f29e7aed6dbb5b38bbf3
antoshkka <antoshkka@userver.tech>
2024-09-05 15:23:36 +0300

    feat universal: add tests for UUID7
    Tests: протестировано локально (баг отлавливает), и в CI
    83622930e5682c5e9158c6a374ced099a7b7c1f6

--- universal/src/utils/uuid7_test.cpp
+++ universal/src/utils/uuid7_test.cpp
@@ -12,6 +12,19 @@ TEST(UUIDv7, String) {
 
   EXPECT_NE(utils::generators::GenerateUuidV7(),
             utils::generators::GenerateUuidV7());
+
+  // Expect random data at the last 11 chars,
+  // unlike in 0191c169-962a-8000-8000-000000000003
+  constexpr std::string_view kBadData = "00000000000";
+  for (int i = 0; i < 10; i++) {
+    auto uuid = utils::generators::GenerateUuidV7();
+    uuid = uuid.substr(uuid.size() - 12, 11);
+    EXPECT_EQ(uuid.size(), kBadData.size());
+    EXPECT_NE(uuid, kBadData);
+    for (auto x : uuid) {
+      EXPECT_TRUE(std::isdigit(x) || (x >= 'a' && x <= 'f')) << uuid;
+    }
+  }
 }
 
 USERVER_NAMESPACE_END
