bfd7853eddd5de431cb3668a7f6bbb1d6b56b460
antoshkka <antoshkka@userver.tech>
2024-09-13 14:01:59 +0300

    feat ydb: make the test less strict
    Tests: протестировано CI
    commit_hash:8b810b0af490706580bc8387013de3d81dd88b0f

--- ydb/tests/schema_test.cpp
+++ ydb/tests/schema_test.cpp
@@ -33,8 +33,7 @@ UTEST_F(YdbSchemaNoPredefinedTables, CreateAndDropTable) {
   static constexpr std::string_view kTableName = "create_and_drop_table";
 
   // Step 0: check that the table does not exist initially.
-  UASSERT_THROW_MSG(DescribeTableNative(*this, kTableName),
-                    ydb::YdbResponseError, "not found");
+  UASSERT_THROW(DescribeTableNative(*this, kTableName), ydb::YdbResponseError);
 
   // Step 1: create a table.
   auto builder =
