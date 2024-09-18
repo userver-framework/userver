02badebd1bc334cd65f0b6ba9e14eff2b6751f3f
fdr400 <fdr400@yandex-team.com>
2024-09-18 19:35:50 +0300

    cc docs: fix default values commit_hash:a6dbf008c2f67374e1b5b35884cd5f218965e0a8

--- kafka/include/userver/kafka/consumer_component.hpp
+++ kafka/include/userver/kafka/consumer_component.hpp
@@ -49,9 +49,9 @@ class Consumer;
 /// env_pod_name                       | environment variable to substitute `{pod_name}` substring in `group_id` | none
 /// security_protocol                  | protocol used to communicate with brokers | --
 /// sasl_mechanisms                    | SASL mechanism to use for authentication | none
-/// ssl_ca_location                    | File or directory path to CA certificate(s) for verifying the broker's key | 5m
-/// topic_metadata_refresh_interval    | period of time at which topic and broker metadata is refreshed | 15m
-/// metadata_max_age                   | metadata cache max age | none
+/// ssl_ca_location                    | File or directory path to CA certificate(s) for verifying the broker's key | none
+/// topic_metadata_refresh_interval    | period of time at which topic and broker metadata is refreshed | 5m
+/// metadata_max_age                   | metadata cache max age | 15
 /// rd_kafka_custom_options            | a map of librdkafka library additional options | '{}'
 
 // clang-format on
