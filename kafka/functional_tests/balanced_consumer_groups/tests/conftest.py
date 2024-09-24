54cbe351739bb96826318248c4fc1da2d9078cf0
Fedor <fiosetrova@gmail.com>
2024-09-24 16:42:23 +0000

    Use new fixture

--- kafka/functional_tests/balanced_consumer_groups/tests/conftest.py
+++ kafka/functional_tests/balanced_consumer_groups/tests/conftest.py
@@ -1,5 +1,6 @@
 import asyncio
 import logging
+import os
 
 
 import pytest
@@ -8,19 +9,31 @@ import pytest
 import common
 
 
-pytest_plugins = ['pytest_userver.plugins.kafka']
+from testsuite.databases.kafka.classes import BootstrapServers
 
 
-@pytest.fixture(scope='session')
+pytest_plugins = ["pytest_userver.plugins.kafka"]
+
+
+@pytest.fixture(scope="session")
 def service_env(kafka_secdist):
-    return {'SECDIST_CONFIG': kafka_secdist}
+    return {"SECDIST_CONFIG": kafka_secdist}
 
 
-@pytest.fixture(name='stop_consumers', autouse=True)
+@pytest.fixture(scope="session")
+def kafka_local() -> BootstrapServers:
+    brokers_list = os.getenv("KAFKA_RECIPE_BROKER_LIST")
+    if brokers_list:
+        return brokers_list.split(",")
+
+    return []
+
+
+@pytest.fixture(name="stop_consumers", autouse=True)
 async def stop_consumers(service_client):
     yield
 
-    logging.debug('Stopping consumers after test...')
+    logging.debug("Stopping consumers after test...")
 
     await asyncio.sleep(1.0)  # wait until messages are consumed
     for consumer in common.CONSUMERS:  # clear consumed messages, if exists
@@ -29,4 +42,4 @@ async def stop_consumers(service_client):
     # guarantee the consumers to be stopped after each test
     await common.stop_consumers(service_client)
 
-    logging.debug('All consumers are stopped')
+    logging.debug("All consumers are stopped")
