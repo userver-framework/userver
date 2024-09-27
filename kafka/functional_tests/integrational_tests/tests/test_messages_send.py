e179b4153460164d93f11c3956271bb7954932e2
Fedor Osetrov <33493672+fdr400@users.noreply.github.com>
2024-09-27 12:40:12 +0300

    Update test_messages_send.py

--- kafka/functional_tests/integrational_tests/tests/test_messages_send.py
+++ kafka/functional_tests/integrational_tests/tests/test_messages_send.py
@@ -8,27 +8,25 @@ from utils import produce
 from utils import produce_async
 from utils import produce_batch
 
-TOPIC = "test-topic-send"
+TOPIC = 'test-topic-send'
 
 
 async def test_one_producer_send_sync(service_client, testpoint):
-    @testpoint("tp_kafka-consumer")
+    @testpoint('tp_kafka-consumer')
     def received_messages_func(_data):
         pass
 
     await service_client.enable_testpoints()
 
-    await produce(service_client, 0, TOPIC, "test-key", "test-message")
+    await produce(service_client, 0, TOPIC, 'test-key', 'test-message')
 
     await clear_topics(
-        service_client,
-        received_messages_func,
-        messages_to_clear_cnt=1,
+        service_client, received_messages_func, messages_to_clear_cnt=1,
     )
 
 
 async def test_many_producers_send_sync(service_client, testpoint):
-    @testpoint("tp_kafka-consumer")
+    @testpoint('tp_kafka-consumer')
     def received_messages_func(_data):
         pass
 
@@ -41,22 +39,20 @@ async def test_many_producers_send_sync(service_client, testpoint):
                 service_client,
                 i % 2,
                 TOPIC,
-                f"test-key-{i}",
-                f"test-message-{i}",
+                f'test-key-{i}',
+                f'test-message-{i}',
             ),
         )
 
     assert all([(await resp).status_code == 200 for resp in responses])
 
     await clear_topics(
-        service_client,
-        received_messages_func,
-        messages_to_clear_cnt=8,
+        service_client, received_messages_func, messages_to_clear_cnt=8,
     )
 
 
 async def test_many_producers_send_async(service_client, testpoint):
-    @testpoint("tp_kafka-consumer")
+    @testpoint('tp_kafka-consumer')
     def received_messages_func(_data):
         pass
 
@@ -66,17 +62,12 @@ async def test_many_producers_send_async(service_client, testpoint):
     for i in range(8):
         requests.append(
             make_producer_request_body(
-                i % 2,
-                TOPIC,
-                f"test-key-{i}",
-                f"test-message-{i}",
+                i % 2, TOPIC, f'test-key-{i}', f'test-message-{i}',
             ),
         )
 
     await produce_batch(service_client, requests)
 
     await clear_topics(
-        service_client,
-        received_messages_func,
-        messages_to_clear_cnt=8,
+        service_client, received_messages_func, messages_to_clear_cnt=8,
     )
