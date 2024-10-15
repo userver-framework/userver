037eb30aa00cb22f77ea79cee4d888ae47edac68
maxibellum <maxibellum@yandex-team.com>
2024-10-15 13:33:42 +0300

    feat debug: add gdb pretty printers for userver
    Tests: протестировано CI
    commit_hash:a87aa38c6c1180b57f1278ab6f175bc50b95a9d4

---
+++ cmake/GenGdbPrinters.cmake
@@ -0,0 +1,50 @@
+find_package(Python3 REQUIRED)
+
+function(gen_gdb_printers TARGET STRUCTURE)
+  # Set the path to the Python script
+  set (PRINTER_PYTHON_SCRIPT ${USERVER_ROOT_DIR}/scripts/gdb/pretty_printers/${STRUCTURE}/printers.py)
+  
+  # Set the output header file path in the build directory
+  set (OUTPUT_HEADER ${CMAKE_CURRENT_SOURCE_DIR}/include/userver/${STRUCTURE}/gdb_autogen/printers.hpp)
+
+  # Create the output directory if it doesn't exist
+  get_filename_component(OUTPUT_HEADER_DIR ${OUTPUT_HEADER} DIRECTORY)
+  make_directory(${OUTPUT_HEADER_DIR})
+
+  message(STATUS "Generating ${STRUCTURE} gdb printing header")
+
+  # Generate the header file during configuration
+  execute_process(
+    COMMAND ${Python3_EXECUTABLE} ${USERVER_ROOT_DIR}/scripts/gdb/gen_gdb_printers.py
+            ${OUTPUT_HEADER} ${PRINTER_PYTHON_SCRIPT}
+    RESULT_VARIABLE GENERATION_RESULT
+    OUTPUT_VARIABLE GENERATION_OUTPUT
+    ERROR_VARIABLE GENERATION_ERROR
+  )
+
+  # Check if the generation was successful
+  if(NOT GENERATION_RESULT EQUAL 0)
+      message(FATAL_ERROR "Header generation failed: ${GENERATION_ERROR}")
+  endif()
+
+  # Add the generated header to the target's sources
+  target_sources(${TARGET} PRIVATE ${OUTPUT_HEADER})
+  
+  target_compile_definitions(${TARGET} PUBLIC USERVER_EMBEDDED_GDB_PRINTERS)
+
+  # Add a custom target to update the .gdbinit file
+  set(GDB_INIT_FILE "$ENV{HOME}/.gdbinit")
+  set(GDB_SAFE_PATH_RECORD "add-auto-load-safe-path ${CMAKE_BINARY_DIR}")
+  set(GDB_INIT_STAMP ${CMAKE_BINARY_DIR}/gdbinit_stamp)
+
+  add_custom_command(
+    OUTPUT ${GDB_INIT_STAMP}
+    COMMAND grep -cx ${GDB_SAFE_PATH_RECORD} ${GDB_INIT_FILE} > /dev/null || echo ${GDB_SAFE_PATH_RECORD} >> ${GDB_INIT_FILE}
+    COMMAND ${CMAKE_COMMAND} -E touch ${GDB_INIT_STAMP}
+    COMMENT "Updating ${GDB_INIT_FILE}"
+    VERBATIM
+  )
+
+  add_custom_target(CONFIGURE_GDBINIT ALL DEPENDS ${GDB_INIT_STAMP})
+  add_dependencies(${TARGET} CONFIGURE_GDBINIT)
+endfunction()
---
+++ universal/include/userver/formats/json/gdb_autogen/printers.hpp
@@ -0,0 +1,90 @@
+
+// Auto-generated. DO NOT EDIT.
+#pragma once
+
+// NOLINTBEGIN
+// clang-format off
+#ifdef USERVER_EMBEDDED_GDB_PRINTERS
+#ifdef __ELF__
+
+#ifdef __clang__
+#pragma clang diagnostic push
+#pragma clang diagnostic ignored "-Woverlength-strings"
+#endif
+
+__asm__(
+    ".pushsection \".debug_gdb_scripts\", \"MS\",@progbits,1\n"
+    ".ascii \"\\4gdb.inlined-script.___SCRIPTS_GDB_PRETTY_PRINTERS_FORMATS_JSON_PRINTERS_PY\\n\"\n"
+    ".ascii \"import marshal, zlib, base64\\n\"\n"
+    ".ascii \"exec(marshal.loads(zlib.decompress(base64.decodebytes(\\n\"\n"
+    ".ascii \"b'eJyNWV9wG0cZ3927k06yLMv//zRpRZu2USe1S5tmwGRCEtdJExIlOE6AK+1x1q4dybKkrs6lETYt\\\\ndaD'\\n\"\n"
+    ".ascii \"b'MUGZgmA7DQGbsGV7ywhvM8ArP8MDjPcLwkEcmDDCmrfm+vTvpTpIT4rkv+327d7v7+/7u6m+k\\\\n658Oz1'\\n\"\n"
+    ".ascii \"b'l4miYlhBNOq2SDWpRim1WZxQS9SLjG9buE6ztsh1oaN3jiLrF0kCe5CXJzh1kGcCmeBi4t\\\\njB3NSgA/w'\\n\"\n"
+    ".ascii \"b'DPAZxSfBH6QZ4HPKt4EfojngM8pPgX8MB8BfkTxaeBH+RjwY4of4ON8AmbNgHyST4F8\\\\nCmYdBG6azwA3'\\n\"\n"
+    ".ascii \"b'A1yWPyFYZaiSW2X8CD96V7eGxQh/cp3JD1C+m+NP7VFrVAyL0R3YoZKNCbY7vkfE\\\\n6B6DPbMb6inkHyA'\\n\"\n"
+    ".ascii \"b'2BeqluOM6parTbBZL3dAZIXQnATpBLMqJxTgCxCyda5bBdWH8knBD6EAT9wwr\\\\nwZNdEgBQJKCdugeQ8L'\\n\"\n"
+    ".ascii \"b'SV4gNWmmdgu4NWhmetQT4E28pZQ3zYyvERa5iPWiN8zBrl49YYn7DG+aQ1\\\\nwaesST5tTfEZa1qkrBkxI'\\n\"\n"
+    ".ascii \"b'HLnifWESKv/j4ghMXyeiBF4xoA/GvCj8IwHsicDGbafCtrQZ+WD9gS0\\\\nPxf/Tvt97HtaZKxnxKB1TGTF'\\n\"\n"
+    ".ascii \"b'JPDP+v+LKWg/12mLaeCf509Yx/kRgPmol1qo15quU3ObVbTEg/fw\\\\nL0lak9JplHmlWa/Nzy9I91y1Wi8'\\n\"\n"
+    ".ascii \"b'5bl22xiMdN5cvfOF06bYjz7SmIuKLoiZkuXTLqW6K0y12Ir9P\\\\nz7SmewdcFRsrQp5uPdXbdW2lIkruaV'\\n\"\n"
+    ".ascii \"b'duihP5FsufkaBi8kARhkRDgit+gFbwIIHERJJDkkeCpvHg\\\\nfSQE3isThkRHYiKBgQ+S0FssFDzTtmvOh'\\n\"\n"
+    ".ascii \"b'rBtL23bG3W+WcV2xrbf3nSqQU9m6bJ981Jx+dRJe+Fl\\\\nLwXc8rkrV64tqI7lxeLCtdcuFS96OeAuLhYX'\\n\"\n"
+    ".ascii \"b'ly4t2LfOXbm56A1HJFcXr55fXIqJrp2/vLiwjO8t\\\\n32kIe724Wa1iCwf5kgtOtSmUqD1oGYBRkpFQ4iM'\\n\"\n"
+    ".ascii \"b'Wf/GclM6d+LAbrizX1uKy4ibqIZzgQtVZs9fP\\\\n1+tVbOEoX+KPUrKhUHap5ipB+7Wb5UAyHBly6mT8Sz'\\n\"\n"
+    ".ascii \"b'ioW/ZafXOlKuIyf63xCRbqjTtKMt6ZoFqu\\\\nCRgbH4gwxiWIWXxtCtj41/xdhvuaiIvbu5vsGR7sZ6r3h'\\n\"\n"
+    ".ascii \"b'f49kf12zX2udicOg6/Z+MqVZuNLUY4c\\\\ngWwiCllE3n7hxu26dPthjJZw1Wmu3yfyS+AgiuyLudv1DTG3'\\n\"\n"
+    ".ascii \"b'4bxbXhHV6ubGnOPIksPLzpwLsrnN\\\\nppDvlEui6beEnGuWZLnhNufW+MpcQwrXvWM3YDJXyObcal1uONC'\\n\"\n"
+    ".ascii \"b'HTj8XSmcbdyT6NTpzcxeISXQN\\\\n/ugIyzF2kNGxHfmLc4HM1EzKNEbZAWPsM2h9Cq1kjNfZJ+wgbcKj+w'\\n\"\n"
+    ".ascii \"b'9IuvsN9l+WYPvsQE/r1KRp\\\\n1jPqwEyq1ZZYJCWhAIPKAm7hJSAu2SI71KUVdpZsM1fb1bfoHuQ+aBmcb'\\n\"\n"
+    ".ascii \"b'lFIPWyLAdXuGXsUQrJe\\\\n3KfHW+yFgkfThYSnATRqGjkOxNMATG+gWq+vbzZsFz0223BkU9hOjdviHada'\\n\"\n"
+    ".ascii \"b'0Dyt4UovKSt+f6Im\\\\nvg2CjiblafxQFvrXhGs36gr7FVwv4p410myKygu4sUNLladJNN8KraJzhnRVV4W'\\n\"\n"
+    ".ascii \"b'KDpnXgK0kvPTS\\\\n5fNOELs0WJ1nrIKFNUtaF2QshAyj9xbdIjbZYkBhhhvkPi0WmHwdl34JiwLN05uiut'\\n\"\n"
+    ".ascii \"b'qRdO8Nonm5\\\\nVnYhpuMncdUJmqat0c5iZsMRxUJCjuCLo0jGkOAOPUMlL3kl/vX2FPIikI3w6yaZoUrSg'\\n\"\n"
+    ".ascii \"b'1i4z4cn\\\\nI4h9SFYpp5zdNaFSUYUdVCt+SWf4BZ2V+BDAtpKAYQrTSye2x5DTgwdlzevK2HbILhjVFsNi'\\n\"\n"
+    ".ascii \"b'agtm\\\\nmQbzmibbmsu2YK5pMEVAVXN14PRp4hqVxA7inNwiFU0+B9QELoWo41Mwip6BBZjt0TqgXm4JL7m'\\n\"\n"
+    ".ascii \"b'h\\\\nsnazkPaM5mZDSB+kLyNZQoKm41vsNJIbCOdg6Xa5yqWo2eWNRtUzQ/a+Lr+Cg9qq9HSc7j7Ufbat\\\\nq'\\n\"\n"
+    ".ascii \"b'j7bjqB+Re0RNosqy9IMzVIGmk0cJLQM1Q9a41Go2jou0QhmRoAZqqqJVv895aIVukNfI28e3wZ8\\\\nKmwL'\\n\"\n"
+    ".ascii \"b'cNsGt0T8gAKCjG1r22DdgKk+RrCCvKdtaavsFpQWoZQnJsMe3e95O6FQTBY9HYsIz3gHrWqf\\\\nviHR1fb'\\n\"\n"
+    ".ascii \"b'pm9LwDdqQTm1N+PghYAVD4eLRspfw8ZZvYN83+5jiTSDfxd1k0ZHezwIgI3SCHqH6J62Z\\\\nGCAxJcRQoc'\\n\"\n"
+    ".ascii \"b'GDPvgQ4eEYiljR0zachprrPvU11eVqGV5uNqrOHfs2hJHv49voyjptTcdmjo7qmTh0\\\\n/ocZNTEoAGC8x'\\n\"\n"
+    ".ascii \"b'9QCWkN19ZV8fTWP5peHdSiUeJ/FpNy63VT57IfhSjK0NRFbSXtIwex2/CshmlIg\\\\nKSvDDcywBCkvNEVF'\\n\"\n"
+    ".ascii \"b'vtHG/xpij0+aYCjIUDNtJnOakh8aEGQRtZzyBpYut4u0w337LOnxbbDSiHdr\\\\nEe/WXSPw7kQlqbzbDHx'\\n\"\n"
+    ".ascii \"b'ZXlc25SgEPVNUxYaAwr+Qksvt/StP7OfMU0gwoMqvtfcf98wf41JRh1ma\\\\nDTwzfdAai+ywv0syEnHJWd'\\n\"\n"
+    ".ascii \"b'LlktltGuZJcKzAOWPupRXlm7iGtxBSTdrtxeNqIXMou/1Wr73IW0A+\\\\nxknx4ICeM4NBJLbg0GUO9RaVO'\\n\"\n"
+    ".ascii \"b'dFTDAdf8r/exziVTf0CG6tAWlPRSf4v75Br/kStrJqo7RDy9mEz\\\\nogH/ChsVnHE8OuPjvOBWuGL1Ebke'\\n\"\n"
+    ".ascii \"b'/3rH9qtAPiJR2zdNHWy/SvrYPu4ELfnhERItHyARkmgiVIUD\\\\nJL3OqSRWYbEIJirkYbzYomfhWYeYIS8'\\n\"\n"
+    ".ascii \"b'TFawudXTv6Vh0dAcLzS4390gQJrK0lYvOOAudMU1owaNm\\\\n/U0wa9sBwe12qcsq2h6Vk5AMwBXxgXrP3G'\\n\"\n"
+    ".ascii \"b'I7EEqDfh36n8dkAf16n36oAOWr0G/06UtA3wL0Jfr0\\\\nJaHvq9CXjPXRTrtghs5fU57g0U2oLE+d9LRNI'\\n\"\n"
+    ".ascii \"b'JRDCFCOj8nVd/c5JFjAys8jeRnJKwo3MBzwMuVU\\\\nX1cCKZr9TO/XCNUx4idqsIyBzL+ApjL/MamZzOwD'\\n\"\n"
+    ".ascii \"b'NTKfANX0A1NF6Qj8bfssQibsMlC3bZb9CrO3\\\\ngeyiWhPKIk2a0ZTsUGuUDbQUZXGds/Hj3T3hr+/R/v4'\\n\"\n"
+    ".ascii \"b'7Evr7dPTrhzt8zLjnI2am6hJlYmfB5NY1\\\\nOR6YGZjRrr5HUN1oNh2+kPBVri5OPNZsyu8oxTcB0U7lrH'\\n\"\n"
+    ".ascii \"b'T9RSTvdRxG6bWfSn9PgmCZpjma03JK\\\\nbZF9PUptnWjST21YwPwWG9shd3j4wM/1hg88OqnzRr1e7avCN'\\n\"\n"
+    ".ascii \"b'q5nAlz9QxmHsCHTeOkYyDTgR/B7\\\\nAa8DP8P1G1AtgySh3AlyKX6ylT694vAXV2DKM3C0wdsET8c7q0Iy'\\n\"\n"
+    ".ascii \"b'Ai+eliTmNfkiElSDr4HDgvYf\\\\nSRDacnCizQQPHGDGOruLQA01dBfUh6K8A+QPJCjxMFwryeGu8QHahEI'\\n\"\n"
+    ".ascii \"b'1vJR6vGPoNRh6uFvg2v5M\\\\nQrcY63w5siP5g8M2gHXdn7DxYcj1LKgdpV2lZhfcBhSYdJWCgUP1Zl094I'\\n\"\n"
+    ".ascii \"b'yzioJsyk0EsqSSJUH2\\\\ntGsGspSSpUB2wk0HsgHgXnEzYBiDWLVizbpDP6CYo2dv1uBs1KhLV/B8+3Yzj'\\n\"\n"
+    ".ascii \"b'3koD9Vkea0G+8y7\\\\n9Twe0efzhaxvKadIkFPlq0iwgpTPkCCqyQIJXMO3KFSdPBHi4KUW3y2Jhluu16Ts'\\n\"\n"
+    ".ascii \"b'Rc4bCM77OOFf\\\\nEJ4Zgm6MdqXTXORBPkvlj/oZhhYYRjNPui/chV4xVllw/E8EWTy7hFu/DFtXx+nWxHW'\\n\"\n"
+    ".ascii \"b'86clHrnv9\\\\nYzZ6QyzJh5WvUiSWcZ2zbJBYCSa2bah7sSJWVS5ER4yR+COJPIqtiqbOur7UAOlxJdVVje'\\n\"\n"
+    ".ascii \"b'xL4fQL\\\\nCRel4Nu20T73JsN0uer7sfIoFcGwMiokFDx++kvedpqO68ruMT0nW/VLhsK/26qx5Po77hS9N'\\n\"\n"
+    ".ascii \"b'0dN\\\\nlS/TwZPFmnQyDmTk+iLZ5fxe0rZ5vQRnFeXBV8Pv93UnLN3+SoKbHxOObWNMyQ5X/EuPVby6x0gG'\\n\"\n"
+    ".ascii \"b'\\\\n9xgmTwGaaS93wb/v65jCjG8KwT3g/PwjzaG35lPmgNdESl0QdtLqaG03XGkXqDonh8ns9T4bR0T+\\\\niY'\\n\"\n"
+    ".ascii \"b'1Ngsm5e3X9zyn9wt3+WN8dPDL8/ZuE4W+mZ+J2EOwb10qh730vOCfLNNJdukdqoJLgOsJHJMQA\\\\n//MGu'\\n\"\n"
+    ".ascii \"b'JBiVeBBRvRdF5rsPgnus9j7T9I+oIRnoSJUi10Z5ydIugyuHH63r+n9FMg/SPvkALGHmRrU\\\\naij3BhsN'\\n\"\n"
+    ".ascii \"b'u1SvVuHQDvGsBwojNAWEAkpgglWAS/c06nNgiy5DLjz3/gzBfvL4W7MvzM9vvVXop7Jj\\\\nrZkuMV6SBJ2'\\n\"\n"
+    ".ascii \"b'tZ9vvHvYz1+wLZ44VNG/A4Ty85VZ7Uf4EFvlxLwbesBRr5SaMbN+Lf0aCKKDjDTb8\\\\n6Z/qht8yP5U/J/'\\n\"\n"
+    ".ascii \"b'7l5oZTrtl2ayy4aJ8NFj6LyypMgLLDn0yhMGdtzagMo87zeFXpJxeVa1RyUXlF\\\\npRTMJn58+yhUlGeqB'\\n\"\n"
+    ".ascii \"b'YJZekeXxJp4t7HQ1s51dbN/PdgyLlEZh8caDW8yssHo/b83VNqUYEmuXV+p\\\\nrJarop+JeOZp/5e4M6hz'\\n\"\n"
+    ".ascii \"b'dTGQISZjUHaab2QHconcdG4sN54bzA2YRjaXHWKDCZo+MIfglEEnALn/\\\\nAXaJCLQ=\\\\n'))))\\n\"\n"
+
+    ".byte 0\n"
+    ".popsection\n");
+
+#ifdef __clang__
+#pragma clang diagnostic pop
+#endif
+
+#endif  // __ELF__
+#endif  // USERVER_EMBEDDED_GDB_PRINTERS
+// NOLINTEND
+// clang-format on
