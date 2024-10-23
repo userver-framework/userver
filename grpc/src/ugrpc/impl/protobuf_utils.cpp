114f730c85be155048b8d3ce709bdb636b4720a1
TTPO100AJIEX <69868712+TTPO100AJIEX@users.noreply.github.com>
2024-10-23 23:13:06 +0300

    Update protobuf_utils.cpp

--- grpc/src/ugrpc/impl/protobuf_utils.cpp
+++ grpc/src/ugrpc/impl/protobuf_utils.cpp
@@ -28,7 +28,7 @@ compiler::ThreadLocal kSecretVisitor = [] {
 
 }  // namespace
 
-userver::FieldOptions GetFieldOptions(const google::protobuf::FieldDescriptor& field) {
+const userver::FieldOptions& GetFieldOptions(const google::protobuf::FieldDescriptor& field) {
     return field.options().GetExtension(userver::field);
 }
 
