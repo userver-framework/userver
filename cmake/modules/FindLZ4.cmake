f8878f7e240c6426e82f46b6d5f2908d1887ae11
Fedor Osetrov <33493672+fdr400@users.noreply.github.com>
2024-12-23 12:31:04 +0300

    Update FindLZ4.cmake

--- cmake/modules/FindLZ4.cmake
+++ cmake/modules/FindLZ4.cmake
@@ -1,5 +1,5 @@
 _userver_module_begin(
-    NAME LZ4
+    NAME lz4
     VERSION 1.9.2
     DEBIAN_NAMES liblz4-dev
     FORMULA_NAMES lz4
@@ -16,4 +16,5 @@ _userver_module_find_library(
 
 _userver_module_end()
 
-add_library(LZ4::LZ4 ALIAS LZ4)
+add_library(LZ4::LZ4 ALIAS lz4)
+add_library(lz4::lz4 ALIAS lz4)
