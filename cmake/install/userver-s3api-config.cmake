ce6485bf6f569c8916b692b484d5eaf0c955b066
Antony Polukhin <antoshkka@userver.tech>
2025-01-03 10:26:07 +0300

    Update userver-s3api-config.cmake

--- cmake/install/userver-s3api-config.cmake
+++ cmake/install/userver-s3api-config.cmake
@@ -8,7 +8,11 @@ find_package(userver REQUIRED COMPONENTS
     core
 )
 
-include("${USERVER_CMAKE_DIR}/modules/FindPugixml.cmake")
+if(USERVER_CONAN)
+  find_package(pugixml REQUIRED CONFIG)
+else()
+  include("${USERVER_CMAKE_DIR}/modules/FindPugixml.cmake")
+endif()
 
 set(userver_s3api_FOUND TRUE)
 
