e1af6618d9253987c238e45a3c978e0009019fd0
Aleksey Ignatiev <iae.ignatev@gmail.com>
2025-01-07 21:29:56 +0300

    Rename redis_standalone.py -> pytest_plugin.py

--- testsuite/pytest_plugins/pytest_userver/plugins/redis.py
+++ testsuite/pytest_plugins/pytest_userver/plugins/redis.py
@@ -5,5 +5,5 @@ Plugin that imports the required fixtures to start the Redis database.
 pytest_plugins = [
     'testsuite.databases.redis.pytest_plugin',
     'pytest_userver.plugins.core',
-    'pytest_userver.plugins.redis_standalone.redis_standalone'
+    'pytest_userver.plugins.redis_standalone.pytest_plugin'
 ]
