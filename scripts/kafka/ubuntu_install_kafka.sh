9f4ff81e6d9fb00d44270c824776dd633fc20a42
fdr400 <fdr400@yandex-team.com>
2025-01-19 20:54:08 +0300

    Try use sudo on script

--- scripts/kafka/ubuntu_install_kafka.sh
+++ scripts/kafka/ubuntu_install_kafka.sh
@@ -1,7 +1,9 @@
+#!/bin/sh
+
 sudo apt install -y default-jre
 
 curl https://dlcdn.apache.org/kafka/3.8.0/kafka_2.13-3.8.0.tgz -o kafka.tgz
-sudo mkdir -p /etc/kafka
-sudo tar xf kafka.tgz --directory=/etc/kafka
-sudo cp -r /etc/kafka/kafka_2.13-3.8.0/* /etc/kafka/
-sudo rm -rf /etc/kafka/kafka_2.13-3.8.0
+mkdir -p /etc/kafka
+tar xf kafka.tgz --directory=/etc/kafka
+cp -r /etc/kafka/kafka_2.13-3.8.0/* /etc/kafka/
+rm -rf /etc/kafka/kafka_2.13-3.8.0
