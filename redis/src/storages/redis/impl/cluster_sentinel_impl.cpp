4b5f3654f8189ff55ad193055ce6bc64b664e5bb
Antony Polukhin <antoshkka@userver.tech>
2025-02-27 22:30:20 +0300

    Update cluster_sentinel_impl.cpp

--- redis/src/storages/redis/impl/cluster_sentinel_impl.cpp
+++ redis/src/storages/redis/impl/cluster_sentinel_impl.cpp
@@ -1069,7 +1069,6 @@ void ClusterSentinelImpl::EnqueueCommand(const SentinelCommand& command) {
 
 size_t ClusterSentinelImpl::ShardsCount() const {
     const auto ptr = topology_holder_->GetTopology();
-    UASSERT(ptr);
     const auto res = ptr->GetShardsCount();
     UASSERT(res != 0);
     return res;
