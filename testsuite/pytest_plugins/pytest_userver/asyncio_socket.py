34ddc16995f3ed69df3de077294c9cab51ce482c
Victor Makarov <vitja@yandex-team.ru>
2025-03-18 16:29:57 +0300

    fix

--- testsuite/pytest_plugins/pytest_userver/asyncio_socket.py
+++ testsuite/pytest_plugins/pytest_userver/asyncio_socket.py
@@ -4,13 +4,15 @@ import select
 import socket
 import typing
 
+DEFAULT_TIMEOUT = 10.0
+
 
 class AsyncioSocket:
     def __init__(
             self,
             sock: socket.socket,
             loop: typing.Optional[asyncio.AbstractEventLoop] = None,
-            timeout=10.0,
+            timeout=DEFAULT_TIMEOUT,
     ):
         if loop is None:
             loop = asyncio.get_running_loop()
@@ -21,6 +23,13 @@ class AsyncioSocket:
 
     def __repr__(self):
         return f'<AsyncioSocket for {self._sock}>'
+
+    @property
+    def type(self):
+        return self._sock.type
+
+    def fileno(self):
+        return self._sock.fileno()
 
     async def connect(self, address, *, timeout=None):
         async with self._timeout(timeout):
@@ -82,20 +91,31 @@ class AsyncioSocketsFactory:
             loop = asyncio.get_running_loop()
         self._loop = loop
 
-    def socket(self, *args, timeout=10.0):
+    def socket(self, *args, timeout=DEFAULT_TIMEOUT):
         sock = socket.socket(*args)
         return from_socket(sock, loop=self._loop, timeout=timeout)
 
-    def tcp(self, *, timeout=10.0):
-        return self.socket(socket.AF_INET, socket.SOCK_INET, timeout=timeout)
+    def tcp(self, *, timeout=DEFAULT_TIMEOUT):
+        return self.socket(socket.AF_INET, socket.SOCK_STREAM, timeout=timeout)
 
-    def udp(self, *, timeout=10.0):
+    def udp(self, *, timeout=DEFAULT_TIMEOUT):
         return self.socket(socket.AF_INET, socket.SOCK_DGRAM, timeout=timeout)
 
 
 def from_socket(
-        sock: typing.Union[socket.socket, AsyncioSocket], *, loop=None, timeout=10.0,
+        sock: typing.Union[socket.socket, AsyncioSocket], *, loop=None, timeout=DEFAULT_TIMEOUT,
 ) -> AsyncioSocket:
     if isinstance(sock, AsyncioSocket):
         return sock
     return AsyncioSocket(sock, loop=loop, timeout=timeout)
+
+
+def create_socket(*args, timeout=DEFAULT_TIMEOUT):
+    return AsyncioSocketsFactory().socket(*args, timeout=timeout)
+
+def create_tcp_socket(*args, timeout=DEFAULT_TIMEOUT):
+    return AsyncioSocketsFactory().tcp(timeout=timeout)
+
+
+def create_udp_socket(timeout=DEFAULT_TIMEOUT):
+    return AsyncioSocketsFactory().udp(timeout=timeout)
