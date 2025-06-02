593a31bdf59d17372c9bc81644071f320c0cda35
fdr400 <fdr400@yandex-team.com>
2025-06-02 22:05:08 +0300

    do not use fmt::detail

--- clickhouse/include/userver/storages/clickhouse/io/floating_point_types.hpp
+++ clickhouse/include/userver/storages/clickhouse/io/floating_point_types.hpp
@@ -3,9 +3,6 @@
 /// @file userver/storages/clickhouse/io/floating_point_types.hpp
 /// @brief @copybrief storages::clickhouse::io::FloatingWithPrecision
 
-#if FMT_VERSION >= 110000
-#include <fmt/base.h>  // Y_IGNORE
-#endif
 #include <fmt/format.h>
 
 USERVER_NAMESPACE_BEGIN
@@ -48,36 +45,11 @@ public:
     }
 
     std::string ToString() {
-        std::string result;
-        fmt::detail::write(std::back_inserter(result), value_, spec_);
-        return result;
+        return fmt::format("{:.{}f}", value_, Precision);
     }
 
 private:
     FloatingPointT value_;
-
-#if FMT_VERSION >= 110100
-    constexpr static fmt::format_specs spec_ = []() {
-        fmt::format_specs spec;
-        spec.precision = Precision;
-        spec.set_type(fmt::presentation_type::fixed);
-        return spec;
-    }();
-#elif FMT_VERSION >= 110000
-    constexpr static fmt::format_specs spec_ = []() {
-        fmt::format_specs spec;
-        spec.precision = Precision;
-        spec.type = fmt::presentation_type::fixed;
-        return spec;
-    }();
-#else
-    constexpr static fmt::basic_format_specs<char> spec_ = []() {
-        fmt::basic_format_specs<char> spec;
-        spec.precision = Precision;
-        spec.type = fmt::presentation_type::fixed_lower;
-        return spec;
-    }();
-#endif
 
     template <typename U, std::uint32_t AnotherPrecision, typename>
     friend class FloatingWithPrecision;
