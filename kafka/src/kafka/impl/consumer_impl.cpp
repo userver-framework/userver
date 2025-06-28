8edb1bb6d3430dd5b964f8528107ac0a03a45360
melonaerial <melon.aerial@protonmail.ch>
2025-06-28 16:38:52 +0500

    Fix for formatting.

--- kafka/src/kafka/impl/consumer_impl.cpp
+++ kafka/src/kafka/impl/consumer_impl.cpp
@@ -153,8 +153,7 @@ void PrintTopicPartitionsList(
     }
 
     const utils::span<const rd_kafka_topic_partition_t> topic_partitions{
-        list->elems, list->elems + static_cast<std::size_t>(list->cnt)
-    };
+        list->elems, list->elems + static_cast<std::size_t>(list->cnt)};
     for (const auto& topic_partition : topic_partitions) {
         if (skip_invalid_offsets && topic_partition.offset == RD_KAFKA_OFFSET_INVALID) {
             /// @note `librdkafka` does not sets offsets for partitions that were
@@ -446,8 +445,7 @@ ConsumerImpl::GetPartitionIds(const std::string& topic, std::optional<std::chron
     }()};
 
     const utils::span<const rd_kafka_metadata_topic> topics{
-        metadata->topics, static_cast<std::size_t>(metadata->topic_cnt)
-    };
+        metadata->topics, static_cast<std::size_t>(metadata->topic_cnt)};
     const auto* topic_it =
         std::find_if(topics.begin(), topics.end(), [&topic](const rd_kafka_metadata_topic& topic_raw) {
             return topic == topic_raw.topic;
@@ -457,8 +455,7 @@ ConsumerImpl::GetPartitionIds(const std::string& topic, std::optional<std::chron
     }
 
     const utils::span<const rd_kafka_metadata_partition> partitions{
-        topic_it->partitions, static_cast<std::size_t>(topic_it->partition_cnt)
-    };
+        topic_it->partitions, static_cast<std::size_t>(topic_it->partition_cnt)};
     std::vector<std::uint32_t> partition_ids;
     partition_ids.reserve(partitions.size());
 
