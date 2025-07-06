156788977dfdbb8e82bbb710551a20cc0d7f423d
Konstantin Goncharik <botankras@gmail.com>
2025-07-07 02:09:38 +0700

    fix: add possibly forgotten ::benchmark::Shutdown() call

--- core/benchmarks/main.cpp
+++ core/benchmarks/main.cpp
@@ -15,4 +15,5 @@ int main(int argc, char** argv) {
     ::benchmark::Initialize(&argc, argv);
     if (::benchmark::ReportUnrecognizedArguments(argc, argv)) return 1;
     ::benchmark::RunSpecifiedBenchmarks();
+    ::benchmark::Shutdown();
 }
