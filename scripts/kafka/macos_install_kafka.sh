0f2c85f7f27c15bccb90137b968756eabc3eaff5
MichaelKab <michael.kabanov4@gmail.com>
2025-07-28 22:18:54 +0300

    fix

--- scripts/kafka/macos_install_kafka.sh
+++ scripts/kafka/macos_install_kafka.sh
@@ -3,7 +3,7 @@
 brew install openjdk@17
 
 export KAFKA_PATH="/opt/homebrew/opt/kafka/libexec"
-curl https://dlcdn.apache.org/kafka/3.8.0/kafka_2.13-4.0.0.tgz -o kafka.tgz
+curl https://dlcdn.apache.org/kafka/4.0.0/kafka_2.13-4.0.0.tgz -o kafka.tgz
 mkdir -p ${KAFKA_PATH}
 tar xf kafka.tgz --directory="${KAFKA_PATH}"
 cp -r ${KAFKA_PATH}/kafka_2.13-4.0.0/* ${KAFKA_PATH}
