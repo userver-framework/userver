2331a09fb9f0e35f617779c87f3bac73629dc360
MichaelKab <michael.kabanov4@gmail.com>
2025-07-28 22:39:08 +0300

    fix

--- scripts/kafka/ubuntu_install_kafka.sh
+++ scripts/kafka/ubuntu_install_kafka.sh
@@ -1,13 +1,22 @@
 #!/bin/sh
 
-sudo apt install -y openjdk-17-jdk
-sudo update-alternatives --display java
-sudo update-alternatives --set java /usr/lib/jvm/java-17-openjdk-amd64/bin/java
+if [ "$(uname)" = "Darwin" ]; then
+  brew install openjdk@17
+else
+  sudo apt install -y openjdk-17-jdk
+fi
 
+if [ -z ${KAFKA_PATH+x} ]; then
+  if [ "$(uname)" = "Darwin" ]; then
+    export KAFKA_PATH="/opt/homebrew/opt/kafka/libexec"
+  else
+    export KAFKA_PATH="/etc/kafka"
+  fi
+fi
 
 curl https://dlcdn.apache.org/kafka/4.0.0/kafka_2.13-4.0.0.tgz -o kafka.tgz
-mkdir -p /etc/kafka
-tar xf kafka.tgz --directory=/etc/kafka
-cp -r /etc/kafka/kafka_2.13-4.0.0/* /etc/kafka/
-rm -rf /etc/kafka/kafka_2.13-4.0.0
+mkdir -p ${KAFKA_PATH}
+tar xf kafka.tgz --directory="${KAFKA_PATH}"
+cp -r ${KAFKA_PATH}/kafka_2.13-4.0.0/* ${KAFKA_PATH}
+rm -rf ${KAFKA_PATH}/kafka_2.13-4.0.0
 rm kafka.tgz
