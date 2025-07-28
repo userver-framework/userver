2d7e2c371cb05415101ec8de5122f4a5453e293d
MichaelKab <michael.kabanov4@gmail.com>
2025-07-28 18:16:45 +0300

    java default path

--- scripts/kafka/ubuntu_install_kafka.sh
+++ scripts/kafka/ubuntu_install_kafka.sh
@@ -1,6 +1,10 @@
 #!/bin/sh
 
 sudo apt install -y openjdk-17-jdk
+java -version
+export JAVA_HOME=/usr/lib/jvm/java-17-openjdk-amd64
+export PATH=$PATH:$JAVA_HOME/bin
+
 
 curl https://dlcdn.apache.org/kafka/4.0.0/kafka_2.13-4.0.0.tgz -o kafka.tgz
 mkdir -p /etc/kafka
