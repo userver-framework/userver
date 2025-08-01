2882de8e82b924d3006c48ed580d4f37854afe71
aselutin <aselutin@yandex-team.com>
2025-08-01 14:00:14 +0300

    feat userver: revert integration tests
    revert integration tests
    commit_hash:095e6569deec6cab5285929b5f65d8654b5fcf20

---
+++ postgresql/functional_tests/integration_tests/tests/conftest.py
@@ -0,0 +1,12 @@
+pytest_plugins = [
+    'testsuite.pytest_plugin',
+    'pytest_userver.plugins.caches',
+    'pytest_userver.plugins.config',
+    'pytest_userver.plugins.dumps',
+    'pytest_userver.plugins.dynamic_config',
+    'pytest_userver.plugins.log_capture',
+    'pytest_userver.plugins.service',
+    'pytest_userver.plugins.service_client',
+    'pytest_userver.plugins.testpoint',
+    'pytest_userver.plugins.postgresql',
+]
