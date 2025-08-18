d6acde5bf9f38058b18c5c2d199a9c733ee3a57d
Vasily Kulikov <segoon@yandex-team.ru>
2025-08-19 01:45:03 +0300

    w

--- cmake/SetupProtobuf.cmake
+++ cmake/SetupProtobuf.cmake
@@ -36,7 +36,6 @@ if(NOT USERVER_FORCE_DOWNLOAD_PROTOBUF)
     if(USERVER_DOWNLOAD_PACKAGE_PROTOBUF)
         find_package(Protobuf QUIET)
     else()
-        set(Protobuf_DEBUG ON)
         find_package(Protobuf)
         if(NOT Protobuf_FOUND)
             message(
