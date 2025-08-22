2460839cd1db4e2f2c21e92e84603e61c2aeb358
segoon <segoon@userver.tech>
2025-08-22 23:26:45 +0300

    cc chaotic: improve in-code documentation
    Relates: <https://nda.ya.ru/t/O-A5FHlf7HxHuo>
    commit_hash:825ffd61ad2ce413d22815f238ba8272b7e24b73

--- chaotic-openapi/chaotic_openapi/back/cpp_client/middleware.py
+++ chaotic-openapi/chaotic_openapi/back/cpp_client/middleware.py
@@ -5,6 +5,26 @@ class Middleware(abc.ABC):
     """
     Per-operation middleware.
     It generates a single struct member for `Request` type.
+    If we want to represent the structure as a jinja template,
+    it will look something like that:
+
+        struct Request {
+            {{ middleware.request_member_cpp_type }} {{ middleware.request_member_name }};
+        };
+
+        void SerializeRequest(
+            const Request& request,
+            const std::string& base_url,
+            USERVER_NAMESPACE::clients::http::Request& http_request
+        )
+        {
+            openapi::ParameterSinkHttpClient sink(
+                http_request,
+                base_url + "{{ op.path }}"
+            );
+            ...
+            {{ middleware.write_member_command('request', 'sink', 'http_request') }}
+        }
     """
 
     @property
@@ -53,7 +73,6 @@ class MiddlewarePlugin(abc.ABC):
     Implement `MiddlewarePlugin` if you want to extend OpenAPI schema
     with your middleware. The schema is extended with
     `x-usrv-middleware.` + MiddlewarePlugin.field object member.
-    Each operation with the middleware member triggers `create`.
 
     A plugin is created per-parser.
     """
@@ -61,8 +80,17 @@ class MiddlewarePlugin(abc.ABC):
     @property
     @abc.abstractmethod
     def field(self) -> str:
+        """
+        Returns `x-usrv-middleware` property name that is responsible for
+        middleware activation and parameters storage.
+        """
         pass
 
     @abc.abstractmethod
     def create(self, args: dict) -> Middleware:
+        """
+        The method is called every time a HTTP operation with
+        "x-usrv-middleware." + field property is discovered.
+        It creates a per-operation `Middleware` object.
+        """
         pass
