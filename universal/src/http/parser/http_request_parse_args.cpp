4237328357c61941af1b8e46cb0e385058b2b059
mistergad <auditore849@gmail.com>
2025-08-26 16:21:11 +0300

    Fix problem with /endpoint?args parameters without =

--- universal/src/http/parser/http_request_parse_args.cpp
+++ universal/src/http/parser/http_request_parse_args.cpp
@@ -72,7 +72,15 @@ void ParseAndConsumeArgs(std::string_view args, ArgsConsumer handler) {
                         USERVER_NAMESPACE::http::parser::UrlDecode(value)
                     );
                 }
+            } else {
+                key_end = ptr;
+                const std::string_view key(key_begin, key_end - key_begin);
+                handler(
+                    USERVER_NAMESPACE::http::parser::UrlDecode(key),
+                    USERVER_NAMESPACE::http::parser::UrlDecode("1")
+                );
             }
+
             parse_key = true;
             key_begin = ptr + 1;
             continue;
