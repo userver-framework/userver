3f635f5f94f237e65ab7d67be4ab4cd8874db218
Vasily Kulikov <segoon@yandex-team.ru>
2025-08-26 21:43:32 +0300

    w

--- cmake/SetupBoost.cmake
+++ cmake/SetupBoost.cmake
@@ -9,6 +9,19 @@ option(USERVER_FORCE_DOWNLOAD_BOOST "Download Boost even if there is an installe
        ${USERVER_FORCE_DOWNLOAD_PACKAGES}
 )
 
+
+set(BOOST_VERSION 1.89.0)
+set(BOOST_ENABLE_CMAKE ON)
+set(BOOST_INCLUDE_LIBRARIES
+    program_options
+    filesystem
+    regex
+    locale
+    iostreams
+    context
+    coroutine
+)
+
 # if(NOT USERVER_FORCE_DOWNLOAD_BOOST)
 #     if(USERVER_DOWNLOAD_PACKAGE_BOOST)
 #         find_package(
@@ -29,84 +42,46 @@ option(USERVER_FORCE_DOWNLOAD_BOOST "Download Boost even if there is an installe
 #     endif()
 # endif()
 
+
+if(FALSE)
 cpmaddpackage(
     NAME Boost
-    VERSION 1.89
+    VERSION ${BOOST_VERSION}
     GITHUB_REPOSITORY boostorg/cmake
-    GIT_TAG boost-1.89.0
+    GIT_TAG boost-${BOOST_VERSION}
     SOURCE_DIR "${CMAKE_BINARY_DIR}/Boost"
     EXCLUDE_FROM_ALL
 )
 include("${CMAKE_BINARY_DIR}/Boost/include/BoostFetch.cmake")
 
-set(BOOST_INCLUDE_LIBRARIES
-    program_options
-    filesystem
-    regex
-    locale
-    iostreams
-    assert
-    context
-    coroutine
-    bind
-    throw_exception
-    type_traits
-    tokenizer
-    core
-    config
-    iterator
-    static_assert
-    lexical_cast
-    mpl
-    function
-    container
-    smart_ptr
-    any
-    scope
-    exception
-    random
-    preprocessor
-    utility
-    detail
-    move
-    intrusive
-    type_index
-    optional
-    io
-    predef
-    system
-    winapi
-    integer
-    thread
-    fusion
-    container_hash
-    variant2
-    mp11
-    describe
-    functional
-    typeof
-    tuple
-    pool
-    atomic
-    dynamic_bitset
-    function_types
-    concept_check
-    stacktrace_basic
-    align
-    date_time
-    chrono
-    numeric_convertion
-    charconv
-    ratio
-    range
-    conversion
-    algorithm
-    array
-    unordered
-)
 foreach(LIBRARY ${BOOST_INCLUDE_LIBRARIES})
-    boost_fetch(boostorg/${LIBRARY} TAG boost-1.89.0 EXCLUDE_FROM_ALL)
+    boost_fetch(boostorg/${LIBRARY} TAG boost-${BOOST_VERSION} EXCLUDE_FROM_ALL)
 endforeach()
+endif()
 
-# set(Boost_VERSION_STRING "1.89")
-# set(Boost_INCLUDE_DIR ${CMAKE_BINARY_DIR}/_deps/ CACHE PATH "" FORCE)
+
+include(FetchContent)
+cpmaddpackage(
+    NAME Boost
+    VERSION 1.89.0
+    # URL https://archives.boost.io/release/1.89.0/source/boost_1_89_0.tar.bz2
+    # URL_HASH SHA256=85a33fa22621b4f314f8e85e1a5e2a9363d22e4f4992925d4bb3bc631b5a0c7a
+
+    
+    URL https://github.com/boostorg/boost/releases/download/boost-1.86.0/boost-1.86.0-cmake.tar.xz
+    URL_HASH SHA256=2c5ec5edcdff47ff55e27ed9560b0a0b94b07bd07ed9928b476150e16b0efc57
+
+    # GIT_REPOSITORY https://github.com/boostorg/boost.git
+    # GIT_TAG boost-${BOOST_VERSION}
+    # GIT_PROGRESS TRUE   
+    # GIT_SHALLOW TRUE
+    OPTIONS
+        "BOOST_ENABLE_CMAKE ON"
+        "BOOST_INCLUDE_LIBRARIES program_options\\\;filesystem\\\;regex\\\;locale\\\;iostreams\\\;context\\\;coroutine\\\;stacktrace\\\;uuid"
+    EXCLUDE_FROM_ALL
+)
+
+find_package(
+    Boost REQUIRED
+    COMPONENTS ${BOOST_INCLUDE_LIBRARIES}
+)
