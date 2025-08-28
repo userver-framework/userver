8b4e886e717890196f59a0d5742b403c8bdb3090
Vasily Kulikov <segoon@yandex-team.ru>
2025-08-28 12:05:45 +0300

    work

--- cmake/SetupOpenssl.cmake
+++ cmake/SetupOpenssl.cmake
@@ -1,9 +1,12 @@
 include_guard(GLOBAL)
 
-find_package(OpenSSL)
-if(OpenSSL_FOUND)
-    return()
+if(NOT OpenSSL_CPM)
+  find_package(OpenSSL)
+  if(OpenSSL_FOUND)
+      return()
+  endif()
 endif()
+set(OpenSSL_CPM TRUE CACHE BOOL "")
  
 include(DownloadUsingCPM)
 
@@ -11,7 +14,7 @@ set(OPENSSL_INSTALL_DIR ${CMAKE_BINARY_DIR}/openssl)
 execute_process(COMMAND mkdir -p ${OPENSSL_INSTALL_DIR}/usr/local/include)
 
 # Flags are copied from Ubuntu's debian/rules
-set(CONFIGURE_FLAGS "no-idea no-mdc2 no-rc5 no-zlib no-ssl3 enable-unit-test no-ssl3-method enable-rfc3779 enable-cms no-capieng")
+set(CONFIGURE_FLAGS no-idea no-mdc2 no-rc5 no-zlib no-ssl3 enable-unit-test no-ssl3-method enable-rfc3779 enable-cms no-capieng)
 set(OPENSSL_VERSION 3.5.2)
 
 cpmaddpackage(
@@ -20,7 +23,8 @@ cpmaddpackage(
      URL_HASH SHA512=db2c7a88bea432f96d867a98af15f850f371d4136c657338de93cb88a39a3578c025b5df7310e195a02fc715ad5a2422a319a44f0247c6a7e2ba8b36aad77651
 )
 
-
+# We use custom target to be able to set build dependency
+# for *external* libcurl from CPM
 add_custom_target(
     OpenSSL
         test -e ${OPENSSL_INSTALL_DIR}/.installed || ./config ${CONFIGURE_FLAGS}
@@ -48,3 +52,7 @@ target_include_directories(SSL INTERFACE ${CMAKE_BINARY_DIR}/openssl/usr/local/i
 
 add_library(OpenSSL::Crypto ALIAS Crypto)
 add_library(OpenSSL::SSL ALIAS SSL)
+
+# Light emulation of find_package(OpenSSL)
+set(OpenSSL_FOUND TRUE CACHE BOOL "" FORCE)
+set(OPENSSL_FOUND TRUE CACHE BOOL "" FORCE)
