001159caf2ef0267f43cc25e3a483e5057c7975d
Vasily Kulikov <segoon@yandex-team.ru>
2025-08-31 14:22:20 +0300

    fix libev + libnghttp2

--- cmake/modules/Findlibev.cmake
+++ cmake/modules/Findlibev.cmake
@@ -25,7 +25,8 @@ if(NOT TARGET libev::libev)
     if(TARGET libev)
         add_library(libev::libev ALIAS libev)
     elseif(libev_ADDED)
-        # libev is autoconf'ed, so create targets manually
+        # nghttp2 doesn't use find_package(), but calls find_path() and find_library()
+        # so we have to provide libev.a at the _configure_ time, not at build time, =(
         execute_process(
             COMMAND ${CMAKE_COMMAND} -E copy_directory ${libev_SOURCE_DIR} ${libev_BINARY_DIR}
         )
@@ -33,15 +34,24 @@ if(NOT TARGET libev::libev)
             COMMAND ./configure
             WORKING_DIRECTORY ${libev_BINARY_DIR}
         )
-
-        add_custom_command(
-            OUTPUT ${libev_BINARY_DIR}/libev.a
+        execute_process(
             COMMAND make
             WORKING_DIRECTORY ${libev_BINARY_DIR}
         )
+        execute_process(
+            COMMAND rm -rf ${libev_BINARY_DIR}/.libs/libev.so
+        )
+
         add_library(libev STATIC IMPORTED)
         target_include_directories(libev INTERFACE ${libev_BINARY_DIR})
-        set_target_properties(libev PROPERTIES IMPORTED_LOCATION ${libev_BINARY_DIR}/libev.a)
+        set_target_properties(libev PROPERTIES IMPORTED_LOCATION ${libev_BINARY_DIR}/.libs/libev.a)
+
+        # For nghttp2 installed from CPM
+        list(APPEND CMAKE_INCLUDE_PATH ${libev_BINARY_DIR})
+        list(APPEND CMAKE_LIBRARY_PATH ${libev_BINARY_DIR}/.libs)
+
         add_library(libev::libev ALIAS libev)
+    else()
+        message(FATAL_ERROR "libev cmake target not found, don't know how to link")
     endif()
 endif()
