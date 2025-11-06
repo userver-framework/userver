ad3b99d20fddab65361b87561e337515afa41592
Denis Razinkin <denis.razinkin@exness.com>
2025-11-06 17:43:34 +0200

    buildfix

--- conanfile.py
+++ conanfile.py
@@ -108,10 +108,8 @@ class UserverConan(ConanFile):
                 'version.txt',
             ),
         )
-        major_version = re.search(r'set\(USERVER_MAJOR_VERSION (.*)\)', content).group(1).strip()
-        minor_version = re.search(r'set\(USERVER_MINOR_VERSION (.*)\)', content).group(1).strip()
 
-        self.version = f'{major_version}.{minor_version}'  # pylint: disable=attribute-defined-outside-init
+        self.version = content.strip()  # pylint: disable=attribute-defined-outside-init
 
     def layout(self):
         cmake_layout(self)
