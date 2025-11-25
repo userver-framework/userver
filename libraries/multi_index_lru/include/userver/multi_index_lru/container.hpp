a93d953a6f33bea40fd80cc7991e520822df85b8
Vadim Leonov <vleonov@edu.hse.ru>
2025-11-25 10:50:12 +0300

    using sequenced_tag instead of boost::intrusive::list

--- libraries/multi_index_lru/include/userver/multi_index_lru/container.hpp
+++ libraries/multi_index_lru/include/userver/multi_index_lru/container.hpp
@@ -1,14 +1,12 @@
 #pragma once
 
-#include <boost/intrusive/link_mode.hpp>
-#include <boost/intrusive/list.hpp>
-#include <boost/intrusive/list_hook.hpp>
 #include <boost/multi_index/hashed_index.hpp>
-#include <boost/multi_index/mem_fun.hpp>
-#include <boost/multi_index/tag.hpp>
-#include <boost/multi_index/ordered_index.hpp>
-#include <boost/multi_index/member.hpp>
+#include <boost/multi_index/sequenced_index.hpp>
+#include <boost/multi_index/identity.hpp>
 #include <boost/multi_index_container.hpp> 
+
+#include <boost/mpl/list.hpp>
+#include <boost/mpl/joint_view.hpp>
 
 #include <utility>
 #include <cstddef>
@@ -17,59 +15,10 @@ USERVER_NAMESPACE_BEGIN
 
 namespace multi_index_lru {
 
-namespace impl {
-template<typename Value>
-struct ValueWithHook
-{
-    Value value;
-    mutable boost::intrusive::list_member_hook<> list_hook;
-    using boost_list = boost::intrusive::list<
-                            ValueWithHook,
-                            boost::intrusive::member_hook<
-                                ValueWithHook,
-                                boost::intrusive::list_member_hook<>, 
-                                &ValueWithHook::list_hook
-                            >
-                        >;
-
-    const ValueWithHook *GetPointerToSelf() const { return this; };
-
-    explicit ValueWithHook(const Value& val) : value(val) {}
-        
-    explicit ValueWithHook(Value&& val) : value(std::move(val)) {}
-    
-    ValueWithHook() = delete;
-    ValueWithHook(const ValueWithHook&) = delete;
-    ValueWithHook(ValueWithHook &&) = delete;
-
-    ValueWithHook &operator=(const ValueWithHook&) = delete;
-    ValueWithHook &operator=(ValueWithHook&&) = delete;
-    
-    operator Value&() { return value; }
-    operator const Value&() const { return value; }
-    
-    Value* operator->() { return &value; }
-    const Value* operator->() const { return &value; }
-
-    Value& get() { return value; }
-    const Value& get() const { return value; }
-
-    void PushBackToList(boost_list &lst) const {
-        lst.push_back(const_cast<ValueWithHook&>(*this));
-    }
-
-    void SpliceInList(boost_list &lst) const {
-        lst.splice(lst.end(), lst, lst.iterator_to(const_cast<ValueWithHook&>(*this)));
-    }
-};
-
-struct internalPtrTag {};
-} // namespace impl
-
 template<
     typename Value,
     typename IndexSpecifierList,
-    typename Allocator = std::allocator<impl::ValueWithHook<Value>>
+    typename Allocator = std::allocator<Value>
 >
 class LRUCacheContainer {
 public:
@@ -77,28 +26,19 @@ public:
     
     template<typename... Args>
     bool emplace(Args&&... args) {
-        if (container.size() >= max_size) {
-            evict_lru();
-        }
-        
-        auto result = container.emplace(std::forward<Args>(args)...);
+        auto result = container.emplace_front(std::forward<Args>(args)...);
 
-        auto &value = *result.first;
-        if (result.second) {
-            value.PushBackToList(usage_list);
-        } else {
-            value.SpliceInList(usage_list);
+        if (result.second == false) {
+            container.relocate(container.begin(),result.first);
+        } else if (container.size() >= max_size) {
+            container.pop_back();
         }
         return result.second;
     }
     
-    bool insert(const Value& value) {
-        return emplace(value);
-    }
-    
-    bool insert(Value&& value) {
-        return emplace(std::move(value));
-    }
+    bool insert(const Value& value) { return emplace(value); }
+
+    bool insert(Value&& value) { return emplace(std::move(value)); }
     
     template<typename Tag, typename Key>
     auto find(const Key& key) {
@@ -106,7 +46,7 @@ public:
         auto it = primary_index.find(key);
         
         if (it != primary_index.end()) {
-            it->SpliceInList(usage_list);
+            container.relocate(container.begin(),it);
         }
         
         return it;
@@ -119,22 +59,7 @@ public:
     
     template<typename Tag, typename Key>
     bool erase(const Key& key) {
-        auto& primary_index = container.template get<Tag>();
-        auto it = primary_index.find(key);
-        if (it != primary_index.end()) {
-            usage_list.erase(usage_list.iterator_to(*it));
-        }
         return container.template get<Tag>().erase(key) > 0;
-    }
-    
-    template<typename Tag>
-    auto& get() {
-        return container.template get<Tag>();
-    }
-    
-    template<typename Tag>
-    const auto& get() const {
-        return container.template get<Tag>();
     }
     
     size_t size() const { return container.size(); }
@@ -144,7 +69,7 @@ public:
     void set_capacity(size_t new_capacity) {
         max_size = new_capacity;
         while (container.size() > max_size) {
-            evict_lru();
+            container.pop_back();
         }
     }
     
@@ -153,41 +78,24 @@ public:
     }
 
 private:
-    using CacheItem = impl::ValueWithHook<Value>;
-    using List =  boost::intrusive::list<
-                        CacheItem,
-                        boost::intrusive::member_hook<
-                            CacheItem,
-                            boost::intrusive::list_member_hook<>, 
-                            &CacheItem::list_hook
-                        >
-                    >;
-
-    using ExtendedIndexSpecifierList = typename boost::mpl::push_back<
-        IndexSpecifierList,
+    using AdditionalIndices = boost::mpl::list<
+        boost::multi_index::sequenced<>,
         boost::multi_index::hashed_unique<
-            boost::multi_index::tag<impl::internalPtrTag>,
-            boost::multi_index::const_mem_fun<CacheItem, const CacheItem*, &CacheItem::GetPointerToSelf>
+            boost::multi_index::identity<Value>
         >
-    >::type;
+    >;
+
+    using ExtendedIndexSpecifierList =
+        boost::mpl::joint_view<IndexSpecifierList, AdditionalIndices>;
 
     using Container = boost::multi_index::multi_index_container<
-        CacheItem,
+        Value,
         ExtendedIndexSpecifierList,
         Allocator
     >;
 
-    void evict_lru() {
-        if (!usage_list.empty()) {
-            CacheItem *ptr_to_erase = &*usage_list.begin();
-            usage_list.erase(usage_list.begin());
-            container.template get<impl::internalPtrTag>().erase(ptr_to_erase);
-        }
-    }
-
     Container container;
     size_t max_size;
-    List usage_list;
 };
 } // namespace multi_index_lru
 
