deacd992e08c6608eacc6361346c7f2641341e54
Antony Polukhin <antoshkka@userver.tech>
2025-11-27 20:32:26 +0300

    Update conanfile.py

--- conanfile.py
+++ conanfile.py
@@ -150,7 +150,7 @@ class UserverConan(ConanFile):
             self.requires('cyrus-sasl/2.1.28')
         if self.options.with_mongodb:
             self.requires(
-                'mongo-c-driver/1.28.0',
+                'mongo-c-driver/1.30.3',
                 transitive_headers=True,
                 transitive_libs=True,
             )
