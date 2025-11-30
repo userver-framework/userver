69c3437d12d5ab9eec41889cbdd801f2e0b6d19b
Vadim Leonov <vleonov@edu.hse.ru>
2025-11-30 18:21:31 +0300

    own realization for adding new index

--- libraries/multi-index-lru/include/userver/multi-index-lru/container.hpp
+++ libraries/multi-index-lru/include/userver/multi-index-lru/container.hpp
@@ -10,14 +10,65 @@
 
 #include <boost/mpl/list.hpp>
 #include <boost/mpl/joint_view.hpp>
+#include <boost/mpl/aux_/na.hpp>
 
 #include <utility>
 #include <cstddef>
+#include <tuple>
 
 USERVER_NAMESPACE_BEGIN
 
 namespace multi_index_lru {
 
+namespace impl {
+template<typename T>
+constexpr bool is_mpl_na = std::is_same_v<T, boost::mpl::na>;
+
+template<typename First, typename... Rest>
+struct filter_na {
+    using rest_filtered = typename filter_na<Rest...>::type;
+
+    using type = std::conditional_t<
+        is_mpl_na<First>,
+        rest_filtered,
+        decltype(std::tuple_cat(std::tuple<First>(), rest_filtered()))
+    >;
+};
+
+template<typename First>
+struct filter_na<First> {
+    using type = std::conditional_t<
+        is_mpl_na<First>,
+        std::tuple<>,
+        std::tuple<First>
+    >;
+};
+
+template<typename IndexList> struct add_seq_index {};
+
+template<typename... Indices>
+struct add_seq_index<boost::multi_index::indexed_by<Indices...>> {
+private:
+    using filtered = typename filter_na<Indices...>::type;
+
+    template<typename Tuple>
+    struct wrap_tuple;
+
+    template<typename... Ts>
+    struct wrap_tuple<std::tuple<Ts...>> {
+        using type = boost::multi_index::indexed_by<
+            boost::multi_index::sequenced<>,
+            Ts...
+        >;
+    };
+
+public:
+    using type = typename wrap_tuple<filtered>::type;
+};
+
+template<typename IndexList>
+using add_seq_index_t = typename add_seq_index<IndexList>::type;
+} // namespace impl
 
 /// @ingroup userver_containers
 ///
@@ -29,7 +80,7 @@ template<
 >
 class Container {
 public:
-    explicit Container(size_t max_size) : max_size_(max_size) {}
+    explicit Container(std::size_t max_size) : max_size_(max_size) {}
     
     template<typename... Args>
     bool emplace(Args&&... args) {
@@ -72,11 +123,11 @@ public:
         return container_.template get<Tag>().erase(key) > 0;
     }
 
-    size_t size() const { return container_.size(); }
+    std::size_t size() const { return container_.size(); }
     bool empty() const { return container_.empty(); }
-    size_t capacity() const { return max_size_; }
+    std::size_t capacity() const { return max_size_; }
 
-    void set_capacity(size_t new_capacity) {
+    void set_capacity(std::size_t new_capacity) {
         max_size_ = new_capacity;
         auto& seq_index = container_.template get<0>();
         while (container_.size() > max_size_) {
@@ -94,12 +145,7 @@ public:
     }
 
 private:
-    using AdditionalIndices = boost::mpl::list<
-        boost::multi_index::sequenced<>
-    >;
-
-    using ExtendedIndexSpecifierList =
-        boost::mpl::joint_view<AdditionalIndices, IndexSpecifierList>;
+    using ExtendedIndexSpecifierList = impl::add_seq_index_t<IndexSpecifierList>;
 
     using BoostContainer = boost::multi_index::multi_index_container<
         Value,
@@ -108,7 +154,7 @@ private:
     >;
 
     BoostContainer container_;
-    size_t max_size_;
+    std::size_t max_size_;
 };
 } // namespace multi_index_lru
 
