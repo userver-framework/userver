048d0cc157ca65e6551d6caf3c497b37a4ffc7da
Konstantin Pavlov <kpavlov00@userver.tech>
2025-12-24 13:43:24 +0300

    replace call 'TTableClient::GetParamsBuilder()' -> 'NYdb::TParamsBuilder{}'

--- ydb/src/ydb/table.cpp
+++ ydb/src/ydb/table.cpp
@@ -441,7 +441,7 @@ void DumpMetric(utils::statistics::Writer& writer, const TableClient& table_clie
     );
 }
 
-PreparedArgsBuilder TableClient::GetBuilder() const { return PreparedArgsBuilder(table_client_->GetParamsBuilder()); }
+PreparedArgsBuilder TableClient::GetBuilder() const { return PreparedArgsBuilder(NYdb::TParamsBuilder{}); }
 
 NYdb::NTable::TExecDataQuerySettings TableClient::ToExecQuerySettings(QuerySettings query_settings) const {
     NYdb::NTable::TExecDataQuerySettings exec_settings;
