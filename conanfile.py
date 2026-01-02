34b4603e1698d30df26f241488a88a137818bb7e
denis.razinkin <denis.razinkin@exness.com>
2026-01-02 14:30:34 +0200

    fix version

--- conanfile.py
+++ conanfile.py
@@ -94,7 +94,7 @@ class UserverConan(ConanFile):
                 'version.txt',
             ),
         )
-        hotfix_version = "1-dev"
+        hotfix_version = "1"
         self.version = content.strip() + f".{hotfix_version}"  # pylint: disable=attribute-defined-outside-init
 
     def source(self):
