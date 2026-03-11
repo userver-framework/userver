9ca1664cd73896f605079a8a410a8e866b5f74e0
Antony Polukhin <antoshkka@gmail.com>
2026-03-11 18:40:18 +0300

    Update ubuntu_install_kafka.sh

--- scripts/kafka/ubuntu_install_kafka.sh
+++ scripts/kafka/ubuntu_install_kafka.sh
@@ -2,9 +2,9 @@
 
 sudo apt install -y openjdk-17-jdk
 
-curl https://dlcdn.apache.org/kafka/4.0.0/kafka_2.13-4.0.0.tgz -o kafka.tgz
+curl https://dlcdn.apache.org/kafka/4.0.1/kafka_2.13-4.0.1.tgz -o kafka.tgz
 mkdir -p /etc/kafka
 tar xf kafka.tgz --directory=/etc/kafka
-cp -r /etc/kafka/kafka_2.13-4.0.0/* /etc/kafka/
-rm -rf /etc/kafka/kafka_2.13-4.0.0
+cp -r /etc/kafka/kafka_2.13-4.0.1/* /etc/kafka/
+rm -rf /etc/kafka/kafka_2.13-4.0.1
 rm kafka.tgz
