36e93638cc8ef11e55efd2a6c73bfa384ae5e20f
Dmitry Isaikin <isaikin-dmitry@yandex.ru>
2026-07-17 13:57:35 +0300

    ubuntu_install_kafka.sh: update kafka download URL
    Old URL returns 404 now

--- scripts/kafka/ubuntu_install_kafka.sh
+++ scripts/kafka/ubuntu_install_kafka.sh
@@ -3,12 +3,11 @@
 # Exit on any error and treat unset variables as errors, print all commands
 set -o errexit -o nounset -o pipefail -o posix -x
 
-KAFKA_VERSION=4.0.1
+KAFKA_VERSION=4.3.1
 KAFKA_HOME=/etc/kafka
+KAFKA_URL="https://www.apache.org/dyn/closer.lua/kafka/${KAFKA_VERSION}/kafka_2.13-${KAFKA_VERSION}.tgz?action=download"
 
 DEBIAN_FRONTEND=noninteractive sudo apt install -y openjdk-17-jdk
 
-curl "https://dlcdn.apache.org/kafka/$KAFKA_VERSION/kafka_2.13-$KAFKA_VERSION.tgz" -o kafka.tgz
 mkdir -p "$KAFKA_HOME"
-tar -xzf kafka.tgz --directory="$KAFKA_HOME" --strip-components=1
-rm kafka.tgz
+curl -fsSL "$KAFKA_URL" | tar -xzf - --directory="$KAFKA_HOME" --strip-components=1
