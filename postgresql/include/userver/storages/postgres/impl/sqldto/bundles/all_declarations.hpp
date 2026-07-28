abeb82693bf0b4551ef6d8816e4711d60bdab753
ilatypov <ilatypov@yandex-team.com>
2026-07-28 12:19:22 +0300

    cc userver: add bundles for sqldto
    Create sqldto bundles for codegen
    commit_hash:34f471fd08da0c1d53bef32b99fb6bfe624e55f7

---
+++ postgresql/include/userver/storages/postgres/impl/sqldto/bundles/all_declarations.hpp
@@ -0,0 +1,19 @@
+#pragma once
+
+#include <boost/uuid/uuid.hpp>
+
+#include <userver/storages/postgres/cluster.hpp>
+#include <userver/storages/postgres/io/array_types.hpp>
+#include <userver/storages/postgres/io/chrono.hpp>
+#include <userver/storages/postgres/io/decimal64.hpp>
+#include <userver/storages/postgres/io/enum_types.hpp>
+#include <userver/storages/postgres/io/floating_point_types.hpp>
+#include <userver/storages/postgres/io/integral_types.hpp>
+#include <userver/storages/postgres/io/io_fwd.hpp>
+#include <userver/storages/postgres/io/json_types.hpp>
+#include <userver/storages/postgres/io/optional.hpp>
+#include <userver/storages/postgres/io/range_types.hpp>
+#include <userver/storages/postgres/io/row_types.hpp>
+#include <userver/storages/postgres/io/string_types.hpp>
+#include <userver/storages/postgres/io/uuid.hpp>
+#include <userver/storages/postgres/postgres_fwd.hpp>
