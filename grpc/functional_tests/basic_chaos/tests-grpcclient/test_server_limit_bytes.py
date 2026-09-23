ab939bd13abb295e802772d4d9496f2a243c80c4
antoshkka <antoshkka@userver.tech>
2026-09-23 11:44:04 +0300

    feat grpc: deal with some flapping tests
    Tests: протестировано локально и в CI
    commit_hash:66ce1f8af7df879c8ef980f45c8ed253fbdb6018

--- grpc/functional_tests/basic_chaos/tests-grpcclient/test_server_limit_bytes.py
+++ grpc/functional_tests/basic_chaos/tests-grpcclient/test_server_limit_bytes.py
@@ -5,14 +5,10 @@ import requests_client
 
 @pytest.mark.parametrize('case', requests_client.ALL_CASES)
 async def test_server_limit_bytes(grpc_ch, service_client, gate, case):
-    if case in ['say_hello_request_stream', 'say_hello_streams']:
-        # TODO(TAXICOMMON-9695, TAXICOMMON-11262) research the flaky test
-        pytest.skip('Flaky')
-        return
-
     for i in range(100, 250, 50):
         await gate.to_server_limit_bytes(i)
         await requests_client.unavailable_request(service_client, gate, case)
 
     await gate.to_server_pass()
+    await requests_client.ensure_grpc_ready_after_gate_reset(grpc_ch, service_client)
     await requests_client.check_200_for(case)(grpc_ch, service_client, gate)
