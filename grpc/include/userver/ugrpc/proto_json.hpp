6cb832f235147067885dd01f24610de1c3ffce4a
arudenko02 <arudenko02@userver.tech>
2026-09-24 19:46:38 +0300

    refactor grpc: rewrite legacy ugrpc JSON conversions via protobuf::json commit_hash:944004c53dcfec6a98736f04743cb7654a2e81e6

--- grpc/include/userver/ugrpc/proto_json.hpp
+++ grpc/include/userver/ugrpc/proto_json.hpp
@@ -4,11 +4,19 @@
 /// @brief Utilities for conversion Protobuf -> Json
 /// @ingroup userver_formats_serialize userver_formats_parse
 
+#include <string>
+#include <string_view>
+#include <type_traits>
+
+#include <google/protobuf/message.h>
 #include <google/protobuf/struct.pb.h>
 #include <google/protobuf/util/json_util.h>
 
-#include <userver/formats/json/serialize.hpp>
+#include <userver/formats/json/exception.hpp>
+#include <userver/formats/json/serialize.hpp>  // kept: legacy users rely on this transitive include
 #include <userver/formats/json/value.hpp>
+#include <userver/formats/parse/to.hpp>
+#include <userver/formats/serialize/to.hpp>
 
 USERVER_NAMESPACE_BEGIN
 
@@ -17,10 +25,19 @@ namespace ugrpc {
 namespace impl {
 
 extern const google::protobuf::util::JsonPrintOptions kDefaultJsonPrintOptions;
+
 extern const google::protobuf::util::JsonParseOptions kDefaultJsonParseOptions;
 
+/// @throws formats::json::Exception on invalid JSON or conversion failure
 void FromJsonStringImpl(
     std::string_view json_string,
+    google::protobuf::Message& output,
+    const google::protobuf::util::JsonParseOptions& options
+);
+
+/// @throws formats::json::Exception on conversion failure
+void JsonToMessageImpl(
+    const formats::json::Value& json,
     google::protobuf::Message& output,
     const google::protobuf::util::JsonParseOptions& options
 );
@@ -42,10 +59,12 @@ formats::json::Value MessageToJson(
 
 /// @brief Returns Json-string representation of protobuf message
 /// @throws formats::json::Exception
+/// @warning This function is **deprecated** and will be removed, use @ref protobuf::json::MessageToJsonString .
 std::string ToJsonString(const google::protobuf::Message& message);
 
 /// @brief Returns Json-string representation of protobuf message
 /// @throws formats::json::Exception
+/// @warning This function is **deprecated** and will be removed, use @ref protobuf::json::MessageToJsonString .
 std::string ToJsonString(
     const google::protobuf::Message& message,
     const google::protobuf::util::JsonPrintOptions& options
@@ -57,7 +76,7 @@ std::string ToJsonString(
 template <typename Message>
 Message JsonToMessage(const formats::json::Value& json) {
     Message message;
-    impl::FromJsonStringImpl(formats::json::ToString(json), message, impl::kDefaultJsonParseOptions);
+    impl::JsonToMessageImpl(json, message, impl::kDefaultJsonParseOptions);
     return message;
 }
 
@@ -67,12 +86,14 @@ Message JsonToMessage(const formats::json::Value& json) {
 template <typename Message>
 Message JsonToMessage(const formats::json::Value& json, const google::protobuf::util::JsonParseOptions& options) {
     Message message;
-    impl::FromJsonStringImpl(formats::json::ToString(json), message, options);
+    impl::JsonToMessageImpl(json, message, options);
     return message;
 }
 
 /// @brief Parses Json to a protobuf message. Throws on unknown enum values and unknown fields by default.
 /// @throws formats::json::Exception on invalid json, field type mismatch, unknown enum values and unknown fields.
+/// @warning This function is **deprecated** and will be removed, use @ref formats::json::FromString and
+///          @ref protobuf::json::JsonToMessage .
 template <typename Message>
 Message FromJsonString(std::string_view json_string) {
     Message message;
@@ -82,6 +103,8 @@ Message FromJsonString(std::string_view json_string) {
 
 /// @brief Parses Json to a protobuf message. Throws on unknown enum values.
 /// @throws formats::json::Exception on invalid json, field type mismatch and unknown enum values.
+/// @warning This function is **deprecated** and will be removed, use @ref formats::json::FromString and
+///          @ref protobuf::json::JsonToMessage .
 template <typename Message>
 Message FromJsonString(std::string_view json_string, const google::protobuf::util::JsonParseOptions& options) {
     Message message;
@@ -126,13 +149,12 @@ Message Parse(const json::Value& value, To<Message>) {
 }
 
 /// @cond
-// Implementation detail: optimization for `google::protobuf::Value` specifically.
+// Non-template overloads are kept for compatibility with the legacy implementation, which had hand-written parsers
+// for these well-known types. Now they behave exactly like the generic overload above.
 google::protobuf::Value Parse(const json::Value& value, To<google::protobuf::Value>);
 
-// Implementation detail: optimization for `google::protobuf::Struct` specifically.
 google::protobuf::Struct Parse(const json::Value& value, To<google::protobuf::Struct>);
 
-// Implementation detail: optimization for `google::protobuf::ListValue` specifically.
 google::protobuf::ListValue Parse(const json::Value& value, To<google::protobuf::ListValue>);
 /// @endcond
 
--- grpc/src/ugrpc/proto_json.cpp
+++ grpc/src/ugrpc/proto_json.cpp
@@ -1,11 +1,14 @@
 #include <userver/ugrpc/proto_json.hpp>
 
-#include <cstddef>
+#include <string>
+#include <string_view>
 
 #include <fmt/format.h>
-#include <grpcpp/support/config.h>
-#include <boost/container/small_vector.hpp>
 
+#include <userver/formats/json/serialize.hpp>
+#include <userver/protobuf/exceptions.hpp>
+#include <userver/protobuf/json/convert.hpp>
+#include <userver/protobuf/json/convert_options.hpp>
 #include <userver/utils/assert.hpp>
 
 USERVER_NAMESPACE_BEGIN
@@ -31,44 +34,49 @@ const google::protobuf::util::JsonParseOptions kDefaultJsonParseOptions = [] {
     return options;
 }();
 
+}  // namespace impl
+
+namespace {
+
+// 'add_whitespace' is handled separately: it does not affect formats::json::Value, and for the string output the
+// result is pretty-printed with formats::json::ToPrettyString.
+protobuf::json::PrintOptions ToPrintOptions(const google::protobuf::util::JsonPrintOptions& options) {
+    protobuf::json::PrintOptions result;
+#if GOOGLE_PROTOBUF_VERSION >= 5026000
+    result.always_print_fields_with_no_presence = options.always_print_fields_with_no_presence;
+#else
+    result.always_print_fields_with_no_presence = options.always_print_primitive_fields;
+#endif
+    result.always_print_enums_as_ints = options.always_print_enums_as_ints;
+    result.preserve_proto_field_names = options.preserve_proto_field_names;
+    return result;
+}
+
+protobuf::json::ParseOptions ToParseOptions(const google::protobuf::util::JsonParseOptions& options) {
+    protobuf::json::ParseOptions result;
+    result.ignore_unknown_fields = options.ignore_unknown_fields;
+    return result;
+}
+
+}  // namespace
+
+namespace impl {
+
 void FromJsonStringImpl(
     std::string_view json_string,
     google::protobuf::Message& output,
     const google::protobuf::util::JsonParseOptions& options
 ) {
-#if defined(ARCADIA_ROOT)
-    // JSON utils use y_absl::string_view.
-    const auto status = google::protobuf::util::JsonStringToMessage(
-        y_absl::string_view(json_string.data(), json_string.size()),
-        &output,
-        options
-    );
-#elif GOOGLE_PROTOBUF_VERSION >= 4022000
-    // JSON utils use absl::string_view.
-    const auto status = google::protobuf::util::JsonStringToMessage(
-        absl::string_view(json_string.data(), json_string.size()),
-        &output,
-        options
-    );
-#else
-    // JSON utils use StringPiece.
-    const auto status = google::protobuf::util::JsonStringToMessage(
-        google::protobuf::StringPiece(json_string.data(), json_string.size()),
-        &output,
-        options
-    );
-#endif
+    JsonToMessageImpl(formats::json::FromString(json_string), output, options);
+}
 
-    if (!status.ok()) {
-#if GOOGLE_PROTOBUF_VERSION >= 4022000
-        // JSON utils use absl::string_view.
-        const std::string_view message(status.message().data(), status.message().size());
-#else
-        // JSON utils use StringPiece.
-        const std::string_view message(status.message().data(), static_cast<std::size_t>(status.message().size()));
-#endif
-        throw formats::json::Exception(fmt::format("Cannot parse protobuf from string: {}", message));
-    }
+void JsonToMessageImpl(
+    const formats::json::Value& json,
+    google::protobuf::Message& output,
+    const google::protobuf::util::JsonParseOptions& options
+) {
+    const auto parse_options = ToParseOptions(options);
+    protobuf::json::JsonToMessage(json, output, parse_options);
 }
 
 }  // namespace impl
@@ -81,7 +89,8 @@ formats::json::Value MessageToJson(
     const google::protobuf::Message& message,
     const google::protobuf::util::JsonPrintOptions& options
 ) {
-    return formats::json::FromString(ToJsonString(message, options));
+    const auto print_options = ToPrintOptions(options);
+    return protobuf::json::MessageToJson(message, print_options);
 }
 
 std::string ToJsonString(const google::protobuf::Message& message) {
@@ -92,15 +101,12 @@ std::string ToJsonString(
     const google::protobuf::Message& message,
     const google::protobuf::util::JsonPrintOptions& options
 ) {
-    grpc::string result{};
-
-    auto status = google::protobuf::util::MessageToJsonString(message, &result, options);
-
-    if (!status.ok()) {
-        throw formats::json::Exception("Cannot convert protobuf to string");
+    if (options.add_whitespace) {
+        return formats::json::ToPrettyString(MessageToJson(message, options));
     }
 
-    return result;
+    const auto print_options = ToPrintOptions(options);
+    return protobuf::json::MessageToJsonString(message, print_options);
 }
 
 }  // namespace ugrpc
@@ -114,196 +120,17 @@ json::Value Serialize(const google::protobuf::Message& message, To<json::Value>)
 }  // namespace formats::serialize
 
 namespace formats::parse {
-namespace {
 
-enum class Type { kStruct, kArray };
-
-Type ParseType(const formats::json::Value& value) { return value.IsObject() ? Type::kStruct : Type::kArray; }
-
-Type ParseType(const formats::json::Value::const_iterator& iter) {
-    return iter->IsObject() ? Type::kStruct : Type::kArray;
+google::protobuf::Value Parse(const json::Value& value, To<google::protobuf::Value>) {
+    return ugrpc::JsonToMessage<google::protobuf::Value>(value);
 }
-
-std::string GetName(const formats::json::Value::const_iterator& iter, const Type previous_type) {
-    return previous_type == Type::kStruct ? iter.GetName() : "";
-}
-
-class ResultStackFrame final {
-public:
-    explicit ResultStackFrame(const formats::json::Value& value)
-        : ResultStackFrame(ParseType(value), value.GetSize(), "")
-    {}
-
-    ResultStackFrame(const formats::json::Value::const_iterator& iter, const Type previous_type)
-        : ResultStackFrame(ParseType(iter), iter->GetSize(), GetName(iter, previous_type))
-    {}
-
-    void SetStructField(std::string_view field_name, google::protobuf::Value&& field) {
-        UINVARIANT(type_ == Type::kStruct, "invalid type");
-#if GOOGLE_PROTOBUF_VERSION >= 4022000
-        (*value_.mutable_struct_value()->mutable_fields())[field_name] = std::move(field);
-#else
-        // No transparent comparisons till
-        // https://github.com/protocolbuffers/protobuf/commit/38d6de1eef8163342084fe
-        (*value_.mutable_struct_value()->mutable_fields())[std::string{field_name}] = std::move(field);
-#endif
-
-        --elements_await_;
-    }
-
-    void AddListElement(google::protobuf::Value&& field) {
-        UINVARIANT(type_ == Type::kArray, "invalid type");
-        *(value_.mutable_list_value()->mutable_values()->Add()) = std::move(field);
-        --elements_await_;
-    }
-
-    bool IsStruct() const { return type_ == Type::kStruct; }
-
-    bool IsArray() const { return type_ == Type::kArray; }
-
-    Type GetType() const { return type_; }
-
-    bool AwaitElements() const { return elements_await_ != 0; }
-
-    std::string_view GetOuterFieldName() const { return outer_field_name_; }
-
-    google::protobuf::Value GetValue() { return google::protobuf::Value(std::move(value_)); }
-
-private:
-    ResultStackFrame(const Type type, std::size_t elements_await, std::string&& outer_field_name)
-        : type_(type),
-          elements_await_(elements_await),
-          outer_field_name_(outer_field_name)
-    {
-        if (type == Type::kStruct) {
-            value_.mutable_struct_value();
-        } else {
-            value_.mutable_list_value();
-        }
-    }
-
-    Type type_;
-    std::size_t elements_await_;
-    std::string outer_field_name_;
-    google::protobuf::Value value_{};
-};
-
-class StackFrame {
-public:
-    using Iterator = formats::json::Value::const_iterator;
-
-    StackFrame(Iterator begin, Iterator end)
-        : cur_(begin),
-          end_(end)
-    {}
-
-    bool IsTrivial() const { return !(cur_->IsObject() || cur_->IsArray()); }
-
-    std::size_t GetSize() const { return cur_->GetSize(); }
-
-    Iterator GetIter() const { return cur_; }
-
-    std::string GetName() const { return cur_.GetName(); }
-
-    void Advance() { ++cur_; }
-
-    bool IsValid() const { return cur_ != end_; }
-
-private:
-    Iterator cur_;
-    Iterator end_;
-};
-
-constexpr std::size_t kInitialStackDepth = 32;
-
-template <typename T>
-using Stack = boost::container::small_vector<T, kInitialStackDepth>;
-
-google::protobuf::Value ParseValue(const formats::json::Value& value) {
-    google::protobuf::Value result;
-    if (value.IsDouble()) {
-        result.set_number_value(value.As<double>());
-    } else if (value.IsInt64()) {
-        result.set_number_value(value.As<std::int64_t>());
-    } else if (value.IsUInt64()) {
-        result.set_number_value(value.As<std::uint64_t>());
-    } else if (value.IsBool()) {
-        result.set_bool_value(value.As<bool>());
-    } else if (value.IsString()) {
-        result.set_string_value(value.As<std::string>());
-    } else if (value.IsNull()) {
-        result.set_null_value(google::protobuf::NullValue{});
-    } else {
-        throw formats::json::ParseException("unsupported type");
-    }
-    return result;
-}
-
-google::protobuf::Value ParseImpl(const formats::json::Value& value) {
-    if (!(value.IsArray() || value.IsObject())) {
-        return ParseValue(value);
-    }
-    if (value.IsEmpty()) {
-        return ResultStackFrame(value).GetValue();
-    }
-
-    Stack<StackFrame> stack{};
-    Stack<ResultStackFrame> result(1, ResultStackFrame(value));
-
-    stack.emplace_back(value.begin(), value.end());
-    for (;;) {
-        if (stack.back().IsTrivial()) {
-            if (result.back().IsStruct()) {
-                result.back().SetStructField(stack.back().GetName(), ParseValue(*stack.back().GetIter()));
-            } else {
-                result.back().AddListElement(ParseValue(*stack.back().GetIter()));
-            }
-            stack.back().Advance();
-        } else {
-            result.emplace_back(stack.back().GetIter(), result.back().GetType());
-            auto stack_back_value = stack.back().GetIter();
-            stack.back().Advance();
-            stack.emplace_back(stack_back_value->begin(), stack_back_value->end());
-        }
-
-        while (result.size() >= 2 && !result.back().AwaitElements()) {
-            auto prev_back = result.end() - 2;
-            auto back = result.back();
-            if (prev_back->AwaitElements()) {
-                if (prev_back->IsStruct()) {
-                    prev_back->SetStructField(back.GetOuterFieldName(), back.GetValue());
-                } else {
-                    prev_back->AddListElement(back.GetValue());
-                }
-                result.pop_back();
-            }
-        }
-        while (!stack.back().IsValid()) {
-            stack.pop_back();
-            if (stack.empty()) {
-                return result.back().GetValue();
-            }
-        }
-    }
-}
-
-}  // namespace
-
-// TODO use iterative implementation for any google::protobuf::Message, not just for top-level Value and Struct.
-google::protobuf::Value Parse(const json::Value& value, To<google::protobuf::Value>) { return ParseImpl(value); }
 
 google::protobuf::Struct Parse(const json::Value& value, To<google::protobuf::Struct>) {
-    value.CheckObject();
-    auto protobuf_value = ParseImpl(value);
-    UASSERT(protobuf_value.has_struct_value());
-    return std::move(*protobuf_value.mutable_struct_value());
+    return ugrpc::JsonToMessage<google::protobuf::Struct>(value);
 }
 
 google::protobuf::ListValue Parse(const json::Value& value, To<google::protobuf::ListValue>) {
-    value.CheckArray();
-    auto protobuf_value = ParseImpl(value);
-    UASSERT(protobuf_value.has_list_value());
-    return std::move(*protobuf_value.mutable_list_value());
+    return ugrpc::JsonToMessage<google::protobuf::ListValue>(value);
 }
 
 }  // namespace formats::parse
